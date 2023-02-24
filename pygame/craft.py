2be3c41cf7c36e6411a028a8c9cd0ee793cc93f3
Mike-Teng <m88215@gmail.com>
2023-02-24 17:35:35 +0800

    modify craft

--- pygame/craft.py
+++ pygame/craft.py
@@ -9,13 +9,14 @@ SCREEN_WIDTH = 960
 t = 0
 clock = pygame.time.Clock()
 max_msl = 100
+total_msl = 6
 ship_loc = {'x':480, 'y':50}
-msl_loc = ((300,600), (500,500), (700,600), (500,300))
-msl_f = [[False]*max_msl,[False]*max_msl,[False]*max_msl,[False]*max_msl]
-msl_x = [[0]*max_msl,[0]*max_msl,[0]*max_msl,[0]*max_msl]
-msl_y = [[0]*max_msl,[0]*max_msl,[0]*max_msl,[0]*max_msl]
-msl_a = [[0]*max_msl,[0]*max_msl,[0]*max_msl,[0]*max_msl]
-no = [0]*4
+msl_loc = ((300,600), (500,500), (700,600), (500,300), (300,300), (700,300))
+msl_f = [[False for i in range(max_msl)] for j in range(total_msl)]
+msl_x = [[0 for i in range(max_msl)] for j in range(total_msl)]
+msl_y = [[0 for i in range(max_msl)] for j in range(total_msl)]
+msl_a = [[0 for i in range(max_msl)] for j in range(total_msl)]
+no = [0]*total_msl
 
 def set_msl(mslNum,x,y):
     global no
@@ -28,14 +29,14 @@ def set_msl(mslNum,x,y):
         no[mslNum] = (no[mslNum]+1)%max_msl
 
 def move_msl(screen):
-    for mslNum in range(4):
+    for mslNum in range(total_msl):
         for i in range(max_msl):
             if msl_f[mslNum][i] is True:
-                msl_x[mslNum][i] = msl_x[mslNum][i] + 2*cos(radians(msl_a[mslNum][i]))
-                msl_y[mslNum][i] = msl_y[mslNum][i] + 2*sin(radians(msl_a[mslNum][i]))
-                pygame.draw.circle(screen, (255, 200, 0), (msl_x[mslNum][i], msl_y[mslNum][i]), 4)
+                msl_x[mslNum][i] = msl_x[mslNum][i] + 1.5*cos(radians(msl_a[mslNum][i]))
+                msl_y[mslNum][i] = msl_y[mslNum][i] + 1.5*sin(radians(msl_a[mslNum][i]))
+                pygame.draw.circle(screen, (255, 200, 0), (msl_x[mslNum][i], msl_y[mslNum][i]), 3)
                 # print(f'draw{mslNum} {i},x = {msl_x[mslNum][i]}, y = {msl_y[mslNum][i]}')
-            if msl_y[mslNum][i] < 10 or msl_y[mslNum][i] > 710 or msl_x[mslNum][i] < 10 or msl_x[mslNum][i] > 950 :
+            if msl_y[mslNum][i] < 3 or msl_y[mslNum][i] > 717 or msl_x[mslNum][i] < 3 or msl_x[mslNum][i] > 957 :
                 msl_f[mslNum][i] = False
 
 def collision():
@@ -45,7 +46,7 @@ def collision():
 
     for x, y in list(zip(flat_x, flat_y)):
         # print(x,y,ship_loc['x'],ship_loc['y'])
-        if (int(x)-6 <= int(ship_loc['x']) <= int(x)+6) and (int(y)-6 <= int(ship_loc['y']) <= int(y)+6): 
+        if (int(x)-4 <= int(ship_loc['x']) <= int(x)+4) and (int(y)-4 <= int(ship_loc['y']) <= int(y)+4): 
             t = pygame.time.get_ticks() 
             return True
     return False
@@ -59,26 +60,28 @@ def check_reset_msl(mslNum):
         randomX = randint(20, 940)
         randomY = randint(20, 700)
         set_msl(mslNum, x=randomX, y=randomY)
-        no = [0]*4
+        no = [0]*total_msl
 
 
 def move_ship(screen,key):
     global ship_loc
     
     if key[pygame.K_UP] == 1:
-        ship_loc['y'] = ship_loc['y']-3
+        ship_loc['y'] = ship_loc['y']-2
     if key[pygame.K_DOWN] == 1:
-        ship_loc['y'] = ship_loc['y']+3
+        ship_loc['y'] = ship_loc['y']+2
     if key[pygame.K_LEFT] == 1:
-        ship_loc['x'] = ship_loc['x']-3
+        ship_loc['x'] = ship_loc['x']-2
     if key[pygame.K_RIGHT] == 1:
-        ship_loc['x'] = ship_loc['x']+3
+        ship_loc['x'] = ship_loc['x']+2
     
 
 set_msl(mslNum=0, x=msl_loc[0][0], y=msl_loc[0][1])
 set_msl(mslNum=1, x=msl_loc[1][0], y=msl_loc[1][1])
 set_msl(mslNum=2, x=msl_loc[2][0], y=msl_loc[2][1])
 set_msl(mslNum=3, x=msl_loc[3][0], y=msl_loc[3][1])
+set_msl(mslNum=4, x=msl_loc[4][0], y=msl_loc[4][1])
+set_msl(mslNum=5, x=msl_loc[5][0], y=msl_loc[5][1])
 
 pygame.init()
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -88,7 +91,7 @@ run = True
 pause = False
 while run:
     screen.fill((0, 0, 0))
-    pygame.draw.circle(screen, (255, 255, 255), (ship_loc['x'], ship_loc['y']), 10)
+    pygame.draw.circle(screen, (255, 255, 255), (ship_loc['x'], ship_loc['y']), 6)
     key = pygame.key.get_pressed()
     move_ship(screen, key)
     move_msl(screen)
@@ -106,13 +109,13 @@ while run:
                 run = False
                 pause = False
 
-    for i in range(4):
+    for i in range(total_msl):
         check_reset_msl(i)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
-    clock.tick(60)
+    clock.tick(50)
     pygame.display.flip()
     pygame.display.update()
 
