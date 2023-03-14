3eeedb0284f221ded3ffbe58b93b957e2367bd9b
Mike-Teng <m88215@gmail.com>
2023-03-14 12:53:45 +0800

    tab -> 4spaces; variables -> snake_style

--- pygame/geometry.py
+++ pygame/geometry.py
@@ -7,37 +7,38 @@ SCREEN_WIDTH = 800
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption('pyivp Geometry Demo')
 
-pattern = pyivp.string2SegList("format=lawnmower, x=200, y=200, height=200, width=200, lane_width=40, rows=north-south, startx=0, starty=0, degs=0")
-waypointPloy = pyivp.string2Poly("x=600, y=200, format=radial, radius=100, pts=8")
-waypointSeg = waypointPloy.exportSegList(500, 200)
+pattern = pyivp.string2SegList("format=lawnmower, x=200, y=200, height=200, width=200, lane_width=40,\
+                                rows=north-south, startx=0, starty=0, degs=0")
+waypoint_ploy = pyivp.string2Poly("x=600, y=200, format=radial, radius=100, pts=8")
+waypoint_seg = waypoint_ploy.exportSegList(500, 200)
 
 run = True
 while run:
 
-	screen.fill((202, 228, 241))
+    screen.fill((202, 228, 241))
 
-	for i in range(pattern.size()-1):
-		x1 = int(pattern.get_vx(i))
-		y1 = int(pattern.get_vy(i))
-		x2 = int(pattern.get_vx(i+1))
-		y2 = int(pattern.get_vy(i+1))
-		pygame.draw.line(screen, (100,100,100), (x1, y1), (x2, y2))
-	
-	for i in range(waypointSeg.size()):
-		x1 = round(waypointSeg.get_vx(i))
-		y1 = round(waypointSeg.get_vy(i))
-		x2 = round(waypointSeg.get_vx(i+1))
-		y2 = round(waypointSeg.get_vy(i+1))
-		if i == waypointSeg.size()-1:
-			x2 = round(waypointSeg.get_vx(0))
-			y2 = round(waypointSeg.get_vy(0))
-		pygame.draw.line(screen, (100,100,100), (x1, y1), (x2, y2))
+    for i in range(pattern.size()-1):
+        x1 = int(pattern.get_vx(i))
+        y1 = int(pattern.get_vy(i))
+        x2 = int(pattern.get_vx(i+1))
+        y2 = int(pattern.get_vy(i+1))
+        pygame.draw.line(screen, (100,100,100), (x1, y1), (x2, y2))
+    
+    for i in range(waypoint_seg.size()):
+        x1 = round(waypoint_seg.get_vx(i))
+        y1 = round(waypoint_seg.get_vy(i))
+        x2 = round(waypoint_seg.get_vx(i+1))
+        y2 = round(waypoint_seg.get_vy(i+1))
+        if i == waypoint_seg.size()-1:
+            x2 = round(waypoint_seg.get_vx(0))
+            y2 = round(waypoint_seg.get_vy(0))
+        pygame.draw.line(screen, (100,100,100), (x1, y1), (x2, y2))
 
-	#event handler
-	for event in pygame.event.get():
-		if event.type == pygame.QUIT:
-			run = False
+    #event handler
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
 
-	pygame.display.update()
+    pygame.display.update()
 
 pygame.quit()
