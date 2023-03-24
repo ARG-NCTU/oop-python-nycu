2a1483ee7b98a94f5b46103bd71d99edd7e947db
Mike-Teng <m88215@gmail.com>
2023-03-24 16:57:12 +0800

    modify pygame/06

--- pygame/06_BouncyBall.py
+++ pygame/06_BouncyBall.py
@@ -3,14 +3,15 @@ from random import choice
 
 SCREEN_WIDTH = 640
 SCREEN_HEIGHT = 480
-WHITE = (255,255,255)
+WHITE = (255, 255, 255)
+
 
 class Ball():
     def __init__(self):
         self.x = SCREEN_WIDTH / 2
         self.y = SCREEN_HEIGHT / 2
-        self.x_direction = choice((-2,2))
-        self.y_direction = choice((-2,2))
+        self.x_direction = choice((-2, 2))
+        self.y_direction = choice((-2, 2))
         self.radius = 20
 
     def move(self):
@@ -20,13 +21,13 @@ class Ball():
 
     def contact_detect(self):
         if self.x + self.radius >= SCREEN_WIDTH or\
-            self.x - self.radius <= 0:
+                self.x - self.radius <= 0:
             self.x_direction = -self.x_direction
-        
+
         if self.y + self.radius >= SCREEN_HEIGHT or\
-            self.y - self.radius <= 0:
+                self.y - self.radius <= 0:
             self.y_direction = -self.y_direction
-        
+
     def draw(self, screen):
         pygame.draw.circle(screen, WHITE, (self.x, self.y), self.radius)
 
@@ -37,16 +38,16 @@ clock = pygame.time.Clock()
 pygame.display.set_caption('BouncyBall')
 ball = Ball()
 
-#game loop
+# game loop
 is_running = True
 while is_running:
     screen.fill((0, 0, 0))
     ball.move()
     ball.draw(screen)
-    
-    #event handler
+
+    # event handler
     for event in pygame.event.get():
-        #quit game
+        # quit game
         if event.type == pygame.QUIT:
             is_running = False
 
