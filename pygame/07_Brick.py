7d63b9b428054c49b6879ddcb63cdb113540b5fd
Mike-Teng <m88215@gmail.com>
2023-03-25 15:23:56 +0800

    use json config to load

--- pygame/07_Brick.py
+++ pygame/07_Brick.py
@@ -1,5 +1,6 @@
 import pygame
 import pyivp
+import json
 from random import choice
 
 SCREEN_WIDTH = 640
@@ -30,7 +31,7 @@ class Brick():
 
 
 class Ball():
-    def __init__(self):
+    def __init__(self, x, y, radius):
         self.x = 300
         self.y = 400
         self.x_direction = choice((-2, 2))
@@ -74,8 +75,12 @@ screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 clock = pygame.time.Clock()
 pygame.display.set_caption('Brick')
 
-ball = Ball()
-brick = Brick(340, 200, 100)
+# load config
+with open('./config/07_config.json', 'r') as f:
+    config = json.load(f)
+
+ball = Ball(config["ball_x"], config["ball_y"], config["ball_radius"])
+brick = Brick(config["brick_x"], config["brick_y"], config["brick_radius"])
 
 # game loop
 is_running = True
