3670778724714381cb625d433701152a8732d2b4
hchengwang <hchengwang@gmail.com>
2023-03-31 01:42:52 +0800

    add python compiler examples fib()

---
+++ sample-code/fib-ocw.py
@@ -0,0 +1,25 @@
+
+def fib(n):
+    if n == 0 or n == 1:
+        return 1
+    else:
+        return fib(n - 1) + fib(n - 2)
+
+for i in range(40):
+    print('fib(' + str(i) + ') =', fib(i))
+
+def fast_fib(n, memo = {}):
+    """Assumes n is an int >= 0, memo used only by recursive calls
+       Returns Fibonacci of n"""
+    if n == 0 or n == 1:
+        return 1
+    try:
+        return memo[n]
+    except KeyError:
+        result = fast_fib(n-1, memo) + fast_fib(n-2, memo)
+        memo[n] = result
+        return result
+
+for i in range(40):
+    print('fib(' + str(i) + ') =', fast_fib(i))
+
