e38ec17b8ddb0021bb6126ce8e66e66e7921fe43
hchengwang <hchengwang@gmail.com>
2023-04-03 23:47:13 +0800

    finish lecture 2 with pytest; add README

--- mit-ocw-exercises/60002-lecture2/menu.py
+++ mit-ocw-exercises/60002-lecture2/menu.py
@@ -28,12 +28,18 @@ class Menu(object):
     """
     A menu of food items
     """
-    def __init__(self, names, values, calories):
+    def __init__(self, names=[], values=[], calories=[]):
         self.foods = []
         for i in range(len(values)):
             self.foods.append(Food(names[i], values[i],
                               calories[i]))
     
+    def build_large_menu(self, num_items, max_val, max_cost):
+        self.foods = []
+        for i in range(num_items):
+            self.foods.append(Food(str(i), random.randint(1, max_val), 
+                              random.randint(1, max_cost)))
+
     def get_foods(self):
         return self.foods
 
@@ -61,66 +67,58 @@ def greedy(items, max_cost, key_function):
             total_value += items_copy[i].get_value()
     return (result, total_value)
 
+def max_val(to_consider, avail):
+    """Assumes to_consider a list of items, avail a weight
+    Returns a tuple of the total value of a solution to the
+    0/1 knapsack problem and the items of that solution"""
+    if to_consider == [] or avail == 0:
+        result = (0, ())
+    elif to_consider[0].get_cost() > avail:
+        # Explore right branch only
+        result = max_val(to_consider[1:], avail)
+    else:
+        next_item = to_consider[0]
+        # Explore left branch
+        with_val, with_to_take = max_val(
+            to_consider[1:], avail - next_item.get_cost())
+        with_val += next_item.get_value()
+        # Explore right branch
+        without_val, without_to_take = max_val(to_consider[1:], avail)
+        # Choose better branch
+        if with_val > without_val:
+            result = (with_val, with_to_take + (next_item,))
+        else:
+            result = (without_val, without_to_take)
+    return result
 
-#def maxVal(toConsider, avail):
-#    """Assumes toConsider a list of items, avail a weight
-#       Returns a tuple of the total value of a solution to the
-#         0/1 knapsack problem and the items of that solution"""
-#    if toConsider == [] or avail == 0:
-#        result = (0, ())
-#    elif toConsider[0].getCost() > avail:
-#        #Explore right branch only
-#        result = maxVal(toConsider[1:], avail)
-#    else:
-#        nextItem = toConsider[0]
-#        #Explore left branch
-#        withVal, withToTake = maxVal(toConsider[1:],
-#                                     avail - nextItem.getCost())
-#        withVal += nextItem.getValue()
-#        #Explore right branch
-#        withoutVal, withoutToTake = maxVal(toConsider[1:], avail)
-#        #Choose better branch
-#        if withVal > withoutVal:
-#            result = (withVal, withToTake + (nextItem,))
-#        else:
-#            result = (withoutVal, withoutToTake)
-#    return result
-#
-#def buildLargeMenu(numItems, maxVal, maxCost):
-#    items = []
-#    for i in range(numItems):
-#        items.append(Food(str(i),
-#                          random.randint(1, maxVal),
-#                          random.randint(1, maxCost)))
-#    return items
-#
-#def fastMaxVal(toConsider, avail, memo = {}):
-#    """Assumes toConsider a list of subjects, avail a weight
-#         memo supplied by recursive calls
-#       Returns a tuple of the total value of a solution to the
-#         0/1 knapsack problem and the subjects of that solution"""
-#    if (len(toConsider), avail) in memo:
-#        result = memo[(len(toConsider), avail)]
-#    elif toConsider == [] or avail == 0:
-#        result = (0, ())
-#    elif toConsider[0].getCost() > avail:
-#        #Explore right branch only
-#        result = fastMaxVal(toConsider[1:], avail, memo)
-#    else:
-#        nextItem = toConsider[0]
-#        #Explore left branch
-#        withVal, withToTake =\
-#                 fastMaxVal(toConsider[1:],
-#                            avail - nextItem.getCost(), memo)
-#        withVal += nextItem.getValue()
-#        #Explore right branch
-#        withoutVal, withoutToTake = fastMaxVal(toConsider[1:],
-#                                                avail, memo)
-#        #Choose better branch
-#        if withVal > withoutVal:
-#            result = (withVal, withToTake + (nextItem,))
-#        else:
-#            result = (withoutVal, withoutToTake)
-#    memo[(len(toConsider), avail)] = result
-#    return result
+
+def fast_max_val(to_consider, avail, memo={}):
+    """Assumes to_consider a list of subjects, avail a weight 
+    memo supplied by recursive calls 
+    Returns a tuple of the total value of a solution to the 
+    0/1 knapsack problem and the subjects of that solution"""
+    if (len(to_consider), avail) in memo:
+        result = memo[(len(to_consider), avail)]
+    elif to_consider == [] or avail == 0:
+        result = (0, ())
+    elif to_consider[0].get_cost() > avail:
+        # Explore right branch only 
+        result = fast_max_val(to_consider[1:], avail, memo)
+    else:
+        next_item = to_consider[0]
+        # Explore left branch 
+        with_val, with_to_take = fast_max_val(
+            to_consider[1:], avail - next_item.get_cost(), memo)
+        with_val += next_item.get_value()
+        # Explore right branch 
+        without_val, without_to_take = fast_max_val(
+            to_consider[1:], avail, memo)
+        # Choose better branch 
+        if with_val > without_val:
+            result = (with_val, with_to_take + (next_item,))
+        else:
+            result = (without_val, without_to_take)
+    memo[(len(to_consider), avail)] = result
+    return result
+
 
--- mit-ocw-exercises/60002-lecture2/test_menu.py
+++ mit-ocw-exercises/60002-lecture2/test_menu.py
@@ -22,89 +22,17 @@ def test_menu():
     assert Menu.get_foods_str(foods) == \
         'apple: <10, 50>; banana: <20, 100>; orange: <30, 150>; '
 
-def test_greedy():
-    items = [
-        Food("apple", 10, 50),
-        Food("banana", 20, 100),
-        Food("orange", 30, 150),
-    ]
-    constraint = 50
-    key_function = lambda x: 1 / Food.get_cost(x)
-
-    taken, val = greedy(items, constraint, key_function)
-    
-    print('Total value of items taken =', val)
-    print(Menu.get_foods_str(taken))
-
-    assert val == 10
-    assert Menu.get_foods_str(taken) == "apple: <10, 50>; "
-
-def test_greedys():
-
-    names = ['wine', 'beer', 'pizza', 'burger', 'fries',
-             'cola', 'apple', 'donut', 'cake']
-    values = [89, 90, 95, 100, 90, 79, 50, 10]
-    calories = [123, 154, 258, 354, 365, 150, 95, 195]
-    menu = Menu(names, values, calories)
+def test_build_large_menu():
+    menu = Menu()
+    num_items = 10
+    max_val = 50
+    max_cost = 400
+    menu.build_large_menu(num_items, max_val, max_cost)
     foods = menu.get_foods()
 
-    max_units = 750
-
-    print('Use greedy by value to allocate', max_units,
-          'calories')
-    taken, val = greedy(foods, max_units, Food.get_value)
-    expected_str = Menu.get_foods_str(taken)
-    assert val == 284 
-    assert expected_str == "burger: <100, 354>; pizza: <95, 258>; wine: <89, 123>; "
-
-    print('\nUse greedy by cost to allocate', max_units,
-          'calories')
-    taken, val = greedy(foods, max_units,
-               lambda x: 1/Food.get_cost(x))
-    expected_str = Menu.get_foods_str(taken)
-    assert val == 318 
-    assert expected_str == \
-        "apple: <50, 95>; wine: <89, 123>; cola: <79, 150>; beer: <90, 154>; donut: <10, 195>; "
-    
-    print('\nUse greedy by density to allocate', max_units,
-          'calories')
-    taken, val = greedy(foods, max_units, Food.density)
-    expected_str = Menu.get_foods_str(taken)
-    assert val == 318 
-    assert expected_str == \
-        "wine: <89, 123>; beer: <90, 154>; cola: <79, 150>; apple: <50, 95>; donut: <10, 195>; "
-
-#    testMaxVal(foods, 750)
-
-#def testMaxVal(foods, maxUnits, printItems = True):
-#    print('Use search tree to allocate', maxUnits,
-#          'calories')
-#    val, taken = maxVal(foods, maxUnits)
-#    print('Total value of items taken =', val)
-#    if printItems:
-#        for item in taken:
-#            print('   ', item)
-#
-#def test_build_large_menu():
-#
-#    for numItems in (5, 10, 15, 20, 25, 30, 35, 40, 45):
-#        print('Try a menu with', numItems, 'items')
-#        items = buildLargeMenu(numItems, 90, 250)
-#        testMaxVal(items, 750, False)
-#
-#
-#def testMaxVal(foods, maxUnits, algorithm, printItems = True):
-#    print('Menu contains', len(foods), 'items')
-#    print('Use search tree to allocate', maxUnits,
-#          'calories')
-#    val, taken = algorithm(foods, maxUnits)
-#    if printItems:
-#        print('Total value of items taken =', val)
-#        for item in taken:
-#            print('   ', item)
-#          
-#for numItems in (5, 10, 15, 20, 25, 30, 35, 40, 45, 50):
-#    items = buildLargeMenu(numItems, 90, 250)
-#    testMaxVal(items, 750, fastMaxVal, True)
-
+    print(menu)
+    assert len(foods) == num_items
+    if num_items > 0:
+        assert foods[0].get_value() <= max_val
+        assert foods[0].get_cost() <= max_cost
 
