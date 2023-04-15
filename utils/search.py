21b9fa960d63d4dfaf7cba958eb0beddf7747891
hchengwang <hchengwang@gmail.com>
2023-04-15 11:43:35 +0800

    add route problem and aima pytest

--- utils/search.py
+++ utils/search.py
@@ -32,6 +32,10 @@ class Node:
     def __len__(self): return 0 if self.parent is None else (1 + len(self.parent))
     def __lt__(self, other): return self.state < other.state
 
+FIFOQueue = deque
+
+LIFOQueue = list
+
 class PriorityQueue:
     """A queue in which the item with minimum f(item) is always popped first."""
 
@@ -118,6 +122,69 @@ def path_states(node):
     else:
         return (path_states(node.parent)) + [node.state]
 
+def is_cycle(node, k=30):
+    "Does this node form a cycle of length k or less?"
+    def find_cycle(ancestor, k):
+        return (ancestor is not None and k > 0 and
+                (ancestor.state == node.state or find_cycle(ancestor.parent, k - 1)))
+    return find_cycle(node.parent, k)
+
+def breadth_first_search(problem):
+    "Search shallowest nodes in the search tree first."
+    node = Node(problem.initial)
+    if problem.is_goal(problem.initial):
+        return node
+    frontier = FIFOQueue([node])
+    reached = {problem.initial}
+    while frontier:
+        node = frontier.pop()
+        for child in expand(problem, node):
+            s = child.state
+            if problem.is_goal(s):
+                return child
+            if s not in reached:
+                reached.add(s)
+                frontier.appendleft(child)
+    return failure
+
+def iterative_deepening_search(problem):
+    "Do depth-limited search with increasing depth limits."
+    for limit in range(1, sys.maxsize):
+        result = depth_limited_search(problem, limit)
+        if result != cutoff:
+            return result
+        
+        
+def depth_limited_search(problem, limit=10):
+    "Search deepest nodes in the search tree first."
+    frontier = LIFOQueue([Node(problem.initial)])
+    result = failure
+    while frontier:
+        node = frontier.pop()
+        if problem.is_goal(node.state):
+            return node
+        elif len(node) >= limit:
+            result = cutoff
+        elif not is_cycle(node):
+            for child in expand(problem, node):
+                frontier.append(child)
+    return result
+
+
+def depth_first_recursive_search(problem, node=None):
+    if node is None: 
+        node = Node(problem.initial)
+    if problem.is_goal(node.state):
+        return node
+    elif is_cycle(node):
+        return failure
+    else:
+        for child in expand(problem, node):
+            result = depth_first_recursive_search(problem, child)
+            if result:
+                return result
+        return failure
+
 def best_first_search(problem, f):
     "Search nodes with minimum f(node) value first."
     global reached
@@ -156,3 +223,48 @@ def random_lines(X=range(15, 130), Y=range(60), N=150, lengths=range(6, 12)):
 def line(x, y, dx, dy, length):
     """A line of `length` cells starting at (x, y) and going in (dx, dy) direction."""
     return {(x + i * dx, y + i * dy) for i in range(length)}
+
+class RouteProblem(Problem):
+    """A problem to find a route between locations on a `Map`.
+    Create a problem with RouteProblem(start, goal, map=Map(...)}).
+    States are the vertexes in the Map graph; actions are destination states."""
+
+    def actions(self, state):
+        """The places neighboring `state`."""
+        return self.map.neighbors[state]
+
+    def result(self, state, action):
+        """Go to the `action` place, if the map says that is possible."""
+        return action if action in self.map.neighbors[state] else state
+
+    def action_cost(self, s, action, s1):
+        """The distance (cost) to go from s to s1."""
+        return self.map.distances[s, s1]
+
+    def h(self, node):
+        "Straight-line distance between state and the goal."
+        locs = self.map.locations
+        return straight_line_distance(locs[node.state], locs[self.goal])
+
+
+def straight_line_distance(A, B):
+    "Straight-line distance between two points."
+    return sum(abs(a - b)**2 for (a, b) in zip(A, B)) ** 0.5
+
+class Map:
+    """A map of places in a 2D world: a graph with vertexes and links between them.
+    In `Map(links, locations)`, `links` can be either [(v1, v2)...] pairs,
+    or a {(v1, v2): distance...} dict. Optional `locations` can be {v1: (x, y)}
+    If `directed=False` then for every (v1, v2) link, we add a (v2, v1) link."""
+
+    def __init__(self, links, locations=None, directed=False):
+        if not hasattr(links, 'items'): # Distances are 1 by default
+            links = {link: 1 for link in links}
+        if not directed:
+            for (v1, v2) in list(links):
+                links[v2, v1] = links[v1, v2]
+        self.distances = links
+        self.neighbors = multimap(links)
+        self.locations = locations or defaultdict(lambda: (0, 0))
+
+
