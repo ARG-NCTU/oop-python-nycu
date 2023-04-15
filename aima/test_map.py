6311b7a73895fec159405c1e4925804992fa5f7d
hchengwang <hchengwang@gmail.com>
2023-04-15 11:43:35 +0800

    add route problem and aima pytest

---
+++ aima/test_map.py
@@ -0,0 +1,33 @@
+import matplotlib.pyplot as plt
+import random
+import heapq
+import math
+import sys
+from collections import defaultdict, deque, Counter
+from itertools import combinations
+import sys
+sys.path.append('../utils')
+from search import * 
+
+def test_map():
+    romania = Map(
+    {('O', 'Z'):  71, ('O', 'S'): 151, ('A', 'Z'): 75, ('A', 'S'): 140, ('A', 'T'): 118,
+     ('L', 'T'): 111, ('L', 'M'):  70, ('D', 'M'): 75, ('C', 'D'): 120, ('C', 'R'): 146,
+     ('C', 'P'): 138, ('R', 'S'):  80, ('F', 'S'): 99, ('B', 'F'): 211, ('B', 'P'): 101,
+     ('B', 'G'):  90, ('B', 'U'):  85, ('H', 'U'): 98, ('E', 'H'):  86, ('U', 'V'): 142,
+     ('I', 'V'):  92, ('I', 'N'):  87, ('P', 'R'): 97},
+    {'A': ( 76, 497), 'B': (400, 327), 'C': (246, 285), 'D': (160, 296), 'E': (558, 294),
+     'F': (285, 460), 'G': (368, 257), 'H': (548, 355), 'I': (488, 535), 'L': (162, 379),
+     'M': (160, 343), 'N': (407, 561), 'O': (117, 580), 'P': (311, 372), 'R': (227, 412),
+     'S': (187, 463), 'T': ( 83, 414), 'U': (471, 363), 'V': (535, 473), 'Z': (92, 539)})
+
+
+    r0 = RouteProblem('A', 'A', map=romania)
+    r1 = RouteProblem('A', 'B', map=romania)
+    r2 = RouteProblem('N', 'L', map=romania)
+    r3 = RouteProblem('E', 'T', map=romania)
+    r4 = RouteProblem('O', 'M', map=romania)
+
+    assert str(path_states(breadth_first_search(r1))) == "['A', 'S', 'F', 'B']"
+    assert str(path_states(depth_first_recursive_search(r1))) == \
+        "['A', 'Z', 'O', 'S', 'R', 'C', 'P', 'B']"
