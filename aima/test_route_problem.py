8bf71e28c793ea35f622874cd254a6a7f30d5fc5
hchengwang <hchengwang@gmail.com>
2023-04-15 15:57:15 +0800

    revise pytest

--- aima/test_route_problem.py
+++ aima/test_route_problem.py
@@ -10,7 +10,8 @@ import sys
 sys.path.append('../utils')
 from search import * 
 
-def test_map():
+def get_romania():
+
     romania = Map(
     {('O', 'Z'):  71, ('O', 'S'): 151, ('A', 'Z'): 75, ('A', 'S'): 140, ('A', 'T'): 118,
      ('L', 'T'): 111, ('L', 'M'):  70, ('D', 'M'): 75, ('C', 'D'): 120, ('C', 'R'): 146,
@@ -22,10 +23,20 @@ def test_map():
      'M': (160, 343), 'N': (407, 561), 'O': (117, 580), 'P': (311, 372), 'R': (227, 412),
      'S': (187, 463), 'T': ( 83, 414), 'U': (471, 363), 'V': (535, 473), 'Z': (92, 539)})
 
+    return romania
+
+def test_map():
+
+    romania = get_romania()
+
     assert str(romania.neighbors['A']) == "['Z', 'S', 'T']"
     assert romania.distances['A', 'Z'] == 75
     assert sldistance(romania.locations['A'], romania.locations['B']) == \
         pytest.approx(366, abs=0.5)
+
+def test_route_problem():
+
+    romania = get_romania()
 
     r0 = RouteProblem('A', 'A', map=romania)
     r1 = RouteProblem('A', 'B', map=romania)
@@ -39,6 +50,16 @@ def test_map():
     assert r1.result('A', 'S') == 'S'
     assert r1.result('A', 'B') == 'A'
     assert r1.action_cost('A', 'S', 'S') == 140
+
+def test_expand():
+
+    romania = get_romania()
+    r1 = RouteProblem('A', 'B', map=romania)
+
+def test_search():
+
+    romania = get_romania()
+    r1 = RouteProblem('A', 'B', map=romania)
 
     assert str(path_states(breadth_first_search(r1))) == "['A', 'S', 'F', 'B']"
     assert str(path_states(depth_first_recursive_search(r1))) == \
