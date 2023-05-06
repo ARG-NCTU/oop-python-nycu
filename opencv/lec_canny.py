824ba1afae2d591ceec5ccfbe85c6c1b650b8ff4
hchengwang <hchengwang@gmail.com>
2023-05-07 00:48:55 +0800

    add color lines example

--- opencv/lec_canny.py
+++ opencv/lec_canny.py
@@ -3,6 +3,7 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 img = cv2.imread('../images/duckietown.jpg', 0)
+#img = cv2.imread('../images/curve-right.jpg', 0)
 edges = cv2.Canny(img, 100, 200)
 
 plt.subplot(121),plt.imshow(img,cmap='gray')
