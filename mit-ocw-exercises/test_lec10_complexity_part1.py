b3a140a217b86ea19158b714b885267d9c51894c
Chengzong Li <chengzongli.ee10@nycu.edu.tw>
2023-09-25 14:11:06 +0800

    add test for lec10

--- mit-ocw-exercises/test_lec10_complexity_part1.py
+++ mit-ocw-exercises/test_lec10_complexity_part1.py
@@ -1,6 +1,11 @@
 import lec10_complexity_part1 as lec10
 import pytest
 
+# create a fixture for the empty list
+@pytest.fixture
+def empty_list():
+    return []
+
 # create a fixture for the list of integers
 @pytest.fixture
 def sample_data():
@@ -15,6 +20,10 @@ def test_linear_search():
 def test_linear_search_int_list(sample_data):
     assert lec10.linear_search(sample_data, 3) == True 
     assert lec10.linear_search(sample_data, 6) == False
+
+# test linear search with empty list
+def test_linear_search_empty_list(empty_list):
+    assert lec10.linear_search(empty_list, 3) == False
 
 # create a list of integers, with parameter n for number of elements
 def int_list(n):
@@ -32,6 +41,12 @@ def int_list_factory():
         return list(range(n))
     return _int_list
 
+# Wrong example
+# # create a fixture factory that takes a parameter n for number of elements
+# @pytest.fixture
+# def int_list_factory(n: int):
+#     return list(range(n))
+
 def test_linear_search_int_list_factory(int_list_factory):
     assert lec10.linear_search(int_list_factory(10), 4) == True
     assert lec10.linear_search(int_list_factory(10000), 4) == True
@@ -45,3 +60,9 @@ def num_elements(request):
 def test_linear_search_int_list_factory_enum(int_list_factory, num_elements):
     assert lec10.linear_search(int_list_factory(num_elements), 4) == True
 
+# test intersection
+def test_intersect():
+    assert lec10.intersect([1, 2, 3], [2, 3, 4]) == [2, 3]
+    assert lec10.intersect([1, 2, 3], [4, 5, 6]) == []
+    assert lec10.intersect([1, 2, 3], []) == []
+    assert lec10.intersect([], [1, 2, 3]) == []
