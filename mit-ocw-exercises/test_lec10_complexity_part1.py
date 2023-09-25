235ce181ebc607cbe43e05c6f1c42ac9d5ce09f9
Chengzong Li <chengzongli.ee10@nycu.edu.tw>
2023-09-26 01:53:43 +0800

    add some test to built-in function complexity test and calculate elapsed time

--- mit-ocw-exercises/test_lec10_complexity_part1.py
+++ mit-ocw-exercises/test_lec10_complexity_part1.py
@@ -1,5 +1,7 @@
 import lec10_complexity_part1 as lec10
 import pytest
+import time
+import random
 
 # create a fixture for the empty list
 @pytest.fixture
@@ -66,3 +68,98 @@ def test_intersect():
     assert lec10.intersect([1, 2, 3], [4, 5, 6]) == []
     assert lec10.intersect([1, 2, 3], []) == []
     assert lec10.intersect([], [1, 2, 3]) == []
+
+
+# create a fixture factory that takes a parameter n for number of elements, and shuffles the list
+@pytest.fixture
+def int_list_factory_shuffled():
+    def _int_list(n):
+        data = list(range(n))
+        random.shuffle(data)
+        return data
+    return _int_list
+
+# create a fixture for num_elements with parameters 1000, 10000, 100000, 1000000, 10000000
+@pytest.fixture(params=[1000, 10000, 100000, 1000000, 10000000])
+def num_elements(request):
+    return request.param
+
+# run the test with the following command to show the time taken for each test
+# pytest test_lec10_complexity_part1.py -s
+
+# test linear search with both shuffled and sorted list and calculate time
+def test_linear_search_int_list_factory_shuffled(int_list_factory_shuffled, num_elements):
+    # Create list spends some time therefore we create it before the timer
+    shuffled_list = int_list_factory_shuffled(num_elements)
+    start = time.time()
+    assert lec10.linear_search(shuffled_list, 4 * num_elements // 10) == True
+    end = time.time()
+    # print the result using exponential notation
+    print(f'Time taken for linear search with {num_elements: 10d} elements shuffled list: {end-start:.2e}')
+
+def test_linear_search_int_list_factory_sorted(int_list_factory, num_elements):
+    # Create list spends some time therefore we create it before the timer
+    sorted_list = int_list_factory(num_elements)
+    start = time.time()
+    assert lec10.linear_search(sorted_list, 4 * num_elements // 10) == True
+    end = time.time()
+    # print the result using exponential notation
+    print(f'Time taken for linear search with {num_elements: 10d} elements sorted list: {end-start:.2e}')
+
+# test built-in 'in' operator for shuffled list and calculate time
+def test_in_operator_int_list_factory_shuffled(int_list_factory_shuffled, num_elements):
+    # Create list spends some time therefore we create it before the timer
+    shuffled_list = int_list_factory_shuffled(num_elements)
+    start = time.time()
+    assert 4 * num_elements // 10 in shuffled_list
+    end = time.time()
+    # print the result using exponential notation
+    print(f'Time taken for \'in\' operator with {num_elements: 10d} elements shuffled list: {end-start:.2e}')
+
+# test built-in 'in' operator for list and calculate time
+def test_in_operator_int_list_factory(int_list_factory, num_elements):
+    # Create list spends some time therefore we create it before the timer
+    sorted_list = int_list_factory(num_elements)
+    start = time.time()
+    assert 4 * num_elements // 10 in sorted_list
+    end = time.time()
+    # print the result using exponential notation
+    print(f'Time taken for \'in\' operator with {num_elements: 10d} elements sorted list: {end-start:.2e}')
+
+
+# create a fixture factory that takes a parameter n for number of elements, and returns a set   
+@pytest.fixture
+def int_set_factory(int_list_factory):
+    def _int_set(n):
+        return set(int_list_factory(n))
+    return _int_set
+
+# set uses a hashtable as its underlying data structure, so it has the O(1) membership checking
+# test built-in 'set' container and calculate time
+def test_in_operator_int_set_factory(int_set_factory, num_elements):
+    # Create set spends some time therefore we create it before the timer
+    target_set = int_set_factory(num_elements)
+    start = time.time()
+    assert 4 * num_elements // 10 in target_set
+    end = time.time()
+    # print the result using exponential notation
+    print(f'Time taken for \'set\' container with {num_elements: 10d} elements: {end-start:.2e}')
+
+
+# create a fixture factory that takes a parameter n for number of elements, and returns a dict   
+@pytest.fixture
+def int_dict_factory(int_list_factory):
+    def _int_dict(n):
+        return {x: None for x in int_list_factory(n)}
+    return _int_dict
+
+# dict also uses a hashtable as its underlying data structure, so it has the O(1) membership checking
+# test built-in 'dict' container and calculate time
+def test_in_operator_int_dict_factory(int_dict_factory, num_elements):
+    # Create dict spends some time therefore we create it before the timer
+    target_dict = int_dict_factory(num_elements)
+    start = time.time()
+    assert 4 * num_elements // 10 in target_dict
+    end = time.time()
+    # print the result using exponential notation
+    print(f'Time taken for \'dict\' container with {num_elements: 10d} elements: {end-start:.2e}')
