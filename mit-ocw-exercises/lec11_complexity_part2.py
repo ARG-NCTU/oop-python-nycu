903004eae2f57b7e26658adcb7c4456d6f64b61c
ptst1110 <ptst1110.ee10@nycu.edu.tw>
2023-10-07 20:59:37 +0800

    add plot_lec11_complexity_part2.py

--- mit-ocw-exercises/lec11_complexity_part2.py
+++ mit-ocw-exercises/lec11_complexity_part2.py
@@ -47,26 +47,3 @@ def genSubsets(L):
 testSet = [1,2,3,4]
 print(genSubsets(testSet))
 
-
-import matplotlib.pyplot as plt
-import time
-
-testSet = []
-for i in range(20):
-    testSet.append(i)
-
-n_values = []
-times = []
-
-for n in range(1, len(testSet)+1):
-    start_time = time.time()
-    genSubsets(testSet[:n])
-    end_time = time.time()
-    n_values.append(n)
-    times.append(end_time - start_time)
-
-plt.plot(n_values, times)
-plt.xlabel('n')
-plt.ylabel('Time (s)')
-plt.title('Time Complexity of genSubsets')
-plt.savefig('complexity of exponential.png')
