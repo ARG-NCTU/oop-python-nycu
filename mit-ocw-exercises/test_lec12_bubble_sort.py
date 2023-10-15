0b76569f45ce3c4c41517082791a37ee777d3cbd
hchengwang <hchengwang@gmail.com>
2023-10-16 01:33:49 +0800

    give bubble sort 1000 element in list

--- mit-ocw-exercises/test_lec12_bubble_sort.py
+++ mit-ocw-exercises/test_lec12_bubble_sort.py
@@ -1,8 +1,11 @@
+import random
 
 from lec12_sorting import *
 
-testList = [1,3,5,7,2,6,25,18,13]
+#testList = [1,3,5,7,2,6,25,18,13]
 
-print('')
-print(bubble_sort(testList))
+# create a list with 1000 random numbers
+testList = [random.randint(0, 1000) for i in range(1000)]
+
+bubble_sort(testList)
 print(testList)
