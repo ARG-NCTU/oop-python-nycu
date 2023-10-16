e3c5258f778d676f100a0eb81d6fd207b08ed518
Peter <73233515+peter921123@users.noreply.github.com>
2023-10-16 12:47:46 +0800

    Update test_lec12_sorting.py
    Add pytest code.

--- mit-ocw-exercises/test_lec12_sorting.py
+++ mit-ocw-exercises/test_lec12_sorting.py
@@ -1,6 +1,7 @@
 from lec12_sorting import *
 import pytest
-
+import time
+import random
 # Test bubble_sort
 def test_bubble_sort():
     testList = [1,3,5,7,2,6,25,18,13]
@@ -14,7 +15,8 @@ def test_bubble_sort():
 def test_selection_sort():
 
     testList = [1,3,5,7,2,6,25,18,13]
-           
+
+    assert bubble_sort(testList) == [1,2,3,5,6,7,13,18,25]
     print('')
     print(selection_sort(testList))
     print(testList)
@@ -23,7 +25,23 @@ def test_selection_sort():
 def test_merge_sort():
 
     testList = [1,3,5,7,2,6,25,18,13]
-    
-    #print('')
-    #print(merge_sort(testList))
 
+    assert merge_sort(testList) == [1,2,3,5,6,7,13,18,25]
+    print('')
+    print(merge_sort(testList))
+
+def diff_in_time():
+    sorting_methods = ['selectiom sort', 'bubble sort', 'merge sort'] 
+    sorting_methods_func = [selection_sort, bubble_sort, merge_sort]
+    test_list = []
+    for j in (100000):
+        a = random.randrange(100000)
+        test_list.append(a)
+
+    for i in (3):
+        test = test_list
+        start_time = time.time()
+        test = sorting_methods_func[i](test)
+        end_time = time.time()
+        print(f'The time that {sorting_methods[i]} cost is {end_time - start_time}')
+        
