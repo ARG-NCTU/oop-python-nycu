cc507b3a758fd740e979fb854fe2df2a92e334fc
hchengwang <hchengwang@gmail.com>
2024-03-04 13:49:17 +0800

    update lec6 test

--- tests/test_lec6_recursion_dictionaries.py
+++ tests/test_lec6_recursion_dictionaries.py
@@ -8,9 +8,18 @@ def test_fib():
     assert lec6.fib(0) == 1
     assert lec6.fib(1) == 1
     assert lec6.fib(2) == 2
+    assert lec6.fib(3) == 3
+    assert lec6.fib(4) == 5
+    assert lec6.fib(5) == 8
+    assert lec6.fib(6) == 13
+    assert lec6.fib(7) == 21
+    assert lec6.fib(8) == 34
+    assert lec6.fib(9) == 55
+    assert lec6.fib(10) == 89
 
-# please write a test for isPalindrome function
-def test_isPalindrome():
-    assert lec6.isPalindrome('eve') == True
-    assert lec6.isPalindrome('Able was I, ere I saw Elba') == True
-    assert lec6.isPalindrome('Is this a palindrome') == False
+
+# please write a test for is_palindrome function
+def test_is_palindrome():
+    assert lec6.is_palindrome('eve') == True
+    assert lec6.is_palindrome('Able was I, ere I saw Elba') == True
+    assert lec6.is_palindrome('Is this a palindrome') == False
