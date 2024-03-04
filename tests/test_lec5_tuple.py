7f373c0a717f817cc6408e657dad81ad3be099fb
reb33340 <160496185+reb33340@users.noreply.github.com>
2024-03-04 15:19:01 +0800

    Update test_lec5_tuple.py
    Are we required to commit only the testing code, or do we need to commit anything else?
    Sorry, I don't quite understand.

--- tests/test_lec5_tuple.py
+++ tests/test_lec5_tuple.py
@@ -11,4 +11,18 @@ def test_quotient_and_remainder():
     assert lec5.quotient_and_remainder(20, 10) == (2, 0)
 
 
+def test_sum_elem_method1():
+    assert sum_elem_method1([1, 2, 3, 4]) == 10
+    assert sum_elem_method1([-1, -2, -3, -4]) == -10
+    assert sum_elem_method1([13, 34, 0, 88, 88, 0, 9]) == 232
+    assert sum_elem_method1([5]) == 5
+    assert sum_elem_method1([]) == 0
+
+
+def test_sum_elem_method2():
+    assert sum_elem_method2([1, 2, 3, 4]) == 10
+    assert sum_elem_method2([-1, -2, -3, -4]) == -10
+    assert sum_elem_method2([13, 34, 0, 88, 88, 0, 9]) == 232
+    assert sum_elem_method2([5]) == 5
+    assert sum_elem_method2([]) == 0
 
