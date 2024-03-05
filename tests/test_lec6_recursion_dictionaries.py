f38f61af909048f5644d61079b278db072022a3e
cookieeikooc <160454617+cookieeikooc@users.noreply.github.com>
2024-03-05 15:51:15 +0800

    Update test_lec6_recursion_dictionaries.py

--- tests/test_lec6_recursion_dictionaries.py
+++ tests/test_lec6_recursion_dictionaries.py
@@ -24,3 +24,57 @@ def test_is_palindrome():
     assert lec6.is_palindrome('Able was I, ere I saw Elba') == True
     assert lec6.is_palindrome('Is this a palindrome') == False
     assert lec6.is_palindrome('eillie') == True
+
+def test_most_common_words():
+    she_loves_you = ['she', 'loves', 'you', 'yeah', 'yeah', 
+    'yeah','she', 'loves', 'you', 'yeah', 'yeah', 'yeah',
+    'she', 'loves', 'you', 'yeah', 'yeah', 'yeah',
+    
+    'you', 'think', "you've", 'lost', 'your', 'love',
+    'well', 'i', 'saw', 'her', 'yesterday-yi-yay',
+    "it's", 'you', "she's", 'thinking', 'of',
+    'and', 'she', 'told', 'me', 'what', 'to', 'say-yi-yay',
+    
+    'she', 'says', 'she', 'loves', 'you',
+    'and', 'you', 'know', 'that', "can't", 'be', 'bad',
+    'yes', 'she', 'loves', 'you',
+    'and', 'you', 'know', 'you', 'should', 'be', 'glad',
+    
+    'she', 'said', 'you', 'hurt', 'her', 'so',
+    'she', 'almost', 'lost', 'her', 'mind',
+    'and', 'now', 'she', 'says', 'she', 'knows',
+    "you're", 'not', 'the', 'hurting', 'kind',
+    
+    'she', 'says', 'she', 'loves', 'you',
+    'and', 'you', 'know', 'that', "can't", 'be', 'bad',
+    'yes', 'she', 'loves', 'you',
+    'and', 'you', 'know', 'you', 'should', 'be', 'glad',
+    
+    'oo', 'she', 'loves', 'you', 'yeah', 'yeah', 'yeah',
+    'she', 'loves', 'you', 'yeah', 'yeah', 'yeah',
+    'with', 'a', 'love', 'like', 'that',
+    'you', 'know', 'you', 'should', 'be', 'glad',
+    
+    'you', 'know', "it's", 'up', 'to', 'you',
+    'i', 'think', "it's", 'only', 'fair',
+    'pride', 'can', 'hurt', 'you', 'too',
+    'pologize', 'to', 'her',
+    
+    'Because', 'she', 'loves', 'you',
+    'and', 'you', 'know', 'that', "can't", 'be', 'bad',
+    'Yes', 'she', 'loves', 'you',
+    'and', 'you', 'know', 'you', 'should', 'be', 'glad',
+    
+    'oo', 'she', 'loves', 'you', 'yeah', 'yeah', 'yeah',
+    'she', 'loves', 'you', 'yeah', 'yeah', 'yeah',
+    'with', 'a', 'love', 'like', 'that',
+    'you', 'know', 'you', 'should', 'be', 'glad',
+    'with', 'a', 'love', 'like', 'that',
+    'you', 'know', 'you', 'should', 'be', 'glad',
+    'with', 'a', 'love', 'like', 'that',
+    'you', 'know', 'you', 'should', 'be', 'glad',
+    'yeah', 'yeah', 'yeah',
+    'yeah', 'yeah', 'yeah', 'yeah'
+    ]
+    beatles = lyrics_to_frequencies(she_loves_you)
+    assert lec6.most_common_words(beatles) == ('yeah', 28)
