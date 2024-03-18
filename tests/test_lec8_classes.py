3192203acd2566cec5b1a8a547098c1dbdd188c8
lonea2005 <rayrayppp123@gmail.com>
2024-03-18 14:01:14 +0800

    test_lec8

--- tests/test_lec8_classes.py
+++ tests/test_lec8_classes.py
@@ -1,6 +1,7 @@
 import add_path
 import mit_ocw_exercises.lec8_classes as lc
 
+
 def test_coordinate():
     c = lc.Coordinate(3, 4)
     origin = lc.Coordinate(0,0)
@@ -19,4 +20,12 @@ def test_intset():
     s.remove(3)
     assert not s.member(3)
     assert s.member(4)
+
+def test_16_coordinate():
+    a=lc.Coordinate(5,12)
+    o=lc.Coordinate(0,0)
+    assert a.x==5
+    assert a.y==12
+    assert a.distance(o) == 13
+    assert o.distance(a) == 13
 
