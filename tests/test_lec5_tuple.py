1eb7e80323d569bac5b742f584e3fe1ed43da7e5
comet1233 <comet112511092.ee12@nycu.edu.tw>
2024-03-18 14:47:35 +0800

    test_lec5_tuple.py

--- tests/test_lec5_tuple.py
+++ tests/test_lec5_tuple.py
@@ -9,6 +9,10 @@ def test_quotient_and_remainder():
     assert lec5.quotient_and_remainder(20, 8) == (2, 4)
     assert lec5.quotient_and_remainder(20, 9) == (2, 2)
     assert lec5.quotient_and_remainder(20, 10) == (2, 0)
-
+    assert lec5.quotient_and_remainder(20, 11) == (1, 9)
+    assert lec5.quotient_and_remainder(20, 12) == (1, 8)
+    assert lec5.quotient_and_remainder(20, 13) == (1, 7)
+    assert lec5.quotient_and_remainder(20, 14) == (1, 6)
+    assert lec5.quotient_and_remainder(20, 15) == (1, 5)
 
 
