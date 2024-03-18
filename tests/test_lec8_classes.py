33be51f57094880535ccc2536bdc0a1c872e07c1
cookieeikooc <bingyuygnib@gmail.com>
2024-03-18 14:36:06 +0800

    group3 update test_lec8_classes.py

--- tests/test_lec8_classes.py
+++ tests/test_lec8_classes.py
@@ -6,6 +6,7 @@ def test_coordinate():
     origin = lc.Coordinate(0,0)
     assert c.x == 3
     assert c.y == 4
+    assert c.str == "<3, 4>" #3 updated str method
     assert c.distance(origin) == 5
     assert origin.distance(c) == 5
 
@@ -20,3 +21,12 @@ def test_intset():
     assert not s.member(3)
     assert s.member(4)
 
+def test_3_Fraction():
+    p = lc.Fraction(7, 8)
+    assert p.num == 7
+    assert p.denom == 8
+    assert p.__str__() == '7/8'
+    assert p.__add__(lc.Fraction(1, 2)).__str__() == '15/16'
+    assert p.__sub__(lc.Fraction(1, 2)).__str__() == '3/8'
+    assert p.__float__() == 0.875
+    assert p.inverse().__str__() == '8/7'
