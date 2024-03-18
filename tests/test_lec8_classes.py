0e9ad0fee6929fa6ff9d36297a3d70d3666ef209
xianli <lix24773@gmail.com>
2024-03-18 16:01:56 +0800

    add test l8

--- tests/test_lec8_classes.py
+++ tests/test_lec8_classes.py
@@ -1,69 +1,21 @@
 import add_path
 import mit_ocw_exercises.lec8_classes as lc
 
-def test_coordinate():
-    c = lc.Coordinate(3, 4)
+def test_9_coordinate():
+    c = lc.Coordinate(6,8)
     origin = lc.Coordinate(0,0)
-    assert c.x == 3
-    assert c.y == 4
-    assert c.distance(origin) == 5
-    assert origin.distance(c) == 5
+    assert c.x == 6
+    assert c.y == 8
+    assert c.distance(origin) == 10
+    assert origin.distance(c) == 10
 
-def test_intset():
+def test_9_intset():
     s = lc.intSet()
-    s.insert(3)
-    s.insert(4)
-    assert s.member(3)
-    assert s.member(4)
-    assert not s.member(5)
-    s.remove(3)
-    assert not s.member(3)
-    assert s.member(4)
-    
-def test_17_coordinate():
-    c = lc.Coordinate(3, 4)
-    origin = lc.Coordinate(0,0)
-    assert c.x == 3
-    assert c.y == 4
-    assert c.distance(origin) == 5
-    assert origin.distance(c) == 5
-
-def test_17_intset():
-    s = lc.intSet()
-    s.insert(3)
-    s.insert(4)
-    assert s.member(3)
-    assert s.member(4)
-    assert not s.member(5)
-    s.remove(3)
-    assert not s.member(3)
-    assert s.member(4)
-    
-
-def test_6_coordinate():
-    c = lc.Coordinate(5, 12)
-    origin = lc.Coordinate(0,0)
-    assert c.x == 5
-    assert c.y == 12
-    assert c.distance(origin) == 13
-    assert origin.distance(c) == 13
-
-def test_17_coordinate():
-    c = lc.Coordinate(3, 4)
-    origin = lc.Coordinate(0,0)
-    assert c.x == 3
-    assert c.y == 4
-    assert c.distance(origin) == 5
-    assert origin.distance(c) == 5
-
-def test_17_intset():
-    s = lc.intSet()
-    s.insert(3)
-    s.insert(4)
-    assert s.member(3)
-    assert s.member(4)
-    assert not s.member(5)
-    s.remove(3)
-    assert not s.member(3)
-    assert s.member(4)
+    s.insert(8)
+    s.insert(7)
+    assert s.member(8)
+    assert s.member(7)
+    s.remove(8)
+    assert not s.member(8)
+    assert s.member(7)
 
