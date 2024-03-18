fadce5959aeddf475cd5c09b94256e1abdead6ab
cookieeikooc <bingyuygnib@gmail.com>
2024-03-18 17:15:44 +0800

    Group3 update test lec8 classes.py-2

--- tests/test_lec8_classes.py
+++ tests/test_lec8_classes.py
@@ -1,12 +1,12 @@
 import add_path
 import mit_ocw_exercises.lec8_classes as lc
+import pytest
 
 def test_coordinate():
     c = lc.Coordinate(3, 4)
     origin = lc.Coordinate(0,0)
     assert c.x == 3
     assert c.y == 4
-    assert c.str == "<3, 4>" #3 updated str method
     assert c.distance(origin) == 5
     assert origin.distance(c) == 5
 
@@ -22,11 +22,24 @@ def test_intset():
     assert s.member(4)
 
 def test_3_Fraction():
-    p = lc.Fraction(7, 8)
-    assert p.num == 7
-    assert p.denom == 8
-    assert p.__str__() == '7/8'
-    assert p.__add__(lc.Fraction(1, 2)).__str__() == '15/16'
-    assert p.__sub__(lc.Fraction(1, 2)).__str__() == '3/8'
-    assert p.__float__() == 0.875
-    assert p.inverse().__str__() == '8/7'
+    p = lc.Fraction(2, 5)
+    assert p.num == 2
+    assert p.denom == 5
+    q = lc.Fraction(1, 5)
+    assert q.num == 1
+    assert q.denom == 5
+    a1 = p + q
+    assert str(a1) == "15/25"
+    a2 = p - q
+    assert str(a2) == "5/25"
+    assert lc.Fraction.__float__(p) == 0.4
+    assert str(p.inverse()) == "5/2"
+
+if __name__ == "__main__":
+    test_coordinate()
+    print("test_coordinate passed")
+    test_intset()
+    print("test_intset passed")
+    test_3_Fraction()
+    print("test_3_Fraction passed")
+    print("All tests passed")
