09fe683e3d9e0d303973fc1efc52dc38bffaf842
JasonChen112101018 <160493897+JasonChen112101018@users.noreply.github.com>
2024-03-18 14:14:45 +0800

    Update test_lec8_classes.py
    Add a new test_17_coordinate

--- tests/test_lec8_classes.py
+++ tests/test_lec8_classes.py
@@ -1,13 +1,21 @@
 import add_path
 import mit_ocw_exercises.lec8_classes as lc
 
-def test_17_coordinate():
+def test_coordinate():
     c = lc.Coordinate(3, 4)
     origin = lc.Coordinate(0,0)
     assert c.x == 3
     assert c.y == 4
     assert c.distance(origin) == 5
     assert origin.distance(c) == 5
+
+def test_17_coordinate():
+    c = lc.Coordinate(5,12)
+    origin = lc.Coordinate(0,0)
+    assert c.x == 5
+    assert c.y == 12
+    assert c.distance(origin) == 13
+    assert origin.distance(c) == 13
 
 def test_intset():
     s = lc.intSet()
