4407da1145990541f918ed38d17728bb073955e0
kevinchung1203 <kevinchung.ee12@nycu.edu.tw>
2024-03-25 14:12:03 +0800

    tests/test_lec9_inheritance.py

--- tests/test_lec9_inheritance.py
+++ tests/test_lec9_inheritance.py
@@ -10,3 +10,17 @@ def test_animal():
     print(a)
     assert a.get_name() == "fluffy"
     assert a.get_age() == 4
+ 
+def test_03_cat
+    class Cat(Animal):
+    def speak(self):
+        print("meow")
+    def __str__(self):
+        return "cat:"+str(self.name)+":"+str(self.age)
+    
+    print("\n---- cat tests ----")
+    c = Cat(5)
+    c.set_name("Black")
+    assert c.get_age() == 5
+    assert c.get_name() == "Black"
+
