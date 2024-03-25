31793a5da6a32a4f54c20eb3ff600915ba8a68c9
Prophet-Wei <prophetwei.ee12@nycu.edu.tw>
2024-03-25 16:08:10 +0800

    lec9

--- tests/test_lec9_inheritance.py
+++ tests/test_lec9_inheritance.py
@@ -3,10 +3,10 @@ import mit_ocw_exercises.lec9_inheritance as inh
 #import pytest
 
 def test_group7_animal():
-    a = inh.Animal(7)
+    a = inh.Animal(69)
     print(a)
     print(a.get_age())
     a.set_name("cookie")
     print(a)
     assert a.get_name() == "cookie"
-    assert a.get_age() == 7
+    assert a.get_age() == 69
