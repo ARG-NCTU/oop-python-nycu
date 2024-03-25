5dc2ba174c8f49f650f51b29e2d370981647b133
Prophet-Wei <prophetwei.ee12@nycu.edu.tw>
2024-03-25 15:45:48 +0800

    update lec9

--- tests/test_lec9_inheritance.py
+++ tests/test_lec9_inheritance.py
@@ -12,10 +12,10 @@ def test_animal():
     assert a.get_age() == 4
 
 def test_7_animal():
-    a = inh.Animal(4)
+    a = inh.Animal(7)
     print(a)
     print(a.get_age())
-    a.set_name("fluffy")
+    a.set_name("cookie")
     print(a)
-    assert a.get_name() == "fluffy"
-    assert a.get_age() == 4
+    assert a.get_name() == "cookie"
+    assert a.get_age() == 7
