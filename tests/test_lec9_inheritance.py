8f7f6009ec7f8a95b90089cc59abf7089d47e648
comet1233 <comet112511092.ee12@nycu.edu.tw>
2024-03-25 14:04:24 +0800

    group6 lec9 testing code

--- tests/test_lec9_inheritance.py
+++ tests/test_lec9_inheritance.py
@@ -10,3 +10,13 @@ def test_animal():
     print(a)
     assert a.get_name() == "fluffy"
     assert a.get_age() == 4
+
+def test_6_cat():
+    c = inh.Cat(5)
+    print(c)
+    print(c.get_age())
+    c.set_name("fluffy")
+    print(c)
+    assert c.get_name() == "fluffy"
+    assert c.get_age() == 5
+
