0396eb0df8938b7b7c0599588db09de658b28dd6
Prophet-Wei <prophetwei.ee12@nycu.edu.tw>
2024-03-25 14:49:43 +0800

    update

--- tests/test_lec6_recursion_dictionaries.py
+++ tests/test_lec6_recursion_dictionaries.py
@@ -25,3 +25,23 @@ def test_is_palindrome():
     assert lec6.is_palindrome('Is this a palindrome') == False
     assert lec6.is_palindrome('eillie') == True
     assert lec6.is_palindrome('cookieliileikooc') == True
+
+def test_7_fib():
+    assert lec6.fib(0) == 1
+    assert lec6.fib(1) == 1
+    assert lec6.fib(2) == 2
+    assert lec6.fib(3) == 3
+    assert lec6.fib(4) == 5
+    assert lec6.fib(5) == 8
+    assert lec6.fib(6) == 13
+    assert lec6.fib(7) == 21
+    assert lec6.fib(8) == 34
+    assert lec6.fib(9) == 55
+    assert lec6.fib(10) == 89
+
+def test_7_is_palindrome():
+    assert lec6.is_palidrome('eve') == True
+    assert lec6.is_palindrome('Able was I, ere I saw Elba') == True
+    assert lec6.is_palindrome('Is this a palindrome') == False
+    assert lec6.is_palindrome('eillie') == True
+    assert lec6.is_palindrome('cookieliileikooc') == True
--- tests/test_lec8_classes.py
+++ tests/test_lec8_classes.py
@@ -21,20 +21,20 @@ def test_intset():
     assert s.member(4)
 
 def test_7_coordinate():
-    c = lc.Coordinate(8,6)
+    c = lc.Coordinate(3,4)
     origin = lc.Coordinate(0,0)
-    assert c.x == 8
-    assert c.y == 6
-    assert c.distance(origin) == 10
-    assert origin.distance(c) == 10
+    assert c.x == 3
+    assert c.y == 4
+    assert c.distance(origin) == 5
+    assert origin.distance(c) == 5
 
 def test_7_intset():
     s = lc.intSet()
-    s.insert(8)
-    s.insert(6)
-    assert s.member(8)
-    assert s.member(6)
-    assert not s.member(10)
-    s.remove(8)
-    assert not s.member(8)
-    assert s.member(6)
+    s.insert(3)
+    s.insert(4)
+    assert s.member(3)
+    assert s.member(4)
+    assert not s.member(5)
+    s.remove(3)
+    assert not s.member(3)
+    assert s.member(4)
