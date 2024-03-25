0ae174b6223e317e8843a391cfc5cf796195aa52
s100179 <liyun0866@gmail.com>
2024-03-25 14:21:49 +0800

    group11_lec8

--- tests/test_lec8_classes.py
+++ tests/test_lec8_classes.py
@@ -20,3 +20,20 @@ def test_intset():
     assert not s.member(3)
     assert s.member(4)
 
+def test_11_coordinate():
+    c = lc.Coordinate(3, 4)
+    origin = lc.Coordinate(0,0)
+    assert c.x == 3
+    assert c.y == 4
+    assert c.distance(origin) == 5
+    assert origin.distance(c) == 5
+
+def test_11_intset():
+    s = lc.intSet()
+    s.insert(3)
+    s.insert(4)
+    assert s.member(3)
+    assert s.member(4)
+    assert not s.member(5)
+    s.remove(3)
+    assert not s.member(3)
