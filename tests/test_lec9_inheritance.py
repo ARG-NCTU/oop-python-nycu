3e054a053679e813f86245a9769d971841fa72de
chienyichieh <ilainchien.ee10@nycu.edu.tw>
2024-03-25 14:10:44 +0800

    group12 lect9_testcode

--- tests/test_lec9_inheritance.py
+++ tests/test_lec9_inheritance.py
@@ -10,3 +10,18 @@ def test_animal():
     print(a)
     assert a.get_name() == "fluffy"
     assert a.get_age() == 4
+    a.set_age(5)
+    assert a.get_age() == 5
+    
+def test_cat():
+    c = inh.Cat(5)
+    print(c)
+    print(c.get_age())
+    c.set_name("fluffy")
+    print(c)
+    assert c.get_name() == "fluffy"
+    assert c.get_age() == 5
+    c.set_age(6)
+    assert c.get_age() == 6
+
+
