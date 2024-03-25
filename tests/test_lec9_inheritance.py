aec20beb9f4647e179bdb1de7e6b99e9a69f4ad4
ChihHsunLin102 <twsam1209@gmail.com>
2024-03-25 13:59:39 +0800

    Group 5 Lecture 9 Test Code

--- tests/test_lec9_inheritance.py
+++ tests/test_lec9_inheritance.py
@@ -2,7 +2,7 @@ import add_path
 import mit_ocw_exercises.lec9_inheritance as inh
 import pytest
 
-def test_animal():
+def test_animal_5A():
     a = inh.Animal(4)
     print(a)
     print(a.get_age())
@@ -10,3 +10,11 @@ def test_animal():
     print(a)
     assert a.get_name() == "fluffy"
     assert a.get_age() == 4
+    a.set_name('outgoing')
+    assert a.get_name() == 'outgoing'
+    a.set_age(5)
+    assert a.get_age() == 5
+    assert a.__str__() == "animal:outgoing:5"
+
+
+
