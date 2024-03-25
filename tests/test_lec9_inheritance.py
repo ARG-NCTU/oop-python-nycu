19d229b1d8dd8fff447b786587b225b28a7627f7
molafish0712 <jerry112511196.ee12@nycu.edu.tw>
2024-03-25 15:03:21 +0800

    group 14 test_lec9_inheritance.py upload

--- tests/test_lec9_inheritance.py
+++ tests/test_lec9_inheritance.py
@@ -19,3 +19,15 @@ def test_13_animal():
     print(a)
     assert a.get_name() == "Yuyu"
     assert a.get_age() == 19
+
+def test_14_rabbit():
+    r1 = inh.Rabbit(3)
+    r1.set_name("fluffy")
+    r2 = inh.Rabbit(4)
+    r2.set_name("peter")
+    r3 = r1 + r2
+    r4 = r1 + r2
+    assert r1.get_name() == "fluffy"
+    assert r1.get_age() == 3
+    assert r1.get_species() == "Rabbit"
+    assert r3 == r4
