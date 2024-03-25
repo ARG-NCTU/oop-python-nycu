0c45494d7c31eb8ff3fb090927b8b0c187833f7b
lawxiuyong <xiuyong.ee12@nycu.edu.tw>
2024-03-25 19:20:29 +0800

    test_lec9_inheritance.py

--- tests/test_lec9_inheritance.py
+++ tests/test_lec9_inheritance.py
@@ -2,11 +2,11 @@ import add_path
 import mit_ocw_exercises.lec9_inheritance as inh
 import pytest
 
-def test_animal():
+def test_4_turtle():
     a = inh.Animal(4)
     print(a)
     print(a.get_age())
-    a.set_name("fluffy")
+    a.set_name("ninja")
     print(a)
-    assert a.get_name() == "fluffy"
+    assert a.get_name() == "ninja"
     assert a.get_age() == 4
