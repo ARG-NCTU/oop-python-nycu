0aca0e8996e98829568cfdabb7a8c82c34c72bdf
Prophet-Wei <prophetwei.ee12@nycu.edu.tw>
2024-03-25 16:51:09 +0800

    lec9

--- tests/test_lec9_inheritance.py
+++ tests/test_lec9_inheritance.py
@@ -1,8 +1,15 @@
 import add_path
 import mit_ocw_exercises.lec9_inheritance as inh
-#import pytest
+import pytest
 
 def test_group7_animal():
+    a = inh.Animal(4)
+    print(a)
+    print(a.get_age())
+    a.set_name("fluffy")
+    print(a)
+    assert a.get_name() == "fluffy"
+    assert a.get_age() == 4
     a = inh.Animal(69)
     print(a)
     print(a.get_age())
