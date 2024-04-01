a2b7e3dfe76f7961dc5f6d36cbc0a9454dc13b8f
lonea2005 <rayrayppp123@gmail.com>
2024-04-01 19:18:13 +0800

    test_lec9_inheritance update again

--- tests/test_lec9_inheritance.py
+++ tests/test_lec9_inheritance.py
@@ -11,4 +11,12 @@ def test_animal():
     assert a.get_name() == "fluffy"
     assert a.get_age() == 4
 
+def test_animal2():
+    a = inh.Animal(10)
+    print(a)
+    print(a.get_age())
+    a.set_name("furry")
+    print(a)
+    assert a.get_name() == "furry"
+    assert a.get_age() == 10
 
