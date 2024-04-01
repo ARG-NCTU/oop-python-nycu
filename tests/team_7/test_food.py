97feb4ebbd2dcffdeadfde72e96f0588b0110069
Prophet-Wei <prophetwei.ee12@nycu.edu.tw>
2024-04-01 15:07:00 +0800

    test_food

---
+++ tests/team_7/test_food.py
@@ -0,0 +1,19 @@
+import pytest
+import add_path
+from mit_ocw_data_science.lec2.menu import food
+
+@pytest.fixture
+def food():
+    return Food("taco", 180, 100)
+
+def test_get_value(food):
+    assert food.get_value() == 180
+
+def test_get_cost(food):
+    assert food.get_cost() == 100
+
+def test_density(food):
+    asser food.density() == 1.8
+
+def test_str(food):
+    assert str(food) == "taco: <180, 100>"
