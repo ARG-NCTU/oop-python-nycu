ae2f7d73ddfafcc511d259b96d1e9f47122b6a6f
molafish0712 <jerry112511196.ee12@nycu.edu.tw>
2024-04-01 13:58:20 +0800

    group 13 update test_food.py

--- tests/team_ta/test_food.py
+++ tests/team_ta/test_food.py
@@ -17,3 +17,10 @@ def test_density(food):
 
 def test_str(food):
     assert str(food) == "apple: <10, 50>"
+
+def test_13_food():
+    food = Food("molafish", 100, 60)
+    assert food.get_value() == 100
+    assert food.get_cost() == 60
+    assert food.density() == 1.6666666666666667
+    assert str(food) == "molafish: <100, 60>"
