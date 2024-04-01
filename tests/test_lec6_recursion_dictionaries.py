38daf524cb7c11dfe6fd480c8878f3d27f035b63
yummypenguin <timlin14012005.ee12@nycu.edu.tw>
2024-04-01 15:13:01 +0800

    Add fib_series test code to test_lec6_recursion_dictionaries.py

--- tests/test_lec6_recursion_dictionaries.py
+++ tests/test_lec6_recursion_dictionaries.py
@@ -198,3 +198,27 @@ def test_most_common_words():
     assert lec6.is_palindrome('cookieliileikooc') == True
 
 >>>>>>> group5_54
+
+
+import add_path
+import mit_ocw_exercises.lec6_recursion_dictionaries as lec6
+import pytest
+
+# please write a test for fib function
+def test_fib():
+    assert lec6.fib(0) == 1
+    assert lec6.fib(1) == 1
+    assert lec6.fib(2) == 2
+    assert lec6.fib(3) == 3
+    assert lec6.fib(4) == 5
+    assert lec6.fib(5) == 8
+    assert lec6.fib(6) == 13
+    assert lec6.fib(7) == 21
+    assert lec6.fib(8) == 34
+    assert lec6.fib(9) == 55
+    assert lec6.fib(10) == 89
+    assert lec6.fib(11) == 144
+    assert lec6.fib(12) == 233
+    assert lec6.fib(13) == 377
+    assert lec6.fib(14) == 610
+    assert lec6.fib(15) == 987
