f01f9438401805065e8730e5272530cebb0a6e99
maxwu1993 <161314915+maxwu1993@users.noreply.github.com>
2024-04-08 14:23:14 +0800

    Update test_food.py

--- tests/team_12/test_food.py
+++ tests/team_12/test_food.py
@@ -1,17 +1,19 @@
 import pytest
 import add_path
 from mit_ocw_data_science.lec2.menu import Food
+
 @pytest.fixture
 def food():
-	return Food("papaya",50 ,100)
-def test_get_value(food):
-	assert food.get_value()==50
-def test_get_cost(food):
-	assert food.get_cost()==100
-def test_density(food):
-	assert food.density()==0.5
-def test_str(food):
-	assert str(food) == "papaya: <50, 100>"
+    return Food("gauva", 20, 40)
 
-def test_neq(food):
-    assert food != Food("apple",50,100)
+def test_get_value(food):
+    assert food.get_value() == 20
+
+def test_get_cost(food):
+    assert food.get_cost() == 40
+
+def test_density(food):
+    assert food.density() == 0.5
+
+def test_str(food):
+    assert str(food) == "gauva: <20, 40>"
