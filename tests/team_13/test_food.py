56d53df554119db895ce03846fddc0381fb5e75c
molafish0712 <jerry112511196.ee12@nycu.edu.tw>
2024-04-10 23:22:49 +0800

    group_13 test_food.py upload

--- tests/team_13/test_food.py
+++ tests/team_13/test_food.py
@@ -3,10 +3,17 @@ import add_path
 from mit_ocw_data_science.lec2.menu import Food
 
 @pytest.fixture
-def test_13_food():
-    food = Food("molafish", 100, 50)
-    assert food.get_value() == 100
-    assert food.get_cost() == 50
-    assert food.density() == 2
-    assert str(food) == "molafish: <100, 50>"
+def food():
+    return Food("molafish", 20, 40)
 
+def test_get_value(food):
+    assert food.get_value() == 20
+
+def test_get_cost(food):
+    assert food.get_cost() == 40
+
+def test_density(food):
+    assert food.density() == 0.5
+
+def test_str(food):
+    assert str(food) == "molafish: <20, 40>"
