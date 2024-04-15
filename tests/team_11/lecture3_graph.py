4620b892d297abd7eb534db08b92ba78dc762106
kingblue5453 <lan.ee12@nycu.edu.tw>
2024-04-15 15:25:10 +0800

    lecture3_graph.py

--- tests/team_11/lecture3_graph.py
+++ tests/team_11/lecture3_graph.py
@@ -1,45 +1,126 @@
 class Node:
-    """Represents a node in a graph."""
+    """
+    Represents a node in a graph.
+
+    Attributes:
+        name (str): The name of the node.
+    """
+
     def __init__(self, name):
-        """Assumes name is a string."""
+        """
+        Initializes a Node with a name.
+
+        Args:
+            name (str): The name of the node.
+        """
         self.name = name
 
     def get_name(self):
+        """
+        Returns the name of the node.
+
+        Returns:
+            str: The name of the node.
+        """
         return self.name
 
     def __str__(self):
+        """
+        Returns the string representation of the node.
+
+        Returns:
+            str: The name of the node.
+        """
         return self.name
 
 
 class Edge:
-    """Represents a directed edge in a graph."""
+    """
+    Represents a directed edge in a graph.
+
+    Attributes:
+        src (Node): The source node of the edge.
+        dest (Node): The destination node of the edge.
+    """
+
     def __init__(self, src, dest):
-        """Assumes src and dest are nodes."""
+        """
+        Initializes an Edge with a source and destination node.
+
+        Args:
+            src (Node): The source node of the edge.
+            dest (Node): The destination node of the edge.
+        """
         self.src = src
         self.dest = dest
 
     def get_source(self):
+        """
+        Returns the source node of the edge.
+
+        Returns:
+            Node: The source node of the edge.
+        """
         return self.src
 
     def get_destination(self):
+        """
+        Returns the destination node of the edge.
+
+        Returns:
+            Node: The destination node of the edge.
+        """
         return self.dest
 
     def __str__(self):
+        """
+        Returns the string representation of the edge.
+
+        Returns:
+            str: The string representation of the edge in the format 'src->dest'.
+        """
         return self.src.get_name() + '->' + self.dest.get_name()
 
 
 class Digraph:
-    """Edges is a dict mapping each node to a list of its children."""
+    """
+    Represents a directed graph.
+
+    Attributes:
+        edges (dict): A dictionary mapping each node to a list of its children.
+    """
+
     def __init__(self):
+        """
+        Initializes a Digraph with an empty dictionary of edges.
+        """
         self.edges = {}
 
     def add_node(self, node):
+        """
+        Adds a node to the graph.
+
+        Args:
+            node (Node): The node to be added.
+
+        Raises:
+            ValueError: If the node already exists in the graph.
+        """
         if node in self.edges:
             raise ValueError('Duplicate node')
         else:
             self.edges[node] = []
 
     def add_edge(self, edge):
+        """
+        Adds an edge to the graph.
+
+        Args:
+            edge (Edge): The edge to be added.
+
+        Raises:
+            ValueError: If the source or destination node of the edge is not in the graph.
+        """
         src = edge.get_source()
         dest = edge.get_destination()
         if not (src in self.edges and dest in self.edges):
@@ -47,18 +128,54 @@ class Digraph:
         self.edges[src].append(dest)
 
     def children_of(self, node):
+        """
+        Returns the children of a node.
+
+        Args:
+            node (Node): The node to get the children of.
+
+        Returns:
+            list: The list of children nodes.
+        """
         return self.edges[node]
 
     def has_node(self, node):
+        """
+        Checks if a node is in the graph.
+
+        Args:
+            node (Node): The node to check.
+
+        Returns:
+            bool: True if the node is in the graph, False otherwise.
+        """
         return node in self.edges
 
     def get_node(self, name):
+        """
+        Returns the node with the given name.
+
+        Args:
+            name (str): The name of the node.
+
+        Returns:
+            Node: The node with the given name.
+
+        Raises:
+            NameError: If no node with the given name exists.
+        """
         for n in self.edges:
             if n.get_name() == name:
                 return n
         raise NameError(name)
 
     def __str__(self):
+        """
+        Returns the string representation of the graph.
+
+        Returns:
+            str: The string representation of the graph in the format 'src->dest'.
+        """
         result = ''
         for src in self.edges:
             for dest in self.edges[src]:
@@ -67,20 +184,52 @@ class Digraph:
 
 
 class Graph(Digraph):
-    """Represents a graph as a dictionary of nodes mapping"""
+    """
+    Represents a graph as a dictionary of nodes mapping.
+
+    Inherits from Digraph.
+    """
+
     def add_edge(self, edge):
+        """
+        Adds an edge to the graph.
+
+        Args:
+            edge (Edge): The edge to be added.
+
+        Overrides the add_edge method of Digraph to add a reverse edge as well.
+        """
         Digraph.add_edge(self, edge)
         rev = Edge(edge.get_destination(), edge.get_source())
         Digraph.add_edge(self, rev)
 
-class CityPlanner():
+
+class CityPlanner:
+    """
+    Represents a city planner.
+
+    Attributes:
+        print_queue (bool): Whether to print the queue or not.
+        g (list): The list of graphs.
+    """
 
     def __init__(self):
+        """
+        Initializes a CityPlanner with an empty list of graphs and print_queue set to True.
+        """
         self.print_queue = True
         self.g = []
 
     def print_path(self, path):
-        """Assumes path is a list of nodes"""
+        """
+        Returns the string representation of a path.
+
+        Args:
+            path (list): The path to print.
+
+        Returns:
+            str: The string representation of the path in the format 'node1->node2->...->nodeN'.
+        """
         result = ''
         for i in range(len(path)):
             result += str(path[i])
@@ -89,9 +238,20 @@ class CityPlanner():
         return result
 
     def DFS(self, graph, start, end, path, shortest, to_print=False):
-        """Assumes graph is a Digraph; start and end are nodes;
-           path and shortest are lists of nodes
-           Returns a shortest path from start to end in graph"""
+        """
+        Performs a depth-first search (DFS) on the graph.
+
+        Args:
+            graph (Digraph): The graph to perform the DFS on.
+            start (Node): The starting node.
+            end (Node): The ending node.
+            path (list): The current path.
+            shortest (list): The current shortest path.
+            to_print (bool, optional): Whether to print the current DFS path. Defaults to False.
+
+        Returns:
+            list: The shortest path from start to end in the graph.
+        """
         path = path + [start]
         if to_print:
             print('Current DFS path:', self.print_path(path))
@@ -101,7 +261,7 @@ class CityPlanner():
             if node not in path:  # avoid cycles
                 if shortest is None or len(path) < len(shortest):
                     new_path = self.DFS(graph, node, end, path, shortest,
-                                    to_print)
+                                        to_print)
                     if new_path is not None:
                         shortest = new_path
             elif to_print:
@@ -109,8 +269,18 @@ class CityPlanner():
         return shortest
 
     def BFS(self, graph, start, end, to_print=False):
-        """Assumes graph is a Digraph; start and end are nodes
-           Returns a shortest path from start to end in graph"""
+        """
+        Performs a breadth-first search (BFS) on the graph.
+
+        Args:
+            graph (Digraph): The graph to perform the BFS on.
+            start (Node): The starting node.
+            end (Node): The ending node.
+            to_print (bool, optional): Whether to print the current BFS path. Defaults to False.
+
+        Returns:
+            list: The shortest path from start to end in the graph.
+        """
         init_path = [start]
         path_queue = [init_path]
         while len(path_queue) != 0:
@@ -133,18 +303,44 @@ class CityPlanner():
         return None
 
     def shortest_path_bfs(self, graph, start, end, toPrint = False):
-        """Assumes graph is a Digraph; start and end are nodes
-           Returns a shortest path from start to end in graph"""
+        """
+        Returns the shortest path from start to end in the graph using BFS.
+
+        Args:
+            graph (Digraph): The graph to find the shortest path in.
+            start (Node): The starting node.
+            end (Node): The ending node.
+            toPrint (bool, optional): Whether to print the path. Defaults to False.
+
+        Returns:
+            list: The shortest path from start to end in the graph.
+        """
         return self.BFS(graph, start, end, toPrint)
 
-
     def shortest_path_dfs(self, graph, start, end, toPrint = False):
-        """Assumes graph is a Digraph; start and end are nodes
-           Returns a shortest path from start to end in graph"""
+        """
+        Returns the shortest path from start to end in the graph using DFS.
+
+        Args:
+            graph (Digraph): The graph to find the shortest path in.
+            start (Node): The starting node.
+            end (Node): The ending node.
+            toPrint (bool, optional): Whether to print the path. Defaults to False.
+
+        Returns:
+            list: The shortest path from start to end in the graph.
+        """
         return self.DFS(graph, start, end, [], None, toPrint)
 
-
     def get_shortest_path(self, source, destination, method='dfs'):
+        """
+        Prints the shortest path from source to destination in the graph.
+
+        Args:
+            source (str): The name of the source node.
+            destination (str): The name of the destination node.
+            method (str, optional): The method to use for finding the shortest path ('dfs' or 'bfs'). Defaults to 'dfs'.
+        """
         sp = self.shortest_path_dfs(self.g,
                            self.g.get_node(source),
                            self.g.get_node(destination),
@@ -154,4 +350,3 @@ class CityPlanner():
                   destination, 'is', self.print_path(sp))
         else:
             print('There is no path from', source, 'to', destination)
-
