54ff38c24ed81b69903b924ecc3f432fa4ab3197
Your Name <kemin0315@gmail.com>
2024-04-15 15:20:45 +0800

    fix city_planner

--- tests/team_7/lec3/test_city_planner.py
+++ tests/team_7/lec3/test_city_planner.py
@@ -1,20 +1,18 @@
 import pytest
 from lecture3_graph import *
-
-# This test case is for the CityPlanner class.
+# This is a test function for the CityPlanner class.
 def test_city_planner():
-
     # Create an instance of CityPlanner
     cp = CityPlanner()
+    
     # Create an instance of Digraph
     g = Digraph()
 
-    # Add nodes to the graph for each city
+    # Loop through a list of city names
     for name in ('Boston', 'Providence', 'New York', 'Chicago',
                  'Denver', 'Phoenix', 'Los Angeles'):  # Create 7 nodes
+        # For each city name, create a Node and add it to the Digraph
         g.add_node(Node(name))
-
-    # Add edges between the nodes (cities)
     g.add_edge(Edge(g.get_node('Boston'), g.get_node('Providence')))
     g.add_edge(Edge(g.get_node('Boston'), g.get_node('New York')))
     g.add_edge(Edge(g.get_node('Providence'), g.get_node('Boston')))
@@ -26,11 +24,9 @@ def test_city_planner():
     g.add_edge(Edge(g.get_node('Denver'), g.get_node('New York')))
     g.add_edge(Edge(g.get_node('Los Angeles'), g.get_node('Boston')))
 
-    # Assign the graph to the CityPlanner instance
     cp.g = g
 
-    # Assert that the node 'Boston' is correctly added to the graph
     assert cp.g.get_node('Boston').get_name() == 'Boston'
 
-    # Test the get_shortest_path method of the CityPlanner class
     cp.get_shortest_path('Chicago', 'Boston')
+
