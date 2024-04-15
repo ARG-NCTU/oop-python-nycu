ca825c0e219c8db6f0493434d4854aee1f12a45f
benjaminpot <benjaminkuo67@gmail.com>
2024-04-15 14:05:16 +0800

    descript the code in lec3 graph

--- tests/team_8/lec3_1/lecture3_graph.py
+++ tests/team_8/lec3_1/lecture3_graph.py
@@ -1,45 +1,124 @@
 class Node:
-    """Represents a node in a graph."""
+    """
+    Represents a node in a graph.
+
+    Attributes:
+        name (str): The name of the node.
+    """
+
     def __init__(self, name):
-        """Assumes name is a string."""
+        """
+        The constructor for Node class.
+
+        Parameters:
+            name (str): The name of the node.
+        """
         self.name = name
 
     def get_name(self):
+        """
+        Retrieves the name of the node.
+
+        Returns:
+            str: The name of the node.
+        """
         return self.name
 
     def __str__(self):
+        """
+        Returns the string representation of the node.
+
+        Returns:
+            str: The name of the node.
+        """
         return self.name
-
-
 class Edge:
-    """Represents a directed edge in a graph."""
+    """
+    Represents a directed edge in a graph.
+
+    Attributes:
+        src (Node): The source node of the edge.
+        dest (Node): The destination node of the edge.
+    """
+
     def __init__(self, src, dest):
-        """Assumes src and dest are nodes."""
+        """
+        The constructor for Edge class.
+
+        Parameters:
+            src (Node): The source node of the edge.
+            dest (Node): The destination node of the edge.
+        """
         self.src = src
         self.dest = dest
 
     def get_source(self):
+        """
+        Retrieves the source node of the edge.
+
+        Returns:
+            Node: The source node of the edge.
+        """
         return self.src
 
     def get_destination(self):
+        """
+        Retrieves the destination node of the edge.
+
+        Returns:
+            Node: The destination node of the edge.
+        """
         return self.dest
 
     def __str__(self):
+        """
+        Returns the string representation of the edge.
+
+        Returns:
+            str: The string representation of the edge in the format 'src->dest'.
+        """
         return self.src.get_name() + '->' + self.dest.get_name()
 
 
 class Digraph:
-    """Edges is a dict mapping each node to a list of its children."""
+    """
+    Represents a directed graph.
+
+    Attributes:
+        edges (dict): A dictionary mapping each node to a list of its children.
+    """
+
     def __init__(self):
+        """
+        The constructor for Digraph class.
+        """
         self.edges = {}
 
     def add_node(self, node):
+        """
+        Adds a node to the graph.
+
+        Parameters:
+            node (Node): The node to be added.
+
+        Raises:
+            ValueError: If the node already exists in the graph.
+        """
         if node in self.edges:
             raise ValueError('Duplicate node')
         else:
             self.edges[node] = []
 
     def add_edge(self, edge):
+        """
+        Adds an edge to the graph.
+
+        Parameters:
+            edge (Edge): The edge to be added.
+
+        Raises:
+            ValueError: If the source or destination node of the edge is not in the graph.
+        """
         src = edge.get_source()
         dest = edge.get_destination()
         if not (src in self.edges and dest in self.edges):
@@ -47,111 +126,56 @@ class Digraph:
         self.edges[src].append(dest)
 
     def children_of(self, node):
+        """
+        Retrieves the children of a node.
+
+        Parameters:
+            node (Node): The node to retrieve the children of.
+
+        Returns:
+            list: The list of children nodes.
+        """
         return self.edges[node]
 
     def has_node(self, node):
+        """
+        Checks if a node exists in the graph.
+
+        Parameters:
+            node (Node): The node to check.
+
+        Returns:
+            bool: True if the node exists, False otherwise.
+        """
         return node in self.edges
 
     def get_node(self, name):
+        """
+        Retrieves a node by its name.
+
+        Parameters:
+            name (str): The name of the node.
+
+        Returns:
+            Node: The node with the given name.
+
+        Raises:
+            NameError: If no node with the given name exists.
+        """
         for n in self.edges:
             if n.get_name() == name:
                 return n
         raise NameError(name)
 
     def __str__(self):
+        """
+        Returns the string representation of the graph.
+
+        Returns:
+            str: The string representation of the graph in the format 'src->dest'.
+        """
         result = ''
         for src in self.edges:
             for dest in self.edges[src]:
                 result += src.get_name() + '->' + dest.get_name() + '\n'
         return result[:-1]  # Omit final newline
-
-
-class Graph(Digraph):
-    """Represents a graph as a dictionary of nodes mapping"""
-    def add_edge(self, edge):
-        Digraph.add_edge(self, edge)
-        rev = Edge(edge.get_destination(), edge.get_source())
-        Digraph.add_edge(self, rev)
-
-class CityPlanner():
-
-    def __init__(self):
-        self.print_queue = True
-        self.g = []
-
-    def print_path(self, path):
-        """Assumes path is a list of nodes"""
-        result = ''
-        for i in range(len(path)):
-            result += str(path[i])
-            if i != len(path) - 1:
-                result += '->'
-        return result
-
-    def DFS(self, graph, start, end, path, shortest, to_print=False):
-        """Assumes graph is a Digraph; start and end are nodes;
-           path and shortest are lists of nodes
-           Returns a shortest path from start to end in graph"""
-        path = path + [start]
-        if to_print:
-            print('Current DFS path:', self.print_path(path))
-        if start == end:
-            return path
-        for node in graph.children_of(start):
-            if node not in path:  # avoid cycles
-                if shortest is None or len(path) < len(shortest):
-                    new_path = self.DFS(graph, node, end, path, shortest,
-                                    to_print)
-                    if new_path is not None:
-                        shortest = new_path
-            elif to_print:
-                print('Already visited', node)
-        return shortest
-
-    def BFS(self, graph, start, end, to_print=False):
-        """Assumes graph is a Digraph; start and end are nodes
-           Returns a shortest path from start to end in graph"""
-        init_path = [start]
-        path_queue = [init_path]
-        while len(path_queue) != 0:
-            # Get and remove oldest element in path_queue
-            if to_print:
-                print('Queue:', len(path_queue))
-                for p in path_queue:
-                    print(print_path(p))
-            tmp_path = path_queue.pop(0)
-            if to_print:
-                print('Current BFS path:', self.print_path(tmp_path))
-                print()
-            last_node = tmp_path[-1]
-            if last_node == end:
-                return tmp_path
-            for next_node in graph.children_of(last_node):
-                if next_node not in tmp_path:
-                    new_path = tmp_path + [next_node]
-                    path_queue.append(new_path)
-        return None
-
-    def shortest_path_bfs(self, graph, start, end, toPrint = False):
-        """Assumes graph is a Digraph; start and end are nodes
-           Returns a shortest path from start to end in graph"""
-        return self.BFS(graph, start, end, toPrint)
-
-
-    def shortest_path_dfs(self, graph, start, end, toPrint = False):
-        """Assumes graph is a Digraph; start and end are nodes
-           Returns a shortest path from start to end in graph"""
-        return self.DFS(graph, start, end, [], None, toPrint)
-
-
-    def get_shortest_path(self, source, destination, method='dfs'):
-        sp = self.shortest_path_dfs(self.g,
-                           self.g.get_node(source),
-                           self.g.get_node(destination),
-                           True)
-        if sp is not None:
-            print('Shortest path from', source, 'to',
-                  destination, 'is', self.print_path(sp))
-        else:
-            print('There is no path from', source, 'to', destination)
-
