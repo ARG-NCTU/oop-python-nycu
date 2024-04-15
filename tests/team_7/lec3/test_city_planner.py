6ddd1edfac9451f4878230f0d310b72782fc9953
Your Name <kemin0315@gmail.com>
2024-04-15 18:41:26 +0800

    fix test_city_planner second time

--- tests/team_7/lec3/test_city_planner.py
+++ tests/team_7/lec3/test_city_planner.py
@@ -1,18 +1,12 @@
-import pytest
-from lecture3_graph import *
-# This is a test function for the CityPlanner class.
 def test_city_planner():
-    # Create an instance of CityPlanner
+
     cp = CityPlanner()
-    
-    # Create an instance of Digraph
     g = Digraph()
 
-    # Loop through a list of city names
     for name in ('Boston', 'Providence', 'New York', 'Chicago',
                  'Denver', 'Phoenix', 'Los Angeles'):  # Create 7 nodes
-        # For each city name, create a Node and add it to the Digraph
         g.add_node(Node(name))
+
     g.add_edge(Edge(g.get_node('Boston'), g.get_node('Providence')))
     g.add_edge(Edge(g.get_node('Boston'), g.get_node('New York')))
     g.add_edge(Edge(g.get_node('Providence'), g.get_node('Boston')))
@@ -28,5 +22,7 @@ def test_city_planner():
 
     assert cp.g.get_node('Boston').get_name() == 'Boston'
 
-    cp.get_shortest_path('Chicago', 'Boston')
+    shortest_path = cp.get_shortest_path('Chicago', 'Boston')
+    expected_path = ['Chicago', 'New York', 'Boston']
+    assert shortest_path == expected_path
 
