af9867002e82ce5564d027bbf2053449c3c10277
chungchenyi <jasonchung.ee12@nycu.edu.tw>
2024-04-19 01:22:33 +0800

    test_lec6_recursion_dictionaries.py

--- tests/test_lec6_recursion_dictionaries.py
+++ tests/test_lec6_recursion_dictionaries.py
@@ -131,6 +131,24 @@ def test_is_palindrome():
     assert lec6.is_palindrome('eillie') == True
     assert lec6.is_palindrome('cookieliileikooc') == True
 
+def test_1_fib():
+    assert lec6.fib(0) == 1
+    assert lec6.fib(1) == 1
+    assert lec6.fib(2) == 2
+    assert lec6.fib(3) == 3
+    assert lec6.fib(4) == 5
+    assert lec6.fib(5) == 8
+    assert lec6.fib(6) == 13
+    assert lec6.fib(7) == 21
+    assert lec6.fib(8) == 34
+    assert lec6.fib(9) == 55
+    assert lec6.fib(10) == 89 
+    assert lec6.fib(11) == 144
+    assert lec6.fib(12) == 233
+    assert lec6.fib(13) == 377
+    
+
+
 fuchengguo = ['wuo','due','ni','i','i','i','bu','wan']
 beatles = lec6.lyrics_to_frequencies(fuchengguo)
 def test_lyrics_to_frequencies():
