5909af4698b1868fe594a05a28463ba732be8b5d
daniel <twd931129@gmail.com>
2024-04-22 15:09:53 +0800

    a

--- tests/team_10/test_lec4.py
+++ tests/team_10/test_lec4.py
@@ -18,13 +18,7 @@ def test_roll():
         result = result + str(roll_die())
     print(f'test_roll: {result}')
 
-def test_roll_die_seed():
-    # Test that the function returns the same value with a seed
-    random.seed(0)
-    result1 = roll_die()
-    random.seed(0)
-    result2 = roll_die():
-    assert result1 == result2
+
 
 
 
