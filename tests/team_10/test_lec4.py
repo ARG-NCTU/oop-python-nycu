74161e2034c8ea5432992cf063c00409ce9ed21c
daniel <twd931129@gmail.com>
2024-04-22 15:13:06 +0800

    a

--- tests/team_10/test_lec4.py
+++ tests/team_10/test_lec4.py
@@ -18,7 +18,12 @@ def test_roll():
         result = result + str(roll_die())
     print(f'test_roll: {result}')
 
-
+def test_roll_die2():
+    # Test that the function returns an int between 1 and 6
+    for i in range(100):
+        result = roll_die()
+        assert isinstance(result, int)
+        assert result >= 1 and result <= 6
 
 
 
