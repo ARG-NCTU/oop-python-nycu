60d433bb75137ebf090173c7ebf14c4fb4e03a53
chinjulu <chinju.ee12@nycu.edu.tw>
2024-04-29 02:25:03 +0800

    lec2

--- tests/team_6/test_6_menu.py
+++ tests/team_6/test_6_menu.py
@@ -23,6 +23,28 @@ def test_menu():
     assert Menu.get_foods_str(foods) == \
         'apple: <10, 50>; banana: <20, 100>; orange: <30, 150>; '
 
+def test_6_menu():
+    names = ["Calculus","Physics","OOP", "Wine", "Beer", "Whiskey"]
+    values = [50, 60, 30, 20, 10, 100]
+    calories = [100, 200, 150, 80, 20, 500]
+    expected_foods = [
+        Food("Calculus", 50, 100),
+        Food("Physics", 60, 200),
+        Food("OOP", 30, 150),
+        Food("Wine", 20, 80),
+        Food("Beer", 10, 20),
+        Food("Whiskey", 100, 500),
+    ]
+    menu = Menu(names, values, calories)
+    foods = menu.get_foods()
+
+    print(menu)
+
+    assert foods[0].get_cost() == expected_foods[0].get_cost()
+    assert foods[1].get_value() == expected_foods[1].get_value()
+    assert Menu.get_foods_str(foods) == \
+        'Calculus: <50, 100>; Physics: <60, 200>; OOP: <30, 150>; Wine: <20, 80>; Beer: <10, 20>; Whiskey: <100, 500>; '
+
 def test_build_large_menu():
     menu = Menu()
     num_items = 10
--- tests/team_6/test_food.py
+++ tests/team_6/test_food.py
@@ -17,3 +17,19 @@ def test_density(food):
 
 def test_str(food):
     assert str(food) == "apple: <10, 50>"
+
+def food_6():
+    return Food("calculus", 20, 500)
+
+def test_get_value_6():
+    assert food_6().get_value() == 20
+
+def test_get_cost_6():
+    assert food_6().get_cost() == 500
+
+def test_density_6():
+    assert food_6().density() == 0.04
+
+def test_str_6():
+    assert str(food_6()) == "calculus: <20, 500>"
+
