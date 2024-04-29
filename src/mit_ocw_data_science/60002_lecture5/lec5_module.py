9d463601851d5f3cbea86c3709252394577356be
s100179 <liyun0866@gmail.com>
2024-04-29 20:16:04 +0800

    lecture5

--- src/mit_ocw_data_science/60002_lecture5/lec5_module.py
+++ src/mit_ocw_data_science/60002_lecture5/lec5_module.py
@@ -1,23 +1,23 @@
 import random
-import pylab
+import matplotlib.pyplot as plt
 
 # set line width
-pylab.rcParams['lines.linewidth'] = 4
+plt.rcParams['lines.linewidth'] = 4
 # set font size for titles
-pylab.rcParams['axes.titlesize'] = 20
+plt.rcParams['axes.titlesize'] = 20
 # set font size for labels on axes
-pylab.rcParams['axes.labelsize'] = 20
+plt.rcParams['axes.labelsize'] = 20
 # set size of numbers on x-axis
-pylab.rcParams['xtick.labelsize'] = 16
+plt.rcParams['xtick.labelsize'] = 16
 # set size of numbers on y-axis
-pylab.rcParams['ytick.labelsize'] = 16
+plt.rcParams['ytick.labelsize'] = 16
 # set size of ticks on x-axis
-pylab.rcParams['xtick.major.size'] = 7
+plt.rcParams['xtick.major.size'] = 7
 # set size of ticks on y-axis
-pylab.rcParams['ytick.major.size'] = 7
+plt.rcParams['ytick.major.size'] = 7
 # set size of markers, e.g., circles representing points
 # set numpoints for legend
-pylab.rcParams['legend.numpoints'] = 1
+plt.rcParams['legend.numpoints'] = 1
 
 
 class Location:
