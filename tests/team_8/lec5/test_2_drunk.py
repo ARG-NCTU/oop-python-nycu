c1d64c984cc4e8413799c51d43f7f720528b0561
benjaminpot <benjaminkuo67@gmail.com>
2024-04-29 13:56:05 +0800

    add test_2_drunk

--- tests/team_8/lec5/test_2_drunk.py
+++ tests/team_8/lec5/test_2_drunk.py
@@ -2,15 +2,15 @@ import pytest
 from lec5_module import Field,  Drunk, UsualDrunk, MasochistDrunk, Location
 
 def test_usual_drunk():
-    # Test taking a step
+    # Test taking a step with UsualDrunk
     d = UsualDrunk()
-    for i in range(10):
+    for i in range(100):
         step = d.take_step()
         assert step in [(0, 1), (0, -1), (1, 0), (-1, 0)]
 
 def test_masochist_drunk():
-    # Test taking a step
+    # Test taking a step with MasochistDrunk
     d = MasochistDrunk()
-    for i in range(10):
+    for i in range(100):
         step = d.take_step()
         assert step in [(0.0, 1.1), (0.0, -0.9), (1.0, 0.0), (-1.0, 0.0)]
