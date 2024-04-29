8d843ffda8280bce1fd29affefbd9d6b90381719
yummypenguin <timlin14012005.ee12@nycu.edu.tw>
2024-04-29 14:22:57 +0800

    add 1 line to plot_matplotlib.py

--- tests/team_17/lec5/plot_matplotlib.py
+++ tests/team_17/lec5/plot_matplotlib.py
@@ -1,9 +1,11 @@
 import matplotlib.pyplot as plt
 
-x_vals = [1, 2, 3, 4]
-y_vals_1 = [1, 2, 3, 4]
-plt.plot(x_vals, y_vals_1, 'b-', label='first')
-y_vals_2 = [1, 7, 3, 5]
-plt.plot(x_vals, y_vals_2, 'r--', label='second')
+x_vals_1 = [1, 2, 3, 4, 5, 6]
+y_vals_1 = [1, 2, 3, 4, 5, 6]
+plt.plot(x_vals_1, y_vals_1, 'b-', label='first')
+y_vals_2 = [1, 7, 3, 5, 6, 2]
+plt.plot(x_vals_1, y_vals_2, 'r--', label='second')
+x_vals_2 = [1, 10, 9, 7, 3, 5]
+plt.plot(x_vals_2, y_vals_2, 'g-.', label='third')
 plt.legend()
 plt.show()
