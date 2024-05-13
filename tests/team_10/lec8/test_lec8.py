6e6bafac8222f3191fffcebf62397e1cbca2b962
JimHsu0313 <jim0313.ee12@nycu.edu.tw>
2024-05-13 14:41:25 +0800

    abc

--- tests/team_10/lec8/test_lec8.py
+++ tests/team_10/lec8/test_lec8.py
@@ -16,7 +16,6 @@ def test_get_highs():
     population = get_highs()
     expected = [3.1, 0.55, 0.0] 
     assert population[0:3] == expected
-
     
 def test_get_highs_length():
     # Test that get_highs returns a list of the expected length
