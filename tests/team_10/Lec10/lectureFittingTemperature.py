4a1dd8a0ca90e78b9920c94aa3899da9d6ba06ea
JimHsu0313 <jim0313.ee12@nycu.edu.tw>
2024-05-27 14:52:41 +0800

    lec10

--- tests/team_10/Lec10/lectureFittingTemperature.py
+++ tests/team_10/Lec10/lectureFittingTemperature.py
@@ -1,177 +1,113 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Sep 19 11:45:20 2016
+import random
+import matplotlib.pyplot as plt
+import numpy as np
 
-@author: johnguttag
-"""
+# Set line width
+plt.rcParams['lines.linewidth'] = 4
+# Set font size for titles 
+plt.rcParams['axes.titlesize'] = 20
+# Set font size for labels on axes
+plt.rcParams['axes.labelsize'] = 20
+# Set size of numbers on x-axis
+plt.rcParams['xtick.labelsize'] = 16
+# Set size of numbers on y-axis
+plt.rcParams['ytick.labelsize'] = 16
+# Set size of ticks on x-axis
+plt.rcParams['xtick.major.size'] = 7
+# Set size of ticks on y-axis
+plt.rcParams['ytick.major.size'] = 7
+# Set size of markers
+plt.rcParams['lines.markersize'] = 10
+# Set number of examples shown in legends
+plt.rcParams['legend.numpoints'] = 1
 
-import random, pylab, numpy
+def getData(fileName):
+    distances = []
+    masses = []
+    with open(fileName, 'r') as dataFile:
+        dataFile.readline()  # discard header
+        for line in dataFile:
+            x, y = line.split()
+            distances.append(float(x))
+            masses.append(float(y))
+    return distances, masses
 
-#set line width
-pylab.rcParams['lines.linewidth'] = 4
-#set font size for titles 
-pylab.rcParams['axes.titlesize'] = 20
-#set font size for labels on axes
-pylab.rcParams['axes.labelsize'] = 20
-#set size of numbers on x-axis
-pylab.rcParams['xtick.labelsize'] = 16
-#set size of numbers on y-axis
-pylab.rcParams['ytick.labelsize'] = 16
-#set size of ticks on x-axis
-pylab.rcParams['xtick.major.size'] = 7
-#set size of ticks on y-axis
-pylab.rcParams['ytick.major.size'] = 7
-#set size of markers
-pylab.rcParams['lines.markersize'] = 10
-#set number of examples shown in legends
-pylab.rcParams['legend.numpoints'] = 1
+def labelPlot():
+    plt.title('Measured Displacement of Spring')
+    plt.xlabel('|Force| (Newtons)')
+    plt.ylabel('Distance (meters)')
 
+def plotData(fileName):
+    xVals, yVals = getData(fileName)
+    xVals = np.array(xVals)
+    yVals = np.array(yVals)
+    xVals = xVals * 9.81  # due to gravity
+    plt.plot(xVals, yVals, 'bo', label='Measured displacements')
+    labelPlot()
+
+def fitData(fileName):
+    xVals, yVals = getData(fileName)
+    xVals = np.array(xVals)
+    yVals = np.array(yVals)
+    xVals = xVals * 9.81  # get force
+    plt.plot(xVals, yVals, 'bo', label='Measured points')
+    labelPlot()
+    a, b = np.polyfit(xVals, yVals, 1)
+    estYVals = a * np.array(xVals) + b
+    print('a =', a, 'b =', b)
+    plt.plot(xVals, estYVals, 'r', label='Linear fit, k = ' + str(round(1 / a, 5)))
+    plt.legend(loc='best')
+
+def fitData1(fileName):
+    xVals, yVals = getData(fileName)
+    xVals = np.array(xVals)
+    yVals = np.array(yVals)
+    xVals = xVals * 9.81  # get force
+    plt.plot(xVals, yVals, 'bo', label='Measured points')
+    labelPlot()
+    model = np.polyfit(xVals, yVals, 1)
+    estYVals = np.polyval(model, xVals)
+    plt.plot(xVals, estYVals, 'r', label='Linear fit, k = ' + str(round(1 / model[0], 5)))
+    plt.legend(loc='best')
 
 def rSquared(observed, predicted):
-    error = ((predicted - observed)**2).sum()
-    meanError = error/len(observed)
-    return 1 - (meanError/numpy.var(observed))
+    error = ((predicted - observed) ** 2).sum()
+    meanError = error / len(observed)
+    return 1 - (meanError / np.var(observed))
 
 def genFits(xVals, yVals, degrees):
     models = []
     for d in degrees:
-        model = pylab.polyfit(xVals, yVals, d)
+        model = np.polyfit(xVals, yVals, d)
         models.append(model)
     return models
 
-def testFits(models, degrees, xVals, yVals, title):
-    pylab.plot(xVals, yVals, 'o', label = 'Data')
+def plotFits(models, degrees, xVals, yVals, title):
+    plt.plot(xVals, yVals, 'o', label='Data')
     for i in range(len(models)):
-        estYVals = pylab.polyval(models[i], xVals)
+        estYVals = np.polyval(models[i], xVals)
         error = rSquared(yVals, estYVals)
-        pylab.plot(xVals, estYVals,
-                   label = 'Fit of degree '\
-                   + str(degrees[i])\
-                   + ', R2 = ' + str(round(error, 5)))
-    pylab.legend(loc = 'best')
-    pylab.title(title)
+        plt.plot(xVals, estYVals, label='Fit of degree ' + str(degrees[i]) + ', R2 = ' + str(round(error, 5)))
+    plt.legend(loc='best')
+    plt.title(title)
 
-def getData(fileName):
-    dataFile = open(fileName, 'r')
-    distances = []
-    masses = []
-    dataFile.readline() #discard header
-    for line in dataFile:
-        d, m = line.split()
-        distances.append(float(d))
-        masses.append(float(m))
-    dataFile.close()
-    return (masses, distances)
-    
-def labelPlot():
-    pylab.title('Measured Displacement of Spring')
-    pylab.xlabel('|Force| (Newtons)')
-    pylab.ylabel('Distance (meters)')
+def genNoisyParabolicData(a, b, c, xVals, fName):
+    yVals = []
+    for x in xVals:
+        theoreticalVal = a * x ** 2 + b * x + c
+        yVals.append(theoreticalVal + random.gauss(0, 35))
+    with open(fName, 'w') as f:
+        f.write('x y\n')
+        for i in range(len(xVals)):
+            f.write(f"{xVals[i]} {yVals[i]}\n")
 
-def plotData(fileName):
-    xVals, yVals = getData(fileName)
-    xVals = pylab.array(xVals)
-    yVals = pylab.array(yVals)
-    xVals = xVals*9.81  #acc. due to gravity
-    pylab.plot(xVals, yVals, 'bo',
-               label = 'Measured displacements')
-    labelPlot()
-    
-def fitData(fileName):
-    xVals, yVals = getData(fileName)
-    xVals = pylab.array(xVals)
-    yVals = pylab.array(yVals)
-    xVals = xVals*9.81 #get force
-    pylab.plot(xVals, yVals, 'bo',
-               label = 'Measured points')                 
-    model = pylab.polyfit(xVals, yVals, 1)
-    xVals = xVals + [2]
-    yVals = yVals + []
-    estYVals = pylab.polyval(model, xVals)
-    pylab.plot(xVals, estYVals, 'r',
-               label = 'Linear fit, r**2 = '
-               + str(round(rSquared(yVals, estYVals), 5)))                
-    model = pylab.polyfit(xVals, yVals, 2)
-    estYVals = pylab.polyval(model, xVals)
-    pylab.plot(xVals, estYVals, 'g--',
-               label = 'Quadratic fit, r**2 = '
-               + str(round(rSquared(yVals, estYVals), 5)))
-    pylab.title('A Linear Spring')
-    labelPlot()
-    pylab.legend(loc = 'best')
-    
-random.seed(0)
+# Generate sample data
+xVals = [i for i in range(30)]
+genNoisyParabolicData(2, -3, 5, xVals, 'sampleData.txt')
 
-class tempDatum(object):
-    def __init__(self, s):
-        info = s.split(',')
-        self.high = float(info[1])
-        self.year = int(info[2][0:4])
-    def getHigh(self):
-        return self.high
-    def getYear(self):
-        return self.year
-    
-def getTempData():
-    inFile = open('temperatures.csv')
-    data = []
-    for l in inFile:
-        data.append(tempDatum(l))
-    return data
-    
-def getYearlyMeans(data):
-    years = {}
-    for d in data:
-        try:
-            years[d.getYear()].append(d.getHigh())
-        except:
-            years[d.getYear()] = [d.getHigh()]
-    for y in years:
-        years[y] = sum(years[y])/len(years[y])
-    return years
-    
-data = getTempData()
-years = getYearlyMeans(data)
-xVals, yVals = [], []
-for e in years:
-    xVals.append(e)
-    yVals.append(years[e])
-pylab.plot(xVals, yVals)
-pylab.xlabel('Year')
-pylab.ylabel('Mean Daily High (C)')
-pylab.title('Select U.S. Cities')
+# Plot the data and fit a linear model
+plt.figure()
+plotData('sampleData.txt')
+fitData('sampleData.txt')
+plt.show()
 
-def splitData(xVals, yVals):
-    toTrain = random.sample(range(len(xVals)),
-                            len(xVals)//2)
-    trainX, trainY, testX, testY = [],[],[],[]
-    for i in range(len(xVals)):
-        if i in toTrain:
-            trainX.append(xVals[i])
-            trainY.append(yVals[i])
-        else:
-            testX.append(xVals[i])
-            testY.append(yVals[i])
-    return trainX, trainY, testX, testY
-    
-##UNCOVER FOR SECOND DEMO    
-#numSubsets = 10
-#dimensions = (1, 2, 3, 4)
-#rSquares = {}
-#for d in dimensions:
-#    rSquares[d] = []
-#        
-#for f in range(numSubsets):
-#    trainX, trainY, testX, testY = splitData(xVals, yVals)
-#    for d in dimensions:
-#        model = pylab.polyfit(trainX, trainY, d)
-#        estYVals = pylab.polyval(model, trainX)
-#        estYVals = pylab.polyval(model, testX)
-#        rSquares[d].append(rSquared(testY, estYVals))
-#print('Mean R-squares for test data')
-#for d in dimensions:
-#    mean = round(sum(rSquares[d])/len(rSquares[d]), 4)
-#    sd = round(numpy.std(rSquares[d]), 4)
-#    print('For dimensionality', d, 'mean =', mean,
-#          'Std =', sd)
-##print(rSquares[1])
---
+++ tests/team_10/Lec10/lecture_testcode.py
@@ -0,0 +1,41 @@
+# test_lectureCode.py
+
+import pytest
+import numpy as np
+from lectureCode import getData, rSquared, genFits
+
+# Sample data for testing
+test_data = """x y
+0.0 0.0
+1.0 9.81
+2.0 19.62
+3.0 29.43
+4.0 39.24
+5.0 49.05
+"""
+
+def create_test_file(file_name, data):
+    with open(file_name, 'w') as f:
+        f.write(data)
+
+def test_getData():
+    create_test_file('test_data.txt', test_data)
+    distances, masses = getData('test_data.txt')
+    assert distances == [0.0, 1.0, 2.0, 3.0, 4.0, 5.0]
+    assert masses == [0.0, 9.81, 19.62, 29.43, 39.24, 49.05]
+
+def test_rSquared():
+    observed = np.array([0, 1, 2, 3, 4, 5])
+    predicted = np.array([0, 1, 2, 3, 4, 5])
+    assert rSquared(observed, predicted) == 1.0
+    predicted = np.array([0, 1, 1.5, 3.5, 4, 5])
+    assert round(rSquared(observed, predicted), 2) == 0.97
+
+def test_genFits():
+    create_test_file('test_data.txt', test_data)
+    distances, masses = getData('test_data.txt')
+    models = genFits(np.array(distances) * 9.81, np.array(masses), [1])
+    assert len(models) == 1
+    assert round(models[0][0], 2) == 1.00
+    assert round(models[0][1], 2) == 0.00
+
