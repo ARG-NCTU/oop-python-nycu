2028e65b57f36943222ee87ede8f377e1d80b81a
Thomas <sthomastsai.com>
2024-06-17 05:21:30 +0800

    data

--- final-project/panda.py
+++ final-project/panda.py
@@ -2,20 +2,18 @@ import pandas as pd
 import json
 import matplotlib.pyplot as plt
 
-# 讀取JSON文件的每一行，將其轉換為字典列表
-data = []
+# 读取JSON文件并将其转换为字典列表
 with open('./oop-python-nycu/final-project/player_data.json', 'r') as f:
-    for line in f:
-        data.append(json.loads(line))
+    data = json.load(f)
 
-# 將數據轉換為DataFrame
+# 将数据转换为DataFrame
 df = pd.DataFrame(data)
 
-# 顯示數據框架的前幾行
+# 显示数据框架的前几行
 print(df.head())
 
-# 繪製圖表
-# 例如：繪製每個玩家的跳躍次數對比
+# 绘制图表
+# 例如：绘制每个玩家的跳跃次数对比
 plt.figure(figsize=(10, 6))
 plt.plot(df.index, df['player1_jump_count'], label='Player 1 Jump Count')
 plt.plot(df.index, df['player2_jump_count'], label='Player 2 Jump Count')
@@ -25,7 +23,7 @@ plt.title('Player Jump Counts Over Games')
 plt.legend()
 plt.show()
 
-# 繪製每個玩家的射擊次數對比
+# 绘制每个玩家的射击次数对比
 plt.figure(figsize=(10, 6))
 plt.plot(df.index, df['player1_shoot_count'], label='Player 1 Shoot Count')
 plt.plot(df.index, df['player2_shoot_count'], label='Player 2 Shoot Count')
@@ -35,4 +33,5 @@ plt.title('Player Shoot Counts Over Games')
 plt.legend()
 plt.show()
 
-# 你可以根據需要繪製更多的圖表，比如炸彈使用次數、死亡次數等
+# 你可以根据需要绘制更多的图表，比如炸弹使用次数、死亡次数等
+
