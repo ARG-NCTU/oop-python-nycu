e73aecd151670eb48c9be7b1f0846fc6df30cc57
ZachLian <45685256k@gmail.com>
2024-09-09 15:06:27 +0800

    add myname.txt

--- opencv/05_plot.py
+++ opencv/05_plot.py
@@ -1,4 +1,4 @@
-import matplotlib.pyplot as plt
+'''import matplotlib.pyplot as plt
 import cv2
 
 data_path = '/home/arg/oop-python-nycu/images/'
@@ -26,4 +26,37 @@ axs[2].imshow(cv2.cvtColor(img3, cv2.COLOR_BGR2RGB))
 axs[2].set_title('Hough Detect')
 
 # Show the plot
-plt.show()
+plt.show()'''
+import matplotlib.pyplot as plt                                                 
+import cv2                                                                      
+                                                                                 
+def plot_images(img1, img2, img3):                                               
+    # Create a figure with 3 subplots                                           
+    fig, axs = plt.subplots(1, 3, figsize=(10, 10))                             
+                                                                                 
+    # Display the first image in the first subplot                              
+    axs[0].imshow(cv2.cvtColor(img1, cv2.COLOR_BGR2RGB))                        
+    axs[0].set_title('Raw')                                                     
+                                                                                 
+    # Display the second image in the second subplot                             
+    axs[1].imshow(cv2.cvtColor(img2, cv2.COLOR_BGR2RGB))                        
+    axs[1].set_title('Gray')                                                    
+                                                                                 
+    # Display the third image in the third subplot                               
+    axs[2].imshow(cv2.cvtColor(img3, cv2.COLOR_BGR2RGB))                        
+    axs[2].set_title('Hough Detect')                                            
+                                                                                 
+    # Show the plot                                                               
+    plt.show()                                                                  
+
+# Load three images                                                               
+data_path = '/home/arg/oop-python-nycu/images/'                                 
+data_name = 'smarties'                                                          
+data_type = '.png'                                                              
+img1 = cv2.imread(data_path + data_name + data_type)                            
+img2 = cv2.imread(data_path + data_name + '_gray' + data_type)                  
+img3 = cv2.imread(data_path + data_name + '_circle' + data_type)                
+
+# Plot the images using the `plot_images` function                                
+plot_images(img1, img2, img3)
+
