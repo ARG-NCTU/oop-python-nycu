b57e6143a64a3e52b773d0177071e36f5cdde1a4
hac-ohmygod0193 <alex.chenghengan@gmail.com>
2024-09-30 14:11:00 +0800

    0930 class assignment

---
+++ tests/group3/test_lec12_bubble_sort_111511198.py
@@ -0,0 +1,11 @@
+import random
+
+from test_lec12_bubble_buggy_111511198 import *
+
+#testList = [1,3,5,7,2,6,25,18,13]
+
+# create a list with 1000 random numbers
+testList = [random.randint(0, 1000) for i in range(1000)]
+
+bubble_sort(testList)
+print(testList)
