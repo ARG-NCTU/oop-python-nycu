948cfbc7cf23a5b0a3fb51fdb6605d6d2af6d0b9
chenmax1016 <chenmax1016@gmail.com>
2024-10-08 00:49:23 +0800

    test_by_112550182

--- tests/group5/test_lec12_bubble_buggy_112550182.py
+++ tests/group5/test_lec12_bubble_buggy_112550182.py
@@ -9,3 +9,12 @@ def bubble_sort(arr):
     return arr
 
 print(bubble_sort([4, 2, 1, 8, 7, 6]))
+
+def test_bubble_sort():
+    assert bubble_sort([4, 2, 1, 8, 7, 6]) == [1, 2, 4, 6, 7, 8]
+    assert bubble_sort([5, 3, 8, 4, 2]) == [2, 3, 4, 5, 8]
+    assert bubble_sort([1, 2, 3, 4, 5]) == [1, 2, 3, 4, 5]
+    assert bubble_sort([]) == []
+    assert bubble_sort([1]) == [1]
+
+test_bubble_sort()
