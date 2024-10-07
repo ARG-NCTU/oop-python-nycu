552fc05420412cb589633c4360d02f30d35973d6
hsuanyo7160 <yoyo235863@gmail.com>
2024-10-07 15:09:12 +0800

    lec11_module's testing code

---
+++ tests/group4/lec11_module.py
@@ -0,0 +1,41 @@
+import matplotlib.pyplot as plt
+import random
+import numpy as np
+
+def variance(X):
+    mean = float(sum(X))/len(X)
+    diffs = 0.0
+    for x in X:
+        diffs += (x - mean)**2
+    return diffs/len(X)
+
+def stdDev(X):
+    return variance(X)**0.5
+ 
+def minkowskiDist(v1, v2, p): 
+    """Assumes v1 and v2 are equal-length arrays of numbers
+       Returns Minkowski distance of order p between v1 and v2"""
+    dist = 0.0
+    for i in range(len(v1)):
+        dist += abs(v1[i] - v2[i])**p
+    return dist**(1.0/p)
+
+class Animal(object):
+    def __init__(self, name, features):
+        """Assumes name a string; features a list of numbers"""
+        self.name = name
+        self.features = np.array(features)
+        
+    def getName(self):
+        return self.name
+    
+    def getFeatures(self):
+        return self.features
+    
+    def distance(self, other):
+        """Assumes other an Animal
+           Returns the Euclidean distance between feature vectors
+              of self and other"""
+        return minkowskiDist(self.getFeatures(),
+                             other.getFeatures(), 2)
+
