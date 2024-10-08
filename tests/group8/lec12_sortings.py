2253305b0a4e64b0ecb6e8bba1a755a8da76816e
OlafLin24 <andreww311sr@gmail.com>
2024-10-09 01:29:04 +0800

    edit lec12_sortings.py

--- tests/group8/lec12_sortings.py
+++ tests/group8/lec12_sortings.py
@@ -7,3 +7,14 @@ def bubble_sort(arr):
             if arr[j] > arr[j+1]:
                 arr[j], arr[j+1] = arr[j+1], arr[j]
     return arr
+
+#selection sort
+def selection_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        min_idx = i
+        for j in range(i+1, n):
+            if arr[j] < arr[min_idx]:
+                min_idx = j
+        arr[i], arr[min_idx] = arr[min_idx], arr[i]
+    return arr
