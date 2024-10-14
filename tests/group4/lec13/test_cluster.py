4d637adc587c8e411f0070138c355c8d4d770093
howard-shi55 <howard.shi55@gmail.com>
2024-10-14 18:57:07 +0800

    add more test examples

--- tests/group4/lec13/test_cluster.py
+++ tests/group4/lec13/test_cluster.py
@@ -7,6 +7,8 @@ import numpy as np
 def test_minkowski_dist():
     assert minkowski_dist([0,0], [3,4], 2) == 5.0
     assert minkowski_dist([1,2,3], [4,5,6], 1) == 9.0
+    assert minkowski_dist([1,2,3], [4,5,6], 2) == 5.196152422706632
+    assert minkowski_dist([1,2,3], [4,5,6], 3) == 4.3267487109222245
     
 def test_Example():
     example = Example('test', [1,2,3], 'test')
