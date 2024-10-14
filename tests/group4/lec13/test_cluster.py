55b3f9022ceafc3580ce1143b11cf658b7d9bef2
WoodyLiang <beta0326fire@gmail.com>
2024-10-14 17:36:59 +0800

    add some test functions

--- tests/group4/lec13/test_cluster.py
+++ tests/group4/lec13/test_cluster.py
@@ -20,4 +20,28 @@ def test_Example():
     assert example.distance(Example('test', [4,5,6], 'test')) == 5.196152422706632
     assert str(example) == 'test:[1, 2, 3]:test'
     
-    
+def test_Cluster():
+    #[age, tale, gender], gender: 1:male, 0:female
+    e1 = Example('LBJ', [39,0,1], 'human')
+    e2 = Example('Tiffany', [19,0,0], 'human')
+    e3 = Example('John', [9,0,1], 'human')
+    e4 = Example('Howard', [26,0,1], 'human')
+    e5 = Example('Bunny', [4,1,1], 'rabbit')
+    e6 = Example('Coco', [8,1,0], 'rabbit')
+    cluster1 = Cluster([e1, e2, e3, e4])
+    cluster2 = Cluster([e5, e6])
+    assert e1 in list(cluster1.members())
+    assert e2 in list(cluster1.members())
+    assert e3 in list(cluster1.members())
+    assert e4 in list(cluster1.members())
+    assert e5 in list(cluster2.members())
+    assert e6 in list(cluster2.members())
+
+    np.testing.assert_almost_equal(cluster1.getCentroid().features, [23.25, 0, 0.75], decimal=2)
+    np.testing.assert_almost_equal(cluster1.variability(), 477.5, decimal=1)
+
+    np.testing.assert_almost_equal(cluster2.getCentroid().features, [6, 1, 0.5], decimal=2)
+    np.testing.assert_almost_equal(cluster2.variability(), 8.5, decimal=1)
+
+    assert dissimilarity([cluster1, cluster2]) == 486.0
+
