ef91dd60227a8671524795efd699ad9cbaabc93c
charlie-ww <smallforest0819@gmail.com>
2024-10-28 22:05:43 +0800

    fix(tests): update import alias in test_insert_sort.py
    Changed the import statement to use an alias for insert_sort function
    to avoid naming conflicts. Updated all test cases to use the new alias
    g8_insert_sort.

--- tests/group8/test_insert_sort.py
+++ tests/group8/test_insert_sort.py
@@ -1,18 +1,18 @@
 #insert sort python test file
 import unittest
-from lec12_sortings import insert_sort
+from lec12_sortings import insert_sort as g8_insert_sort
 
 class TestInsertSort(unittest.TestCase):
     
         def test_insert_sort(self):
-            self.assertEqual(insert_sort([1, 2, 3, 4, 5]), [1, 2, 3, 4, 5])
-            self.assertEqual(insert_sort([5, 4, 3, 2, 1]), [1, 2, 3, 4, 5])
-            self.assertEqual(insert_sort([1, 3, 2, 5, 4]), [1, 2, 3, 4, 5])
-            self.assertEqual(insert_sort([1, 2, 3, 5, 4]), [1, 2, 3, 4, 5])
-            self.assertEqual(insert_sort([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]), [1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
-            self.assertEqual(insert_sort([10, 9, 8, 7, 6, 5, 4, 3, 2, 1]), [1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
-            self.assertEqual(insert_sort([1, 3, 2, 5, 4, 7, 6, 9, 8, 10]), [1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
-            self.assertEqual(insert_sort([1, 2, 3, 5, 4, 7, 6, 9, 8, 10]), [1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+            self.assertEqual(g8_insert_sort([1, 2, 3, 4, 5]), [1, 2, 3, 4, 5])
+            self.assertEqual(g8_insert_sort([5, 4, 3, 2, 1]), [1, 2, 3, 4, 5])
+            self.assertEqual(g8_insert_sort([1, 3, 2, 5, 4]), [1, 2, 3, 4, 5])
+            self.assertEqual(g8_insert_sort([1, 2, 3, 5, 4]), [1, 2, 3, 4, 5])
+            self.assertEqual(g8_insert_sort([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]), [1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+            self.assertEqual(g8_insert_sort([10, 9, 8, 7, 6, 5, 4, 3, 2, 1]), [1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+            self.assertEqual(g8_insert_sort([1, 3, 2, 5, 4, 7, 6, 9, 8, 10]), [1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+            self.assertEqual(g8_insert_sort([1, 2, 3, 5, 4, 7, 6, 9, 8, 10]), [1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
 
 if __name__ == '__main__':
       
