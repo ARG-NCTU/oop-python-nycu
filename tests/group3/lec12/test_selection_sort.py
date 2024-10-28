423edbdf276d4dffaae436d973918c0391e76aa7
ZachLian <45685256k@gmail.com>
2024-10-28 13:35:25 +0800

    add prefix on lec12 function name

--- tests/group3/lec12/test_selection_sort.py
+++ tests/group3/lec12/test_selection_sort.py
@@ -1,16 +1,16 @@
 # test_bubble_sort.py
 
 import pytest
-from lec12_sorting import bubble_sort
+from lec12_sorting import selection_sort as g3_selection_sort
 
 def test_bubble_sort():
-    assert bubble_sort([3, 2, 1]) == [1, 2, 3]
-    assert bubble_sort([5, 2, 9, 1, 5, 6]) == [1, 2, 5, 5, 6, 9]
-    assert bubble_sort([1]) == [1]  # Single element
-    assert bubble_sort([]) == []  # Empty list
-    assert bubble_sort([1, 2, 3]) == [1, 2, 3]  # Already sorted
-    assert bubble_sort([2, 3, 1]) == [1, 2, 3]  # Unsorted
-    assert bubble_sort([1, 0, -1]) == [-1, 0, 1]  # Negative numbers
+    assert g3_selection_sort([3, 2, 1]) == [1, 2, 3]
+    assert g3_selection_sort([5, 2, 9, 1, 5, 6]) == [1, 2, 5, 5, 6, 9]
+    assert g3_selection_sort([1]) == [1]  # Single element
+    assert g3_selection_sort([]) == []  # Empty list
+    assert g3_selection_sort([1, 2, 3]) == [1, 2, 3]  # Already sorted
+    assert g3_selection_sort([2, 3, 1]) == [1, 2, 3]  # Unsorted
+    assert g3_selection_sort([1, 0, -1]) == [-1, 0, 1]  # Negative numbers
 
 if __name__ == "__main__":
     pytest.main()
