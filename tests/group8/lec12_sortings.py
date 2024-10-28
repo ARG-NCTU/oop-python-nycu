4805a369136c7ba88c0a2eb929f6449e07a378cf
charlie-ww <smallforest0819@gmail.com>
2024-10-28 21:48:38 +0800

    feat: add insertion sort implementation
    - Added a new function `insert_sort` to implement the insertion sort algorithm.
    - Ensured the function sorts the array in-place and returns the sorted array.
    - Added a missing return statement to the `merge_sort` function.

--- tests/group8/lec12_sortings.py
+++ tests/group8/lec12_sortings.py
@@ -45,3 +45,15 @@ def merge_sort(arr):
             j += 1
             k += 1
     return arr
+
+#insert sort
+def insert_sort(arr):
+    n = len(arr)
+    for i in range(1, n):
+        key = arr[i]
+        j = i-1
+        while j >= 0 and key < arr[j]:
+            arr[j+1] = arr[j]
+            j -= 1
+        arr[j+1] = key
+    return arr
