9a4e94dbbd598d20fc8d26b29437f0548011a958
lonea2005 <rayrayppp123@gmail.com>
2024-11-02 06:01:41 +0800

    tiles collision done

--- tests/group1/game_testing/script/tilemap.py
+++ tests/group1/game_testing/script/tilemap.py
@@ -1,3 +1,8 @@
+import pygame
+
+NEIGHBORS = [(0,1), (0,-1), (1,0), (-1,0), (1,1), (-1,-1), (1,-1), (-1,1)]
+HAVE_COLLISION = {'stone', 'grass'}
+
 class small_tile:
     def __init__(self, type, variant, pos=(0,0)):
         self.type = type
@@ -15,6 +20,13 @@ class Tilemap:
             self.tilemap[str(3+i) + ";10"] = small_tile('grass', 1, (3+i,10))
             self.tilemap["10;" + str(i+5)] = small_tile('stone', 1, (10,i+5))
 
+        for i in range(10):
+            #setup offgrid tiles
+            self.offgrid_tiles.append(small_tile('decor', 0, (i*16, 0)))
+            self.offgrid_tiles.append(small_tile('decor', 1, (i*16, 16)))
+            self.offgrid_tiles.append(small_tile('decor', 2, (i*16, 32)))
+            self.offgrid_tiles.append(small_tile('decor', 3, (i*16, 48)))
+
     def render(self, surface):
 
         for tile in self.offgrid_tiles:
@@ -24,4 +36,18 @@ class Tilemap:
             tile = self.tilemap[location]
             surface.blit(self.game.assets[tile.type][tile.variant], (tile.pos[0]*self.tile_size, tile.pos[1]*self.tile_size))
 
-        
+    def tiles_around(self, pos):
+        tiles = []
+        tile_loc = (int(pos[0]//self.tile_size), int(pos[1]//self.tile_size)) #convert pixel position to tile position, which is bigger
+        for offset in NEIGHBORS:
+            check = str(tile_loc[0] + offset[0]) + ";" + str(tile_loc[1] + offset[1])
+            if check in self.tilemap:
+                tiles.append(self.tilemap[check])
+        return tiles
+    
+    def tile_collision(self, pos):
+        rects = []  
+        for tile in self.tiles_around(pos):
+            if tile.type in HAVE_COLLISION:
+                rects.append(pygame.Rect(tile.pos[0]*self.tile_size, tile.pos[1]*self.tile_size, self.tile_size, self.tile_size))
+        return rects
