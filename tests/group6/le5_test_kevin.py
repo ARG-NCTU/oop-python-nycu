3a4318cbe89da5e700fda406172574848ad64648
kevinrayrayray <kevinray.ee12@nycu.edu.tw>
2024-11-06 18:13:01 +0800

    kevin test lec5 new 5

--- tests/group6/le5_test_kevin.py
+++ tests/group6/le5_test_kevin.py
@@ -5,22 +5,22 @@ import pytest
 
 # please write a test for quotient_and_remainder function
 def test_quotient_and_remainder():
-    assert lec5.quotient_and_remainder(27, 6) == (4, 1)
+    assert lec5.quotient_and_remainder(27, 6) == (4, 3)
     assert lec5.quotient_and_remainder(20, 7) == (2, 6)
-    assert lec5.quotient_and_remainder(25, 8) == (5, 1)
+    assert lec5.quotient_and_remainder(25, 8) == (3, 1)
     assert lec5.quotient_and_remainder(27, 9) == (3, 0)
-    assert lec5.quotient_and_remainder(22, 10) == (2, 3)
+    assert lec5.quotient_and_remainder(22, 10) == (2, 2)
     assert lec5.quotient_and_remainder(28, 11) == (2, 6)
 
     try:
         lec5.quotient_and_remainder(20, 0)
     except ZeroDivisionError:
          print("ZeroDivisionError")
-    assert lec5.quotient_and_remainder(15, 11) == (1, 5)
+    assert lec5.quotient_and_remainder(15, 11) == (1, 4)
 
 
 def test_sum_elem_method1():
-    assert lec5.sum_elem_method1([1, 2, 3, 6]) == 11
+    assert lec5.sum_elem_method1([1, 2, 3, 6]) == 10
     assert lec5.sum_elem_method1([-1, -7, -3, -4]) == -15
     assert lec5.sum_elem_method1([13, 34, 0, 84, 95, 0, 9]) == 235
     assert lec5.sum_elem_method1([7]) == 7
