ff17c998ddf58ee82b419000cccb902a12aee0ab
Nick Wang <hchengwang@gmail.com>
2024-11-10 06:06:30 +0000

    feat(install): automate Neovim installation
    - Add `-y` flag to `add-apt-repository` and `apt` commands to automate
      the installation process.
    - Copy `install_init.vim` to Neovim config directory.
    - Run `PlugInstall` in headless mode to install plugins.

--- install_copilot.sh
+++ install_copilot.sh
@@ -1,6 +1,6 @@
-sudo add-apt-repository ppa:neovim-ppa/stable
+sudo add-apt-repository ppa:neovim-ppa/stable  -y
 sudo apt update -y
-sudo apt install neovim
+sudo apt install neovim  -y
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
 source ~/.bashrc
@@ -9,9 +9,9 @@ nvm install v18.19.1
 git clone https://github.com/github/copilot.vim \
    ~/.config/nvim/pack/github/start/copilot.vim
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-sudo add-apt-repository ppa:neovim-ppa/unstable
-sudo apt-get update
-sudo apt-get install neovim
+sudo add-apt-repository ppa:neovim-ppa/unstable  -y
+sudo apt-get update  -y
+sudo apt-get install neovim  -y
 git config --global core.editor nvim
-
-
+cp ./install_init.vim ~/.config/nvim/init.vim
+nvim --headless +PlugInstall +qall
