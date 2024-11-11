967ba116a730ffdb3de177ae019899e77129377f
James Hsu <145538660+jameshsu1973@users.noreply.github.com>
2024-11-11 12:27:43 +0800

    Update lec13_test.py

--- tests/group2/Lecture13/lec13_test.py
+++ tests/group2/Lecture13/lec13_test.py
@@ -1,4 +1,11 @@
-ef test_minkowski_dist():
+import pytest
+import add_path
+import random
+from mit_ocw_data_science.lec12.cluster import *
+import numpy as np
+from mit_ocw_data_science.lec12 import TitanicPassengers.txt
+
+def test_minkowski_dist():
     assert minkowski_dist([0,0], [3,4], 2) == 5.0
     assert minkowski_dist([1,2,3], [4,5,6], 1) == 9.0
     assert minkowski_dist([1,2,3], [4,5,6], 2) == 5.196152422706632
