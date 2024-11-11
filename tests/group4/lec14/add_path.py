3f69540faccaebd7d1ef2e0b95d488ba0b7bffa5
WoodyLiang <132034021+WoodyLiang@users.noreply.github.com>
2024-11-11 14:25:08 +0800

    Update add_path.py

--- tests/group4/lec14/add_path.py
+++ tests/group4/lec14/add_path.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 import os
-
-# 取得專案根目錄
-project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-file_path = os.path.join(project_root, 'src/mit_ocw_data_science/lec13/TitanicPassengers.txt')
-
+import sys
+sys.path.append(
+    os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                 '../../../src'))
