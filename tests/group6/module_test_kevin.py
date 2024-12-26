d931e1db7ede74f81e23f9f2ad94885a316d42a4
kevinrayrayray <kevinray.ee12@nycu.edu.tw>
2024-12-26 21:52:21 +0800

    lec 11 test kevin

---
+++ tests/group6/module_test_kevin.py
@@ -0,0 +1,58 @@
+import pytest
+import add_path
+import random
+from mit_ocw_data_science.lec11.lec11_module import *
+import numpy as np
+import time
+
+
+def test_variance():  #with denomiator = n
+    assert variance([2, 4, 6, 8, 10]) == 8.0
+    assert variance([1, 2, 3, 4, 5, 6]) == 2.9166666666666665
+    assert variance([10, 20, 30, 40, 50, 60, 70]) == 466.6666666666667
+    assert variance([5, 10, 15, 20, 25, 30, 35, 40]) == 122.5
+    assert variance([7, 14, 21, 28, 35, 42, 49, 56, 63]) == 322.6666666666667
+    assert variance([1, 3, 5, 7, 9, 11, 13, 15, 17, 19]) == 33.0
+    assert variance([2, 3, 5, 7, 11, 13, 17]) == 26.693877551020407
+    
+def test_variance_negative():  #with denomiator = n
+   assert variance([-2, -4, -6, -8, -10]) == 8.0
+    assert variance([-3, -6, -9, -12, -15, -18]) == 21.0
+    assert variance([-10, -20, -30, -40, -50, -60, -70]) == 466.6666666666667
+    assert variance([-5, -10, -15, -20, -25, -30, -35, -40]) == 122.5
+    assert variance([-7, -14, -21, -28, -35, -42, -49, -56, -63]) == 322.6666666666667
+    assert variance([-1, -3, -5, -7, -9, -11, -13, -15, -17, -19]) == 33.0
+    assert variance([-2, -3, -5, -7, -11, -13, -17]) == 26.693877551020407
+    
+def test_variance_random(): #with denomiator = n
+    for i in range(1000):
+        n = random.randint(1,100)
+        X = [random.randint(0,100) for i in range(n)]
+        assert variance(X) == sum([(x - sum(X)/n)**2 for x in X])/n
+
+# Test variance with single element
+def test_variance_single_element():
+    assert variance([10]) == 0.0
+    assert variance([-10]) == 0.0
+    assert variance([0]) == 0.0
+    assert variance([50]) == 0.0
+
+# Test variance with all same elements
+def test_variance_all_same_elements():
+    assert variance([8, 8, 8, 8, 8]) == 0.0
+    assert variance([-8, -8, -8, -8, -8]) == 0.0
+    assert variance([0, 0, 0, 0, 0]) == 0.0
+    
+def test_minkowskiDist_p(): #v1 = [1,2,3] v2 = [4,5,6] p = 1~10
+    v1 = [3, 6, 9]
+    v2 = [12, 15, 18]
+    for i in range(1,11):
+        assert minkowskiDist(v1, v2, i) == pytest.approx(np.linalg.norm(np.array(v1) - np.array(v2), ord=i))
+    
+                                                                  
+def test_minkowskiDist_random():
+    for i in range(1000):
+        v1 = [random.randint(0,100) for i in range(100)]
+        v2 = [random.randint(0,100) for i in range(100)]
+        p = random.randint(1,100)
+        assert minkowskiDist(v1, v2, p) == pytest.approx(np.linalg.norm(np.array(v1) - np.array(v2), ord=p))
