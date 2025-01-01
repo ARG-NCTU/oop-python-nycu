1f2f3adde53cc0e51861d6abcf34ac6fef4e78ef
Morgan119502 <fuliang.ee12@nycu.edu.tw>
2025-01-01 18:56:20 +0800

    morgan_lec5.py

--- tests/group6/morgan_lec5.py
+++ tests/group6/morgan_lec5.py
@@ -2,7 +2,6 @@ import add_path
 import mit_ocw_exercises.lec5_tuples_lists as lec5
 import pytest
 
-
 # please write a test for quotient_and_remainder function
 def test_quotient_and_remainder():
     assert lec5.quotient_and_remainder(27, 6) == (4, 3)
