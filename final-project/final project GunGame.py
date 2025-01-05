4a0af4816f3b289d2e97e77cfa8f3a4bda2cf3f9
billwang0517 <wgb.ee12@nycu.edu.tw>
2025-01-05 22:10:47 +0800

    final

--- final-project/final project GunGame.py
+++ final-project/final project GunGame.py
@@ -677,7 +677,7 @@ class Game():
             # 'player2_death_count': player2.death_count,
             # 'player2_hit_count': player2.hit_count,
             # 'player2_pickup_count': player2.pickup_count,
-            'player2_remain_life': player2.remain_life,
+            # 'player2_remain_life': player2.remain_life,
         }
 
         # 定义文件路径
