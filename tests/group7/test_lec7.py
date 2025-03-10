bf7ccb01babe88ad05a66826bfcab9c607b1157d
Tony104147 <tony104147@gmail.com>
2025-03-10 21:03:29 +0800

    update: test_lec7.py

--- tests/group7/test_lec7.py
+++ tests/group7/test_lec7.py
@@ -4,11 +4,11 @@ class Mat:
     __data: np.ndarray
 
     def __init__(self,
-                 mat: list[list] = None):
+                 mat: list = []):
         data = np.array(mat)
-        assert len(data.shape) == 2 or mat == None, 'Warning: shape format wrong'
+        assert len(data.shape) == 2 or mat == [], 'Warning: shape format wrong'
         self.__data = data
-    
+
     """ Set matrix by the input matrix """
     def set(self, mat):
         data = np.array(mat)
@@ -18,39 +18,50 @@ class Mat:
     """ Get shape of matrix """
     def shape(self)-> tuple:
         return self.__data.shape
-    
+
     # operator overridings
     ########################################################################
 
     """ Matrix addition """
     def __add__(self, other):
-        M = self.copy()
-        M += other
-        return M
-    
+        assert self.shape() == other.shape(), 'Error: matrix shape vary'
+        return Mat(np.vectorize(lambda a, b: a + b)(self.__data, other.__data))
+
     """ Matrix subtract """
     def __sub__(self, other):
-        M = self.copy()
-        M -= other
-        return M
-    
+        assert self.shape() == other.shape(), 'Error: matrix shape vary'
+        return Mat(np.vectorize(lambda a, b: a - b)(self.__data, other.__data))
+
     """ Matrix multiplication """
     def __mul__(self, other):
         assert self.__data.shape[1] == other.__data.shape[0], 'Warning: matrices shape not fit'
-        k = self.__data.shape[1]
-        M = np.vectorize()(self.__data, other.__data)
-        return 
-    
+
+        data = []
+        K = self.__data.shape[1]
+        for i in range(self.__data.shape[0]):
+            for j in range(other.__data.shape[1]):
+                data[i].append(self.__data[i][0] * other.__data[0][j])
+                for k in range(1, K):
+                    data[i][j] += self.__data[i][k] * other.__data[k][j]
+        return Mat(data)
+
     """ Get element """
     def __getitem__(self, key: tuple):
         return self.__data[key[0], key[1]]
-    
+
     ########################################################################
-    
+
     """ Get copy of matrix """
     def copy(self):
         return Mat(self.__data)
     
+    def transpose(self):
+        mat = np.zeros_like(self.__data, shape= (self.__data.shape[1], self.__data.shape[0]))
+        for i in np.mgrid[0:self.__data.shape[1]]:
+            for j in np.mgrid[0:self.__data.shape[0]]:
+                mat[i][j] = self.__data[j][i]
+        return Mat(mat)
+
     """ Print matrix """
     def print(self):
         print(self.__data)
@@ -63,8 +74,7 @@ m2 = Mat([[1, 1, 1],
           [1, 1, 1]])
 m3 = Mat()
 m3.print()
-m3 = m1 + m2
-print(m3[1, 2])
+m3 = m1 * m2
 
-m3.print()
+m3print()
 print(0)
