9277139c74477f141c037862172e81f605d3a8f0
jui-pixel <absta.ee13@nycu.edu.tw>
2025-03-10 14:33:41 +0800

    Update lec7_test_code.py

--- tests/group6/lec7_test_code.py
+++ tests/group6/lec7_test_code.py
@@ -1,3 +1,10 @@
 import add_path
 import pytest
 import mit_ocw_exercises.lec7_debug_except as lec7
+
+def test_reverse_list():
+  L = [1,2,3,4];
+  U = [4,3,2,1];
+  lec7.rev_list(L);
+  assert U[:] == L[:]
+  
