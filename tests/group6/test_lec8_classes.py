1d0aca6441fefc3c6a151277a20337240dec39bb
jui-pixel <absta.ee13@nycu.edu.tw>
2025-03-17 14:02:44 +0800

    Update test_lec8_classes.py
    add test_fraction()

--- tests/group6/test_lec8_classes.py
+++ tests/group6/test_lec8_classes.py
@@ -3,8 +3,22 @@ import mit_ocw_exercises.lec8_classes as lec8
 import pytest
 
 def test_coordinate():
-  coo1 = lec8.Coordinate(1,2);
-  coo2 = coo1 + lec8.Coordinate(5,5);
-  assert coo1.x == 1;
-  assert coo1.y == 2;
+  coo1 = lec8.Coordinate(3,4);
+  o = lec8.Coordinate(0,0)
+  assert coo1.x == 3;
+  assert coo1.y == 4;
+  assert str(coo1) == "<3,4>";
   assert coo1.distance(coo2) == 5;
+
+def test_fraction():
+  frac1 = lec8.Fraction(1, 4);
+  frac2 = lec8.Fraction(3, 4);
+  result = frac1 + frac2;
+  assert result.num == 16 and result.denom == 16;
+  result = frac2 - frac1;
+  assert result.num == 8 and result.denom == 16;
+  assert float(frac1) == 0.25;
+  assert float(frac2) == 0.75;
+  inverse_frac1 = frac1.inverse();
+  assert inverse_frac1.num == 4 and inverse_frac1.denom == 1;
+  
