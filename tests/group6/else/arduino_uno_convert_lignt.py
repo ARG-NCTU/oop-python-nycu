bd9f5bcc9becafa6356d183ee193cda2a8739f25
jui-pixel <absta.ee13@nycu.edu.tw>
2025-03-22 00:01:38 +0800

    Update arduino_uno_convert_lignt.py

--- tests/group6/else/arduino_uno_convert_lignt.py
+++ tests/group6/else/arduino_uno_convert_lignt.py
@@ -1,15 +1,15 @@
 import tkinter as tk
 from pyfirmata import Arduino, util
 
-# 初始化 Arduino 板和 LED 引腳
+# 設定 Arduino 板和 LED 引腳
 board = Arduino('/dev/ttyUSB0')
 led_pin = board.get_pin('d:13:o')
 
-# 啟動板的迭代器
+# 啟動
 it = util.Iterator(board)
 it.start()
 
-# 初始化 Tkinter 根窗口
+# 設定 Tkinter 根窗口
 root = tk.Tk()
 root.title("LED Control")
 
@@ -24,26 +24,26 @@ current_status_label.pack(pady=20)
 # LED 狀態變量
 ledstatus = 0
 
-# 打開 LED 的函數
+# 打開 LED
 def turn_on_led():
     led_pin.write(1)
     status_label.config(text="LED ON")
     print("LED ON")
 
-# 關閉 LED 的函數
+# 關閉 LED
 def turn_off_led():
     led_pin.write(0)
     status_label.config(text="LED OFF")
     print("LED OFF")
 
-# 更新當前 LED 狀態標籤的函數
+# 更新當前 LED 狀態標籤
 def update_status_label():
     if ledstatus == 0:
         current_status_label.config(text="Current LED Status: OFF")
     else:
         current_status_label.config(text="Current LED Status: ON")
 
-# 切換 LED 狀態的函數
+# 切換 LED 狀態
 def switch():
     global ledstatus
     if ledstatus == 0:
@@ -60,5 +60,5 @@ def switch():
 mybutton = tk.Button(root, text='turn on', font=('Arial', 30, 'bold'), command=switch)
 mybutton.pack()
 
-# 啟動 Tkinter 主循環
+# 啟動
 root.mainloop()
