3a16837f0890ba6cc8b328fb0400060e52e0a500
jing1688 <eric35398.mg11@nycu.edu.tw>
2025-03-23 11:32:13 +0800

    add test lec8

--- tests/group13/testing_code/test_lec8.py
+++ tests/group13/testing_code/test_lec8.py
@@ -3,10 +3,11 @@ import pytest
 import mit_ocw_exercises.lec8_classes as lec8
 import math
 def test_cor():
-    c = lec8.Coordinate(7, 12)
+    c = lec8.Coordinate(1, 2)
     origin = lec8.Coordinate(0,0)
-    assert c.x == 3
-    assert c.y == 4
+    assert c.x == 1
+    assert c.y == 2
     assert origin.x == origin.y == 0
-    assert c.distance(origin) == 5
-    assert origin.distance(c) == 5
+    assert c.distance(origin) == math.sqrt(5)
+    assert origin.distance(c) == math.sqrt(5)
+    assert str(c) == "<1,2>"
