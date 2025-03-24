09456da2ea53675d38d23b18bd822799a1af4ce6
jui-pixel <absta.ee13@nycu.edu.tw>
2025-03-24 14:22:44 +0800

    Update test_lec9_inheritance.py

--- tests/group6/lec_test_codes/test_lec9_inheritance.py
+++ tests/group6/lec_test_codes/test_lec9_inheritance.py
@@ -1,4 +1,23 @@
 import add_path
-import mit_ocw_exercises.lec9_inheritance as lec9
 import pytest
+from mit_ocw_exercises.lec9_inheritance import Animal, Cat, Person, Student, Rabbit
 
+def test_animal_init():
+    a = Animal(1)
+    assert a.get_age() == 1
+    assert a.get_name() is None
+  
+def test_animal_set_name():
+    a = Animal(5)
+    a.set_name("5")
+    assert a.get_name() == "5"
+
+def test_animal_set_age():
+    a = Animal(100)
+    a.set_age(4)
+    assert a.get_age() == 4
+
+def test_animal_str():
+    a = Animal(1)
+    a.set_name("test")
+    assert str(a) == "animal:test:1"
