c82ce36d9ce9484f91a05b8dd548006747c7d810
jing <absta.ee13@nycu.edu.com>
2025-03-24 18:28:01 +0800

    update

--- tests/group6/lec_test_codes/test_lec9_inheritance.py
+++ tests/group6/lec_test_codes/test_lec9_inheritance.py
@@ -1,26 +1,26 @@
 import add_path
 import pytest
 
-import mit_ocw_exercises.lec9_inheritance as Animal, Cat, Person, Student, Rabbit
+import mit_ocw_exercises.lec9_inheritance as lec9
 
 # class animal test
 def test_animal_init():
-    a = Animal(1)
+    a = lec9.Animal(1)
     assert a.get_age() == 1
     assert a.get_name() is None
   
 def test_animal_set_name():
-    a = Animal(5)
+    a = lec9.Animal(5)
     a.set_name("5")
     assert a.get_name() == "5"
 
 def test_animal_set_age():
-    a = Animal(100)
+    a = lec9.Animal(100)
     a.set_age(4)
     assert a.get_age() == 4
 
 def test_animal_str():
-    a = Animal(1)
+    a = lec9.Animal(1)
     a.set_name("test")
     assert str(a) == "animal:test:1"
 
@@ -28,19 +28,19 @@ def test_animal_str():
 # def test_cat_speak(capsys):
 
 def test_cat_str():
-    c = Cat(4)
+    c = lec9.Cat(4)
     c.set_name("a")
     assert str(c) == "cat:a:4"
 
 # person
 def test_person_init():
-    p = Person("a", 18)
+    p = lec9.Person("a", 18)
     assert p.get_name() == "a"
     assert p.get_age() == 18
     assert p.get_friends() == []
 
 def test_person_add_friend():
-    p = Person("Bob", 30)
+    p = lec9.Person("Bob", 30)
     p.add_friend("C")
     assert "C" in p.get_friends()
 
@@ -51,25 +51,25 @@ def test_person_add_friend():
 
 
 def test_person_str():
-    p = Person("e", 278)
+    p = lec9.Person("e", 278)
     assert str(p) == "person:e:278"
 
     
 # student
 def test_student_init():
-    s = Student("A", 200, "A")
+    s = lec9.Student("A", 200, "A")
     assert s.get_name() == "A"
     assert s.get_age() == 200
     assert s.major == "A"
 
 def test_student_change_major():
-    s = Student("qwer", 7, "A")
+    s = lec9.Student("qwer", 7, "A")
     s.change_major("B")
     assert s.major == "B"
 
 # def test_student_speak(capsys):
 
 def test_student_str():
-    s = Student("qaq", 21, "C")
+    s = lec9.Student("qaq", 21, "C")
     assert str(s) == "student:qaq:21:C"
 # rabbit
