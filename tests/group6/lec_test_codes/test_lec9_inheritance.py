c68d341a151539a91b8b4f60bb1216e5580b71e3
jui-pixel <absta.ee13@nycu.edu.tw>
2025-03-24 14:32:54 +0800

    Update test_lec9_inheritance.py

--- tests/group6/lec_test_codes/test_lec9_inheritance.py
+++ tests/group6/lec_test_codes/test_lec9_inheritance.py
@@ -2,6 +2,7 @@ import add_path
 import pytest
 from mit_ocw_exercises.lec9_inheritance import Animal, Cat, Person, Student, Rabbit
 
+# class animal test
 def test_animal_init():
     a = Animal(1)
     assert a.get_age() == 1
@@ -21,3 +22,38 @@ def test_animal_str():
     a = Animal(1)
     a.set_name("test")
     assert str(a) == "animal:test:1"
+
+# cat test
+# def test_cat_speak(capsys):
+
+def test_cat_str():
+    c = Cat(4)
+    c.set_name("a")
+    assert str(c) == "cat:a:4"
+
+# person
+def test_person_init():
+    p = Person("a", 18)
+    assert p.get_name() == "a"
+    assert p.get_age() == 18
+    assert p.get_friends() == []
+
+def test_person_add_friend():
+    p = Person("Bob", 30)
+    p.add_friend("C")
+    assert "C" in p.get_friends()
+
+# def test_person_speak(capsys):
+
+
+# def test_person_age_diff(capsys):
+
+
+def test_person_str():
+    p = Person("e", 278)
+    assert str(p) == "person:e:278"
+
+    
+# student
+
+# rabbit
