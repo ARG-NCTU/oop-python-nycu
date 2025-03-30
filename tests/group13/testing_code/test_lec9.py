31b9aa3f8c1136fc9b5567e8306936fd63696d81
jing1688 <eric35398.mg11@nycu.edu.tw>
2025-03-31 00:03:47 +0800

    add the object test

--- tests/group13/testing_code/test_lec9.py
+++ tests/group13/testing_code/test_lec9.py
@@ -1,3 +1,8 @@
 import add_path
 import pytest
 import mit_ocw_exercises.lec9_inheritance as lec9
+
+ele=lec9.Animal(4)
+def test_animal_init():
+    assert ele.age == 4
+    assert ele.name == None
