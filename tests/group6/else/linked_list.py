caa65dbaee4e88ff721ecc6303f5ae335216dd76
jui-pixel <absta.ee13@nycu.edu.tw>
2025-03-31 14:19:29 +0800

    Update linked_list.py

--- tests/group6/else/linked_list.py
+++ tests/group6/else/linked_list.py
@@ -1,8 +1,4 @@
-struct ListNode {
-  int val;
-  ListNode *next;
-  ListNode() : val(0), next(nullptr) {}
-  ListNode(int x) : val(x), next(nullptr) {}
-  ListNode(int x, ListNode *next) : val(x), next(next) {}
-};
-
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
