ca7e36296b900bbfbb17d627ed4269443dbafb8e
chen <a0966903085@gmail.com>
2025-03-31 14:16:48 +0800

    add test_lec2_testfood

--- tests/group10/test_lec2_116.py
+++ tests/group10/test_lec2_116.py
@@ -1,6 +1,6 @@
 import add_path
 import pytest
-import mit_ocw_data_science.lec2.menu.py as lec2
+import mit_ocw_data_science.lec2.menu as lec2
 import random
 
 def test_food():
@@ -10,6 +10,13 @@ def test_food():
     assert food.get_cost() == 95
     assert food.density() == pytest.approx(50 / 95)
     assert str(food) == "apple: <50, 95>"
+    
+    food2 = lec2.Food("neko", 1, 105000)
+    assert food2.get_value() == 1
+    assert food2.get_cost() == 105000
+    assert food2.density() == pytest.approx(1 / 105000)
+    assert str(food2) == "neko: <1, 105000>"
+    
 
 def test_menu():
     # 測試 Menu 類別的基本功能
