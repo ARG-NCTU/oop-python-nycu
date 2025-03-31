4a9494797f62e35608c2f226e97042c4fb04f0a7
jui-pixel <absta.ee13@nycu.edu.tw>
2025-03-31 14:22:43 +0800

    Update binary_tree.py

--- tests/group6/else/binary_tree.py
+++ tests/group6/else/binary_tree.py
@@ -1,1 +1,6 @@
-bb
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
