d4f72c05c647ff743a2f1b45c1b6fc7919811f37
peienwu1216 <peienwu.ee13@nycu.edu.tw>
2025-04-03 23:42:36 +0800

    group4: 測試 is_palindrome() 遞迴與清理字元

--- tests/group4/113511103/test_lec6_recursion_dictionaries.py
+++ tests/group4/113511103/test_lec6_recursion_dictionaries.py
@@ -11,3 +11,22 @@ def test_fib_basic():
     assert fib(2) == 2
     assert fib(3) == 3
     assert fib(5) == 8
+
+# Part 2: is_palindrome
+def is_palindrome(s): 
+    def to_chars(s):
+        s = s.lower()
+        return ''.join([c for c in s if c.isalpha()])
+    def is_pal(s):
+        if len(s) <= 1:
+            return True
+        else:
+            return s[0] == s[-1] and is_pal(s[1:-1]) 
+    return is_pal(to_chars(s))
+
+def test_is_palindrome():
+    assert is_palindrome("Able was I, ere I saw Elba")
+    assert is_palindrome("racecar")
+    assert not is_palindrome("python")
+    assert is_palindrome("a")
+    assert is_palindrome("")
