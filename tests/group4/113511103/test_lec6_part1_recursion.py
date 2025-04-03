1723a7b87ec863db6ac26d2bf92762b84fc5a189
peienwu1216 <peienwu.ee13@nycu.edu.tw>
2025-04-04 00:06:00 +0800

    group4: Commit 3 - 定義 is_palindrome() 與 to_chars()

--- tests/group4/113511103/test_lec6_part1_recursion.py
+++ tests/group4/113511103/test_lec6_part1_recursion.py
@@ -12,3 +12,14 @@ def test_fib_small():
     assert fib(2) == 2
     assert fib(3) == 3
 
+# === Commit 3: 定義 is_palindrome 函式（含 to_chars）===
+def is_palindrome(s):
+    def to_chars(s):
+        s = s.lower()
+        return ''.join([c for c in s if c.isalpha()])
+    def is_pal(s):
+        if len(s) <= 1:
+            return True
+        else:
+            return s[0] == s[-1] and is_pal(s[1:-1])
+    return is_pal(to_chars(s))
