339b95672e0c848a53a15f1b4e9b789f4fb51078
HSIN-HUI YANG <yxh.ee13@nycu.edu.tw>
2025-04-07 20:58:13 +0800

    update test_lec3graph_203.py

--- tests/group10/test_lec3graph_203.py
+++ tests/group10/test_lec3graph_203.py
@@ -17,3 +17,7 @@ class TestDigraph:
         assert not self.graph.has_node(self.node2)
         self.graph.add_node(self.node2)
         assert self.graph.has_node(self.node2)
+    def test_add_edge(self):
+        self.graph.add_node(self.node1)
+        self.graph.add_edge(Edge(self.node3, Node('4')))
+        assert Node('4') in self.graph.children_of(self.node3)
