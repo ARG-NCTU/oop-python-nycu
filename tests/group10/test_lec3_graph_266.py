7948157ac833eb1bcfe38a98d75d96a329a38ea7
xiaotin22 <terry941020@gmail.com>
2025-04-07 12:59:39 +0800

    add test_digraph into test_lec3_graph_266

--- tests/group10/test_lec3_graph_266.py
+++ tests/group10/test_lec3_graph_266.py
@@ -14,3 +14,29 @@ def test_edge():
     assert edge.getDestination() == node2
     assert str(edge) == "A->B"
 
+def test_digraph():
+    graph = lec3.Digraph()
+    node1 = lec3.Node("A")
+    node2 = lec3.Node("B")
+    node3 = lec3.Node("C")
+    
+    graph.addNode(node1)
+    graph.addNode(node2)
+    graph.addNode(node3)
+    
+    assert graph.hasNode(node1) == True
+    assert graph.hasNode(lec3.Node("D")) == False
+    
+    edge1 = lec3.Edge(node1, node2)
+    edge2 = lec3.Edge(node2, node3)
+    
+    graph.addEdge(edge1)
+    graph.addEdge(edge2)
+    
+    assert graph.childrenOf(node1) == [node2]
+    assert graph.childrenOf(node2) == [node3]
+    
+    with pytest.raises(ValueError):
+        graph.addEdge(edge1)  # Adding duplicate edge should raise ValueError
+
+
