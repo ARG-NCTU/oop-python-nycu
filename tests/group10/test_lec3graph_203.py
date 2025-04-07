ff2ad58607ea972aa251dcd7b90164995a50e94c
HSIN-HUI YANG <yxh.ee13@nycu.edu.tw>
2025-04-07 14:57:07 +0800

    modify test_lec3graph_203.py

--- tests/group10/test_lec3graph_203.py
+++ tests/group10/test_lec3graph_203.py
@@ -1,18 +1,19 @@
+import add_path
+import mit_ocw_data_science.lec3.lecture3_graph as lec3
 import pytest
-from lecture3_graph import *
 
 class TestDigraph:
     def setup_method(self):
-        self.digraph = Digraph()
-        self.node1 = Node('1')
-        self.node2 = Node('2')
-        self.node3 = Node('3')
-        self.edge1 = Edge(self.node1, self.node2)
-        self.edge2 = Edge(self.node2, self.node3)
+        self.graph = lec3.Digraph()
+        self.node1 = lec3.Node('1')
+        self.node2 = lec3.Node('2')
+        self.node3 = lec3.Node('3')
+        self.edge1 = lec3.Edge(self.node1, self.node2)
+        self.edge2 = lec3.Edge(self.node2, self.node3)
 
     def test_add_node(self):
-        self.digraph.add_node(self.node1)
+        self.graph.add_node(self.node1)
         assert self.graph.has_node(self.node1)
         assert not self.graph.has_node(self.node2)
-        self.digraph.add_node(self.node2)
+        self.graph.add_node(self.node2)
         assert self.graph.has_node(self.node2)
