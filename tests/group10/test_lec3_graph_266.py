0890b772036583d33ffefa90576fe52751fcd22f
xiaotin22 <terry941020@gmail.com>
2025-04-07 12:58:07 +0800

    add test_edge into test_lec3_graph_266.py

--- tests/group10/test_lec3_graph_266.py
+++ tests/group10/test_lec3_graph_266.py
@@ -5,3 +5,12 @@ def test_node():
     node = lec3.Node("A")
     assert node.getName() == "A"
     assert str(node) == "A"
+
+def test_edge():
+    node1 = lec3.Node("A")
+    node2 = lec3.Node("B")
+    edge = lec3.Edge(node1, node2)
+    assert edge.getSource() == node1
+    assert edge.getDestination() == node2
+    assert str(edge) == "A->B"
+
