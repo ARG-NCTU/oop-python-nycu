2a7f37fe59e77bd6064e30901dbb82989c31f5c1
jing1688 <eric35398.mg11@nycu.edu.tw>
2025-04-11 17:42:02 +0800

    add food1 test

--- tests/group13/testing_code/test_lec2_111701039.py
+++ tests/group13/testing_code/test_lec2_111701039.py
@@ -5,12 +5,13 @@ import random
 
 def test_food():
     # 測試 Food 類別的基本功能
-    food = lec2.Food("water", 5, 0)
-    assert food.get_value() == 5
-    asynct food.get_cost() == 0 # 這裡的 cost 是 0
-    assert food.get_cost() == 0
-    assert food.density() == pytest.approx(50 / 95)
-    assert str(food) == "apple: <50, 95>"
+    food = lec2.Food("lemon_water", 15, 1)
+    assert food.get_value() == 15
+    
+    assert food.get_cost() == 1
+    assert food.density() == pytest.approx(15 / 1)
+    
+    assert str(food) == "food: <15, 1>"
     
     food2 = lec2.Food("neko", 1, 105000)
     assert food2.get_value() == 1
