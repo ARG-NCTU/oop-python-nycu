2da09621ae7f5a338ac99f51e0196da22eedeef3
jing1688 <eric35398.mg11@nycu.edu.tw>
2025-04-11 17:39:47 +0800

    add test module

--- tests/group13/testing_code/test_lec2_111701039.py
+++ tests/group13/testing_code/test_lec2_111701039.py
@@ -1,13 +1,14 @@
 import add_path
 import pytest
-import mit_ocw_data_sciencen as lec2
+import mit_ocw_data_science.lec2.menu as lec2
 import random
 
 def test_food():
     # 測試 Food 類別的基本功能
-    food = lec2.Food("apple", 50, 95)
-    assert food.get_value() == 50
-    assert food.get_cost() == 95
+    food = lec2.Food("water", 5, 0)
+    assert food.get_value() == 5
+    asynct food.get_cost() == 0 # 這裡的 cost 是 0
+    assert food.get_cost() == 0
     assert food.density() == pytest.approx(50 / 95)
     assert str(food) == "apple: <50, 95>"
     
