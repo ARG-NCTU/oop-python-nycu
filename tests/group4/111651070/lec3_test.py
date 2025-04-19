c7dfb1869c6adb42135b11aea347e29666db0a7f
PO-FENG CHANG <107752584+JumboZhang1119@users.noreply.github.com>
2025-04-19 16:26:09 +0000

    Lec3: feat(tests): Add a new function to check the cube.
    * 新增 `guess_and_check_cube_root` 函數以檢查輸入的整數是否為完美立方數。
    * 更新測試流程以包含新的函數調用。

--- tests/group4/111651070/lec3_test.py
+++ tests/group4/111651070/lec3_test.py
@@ -1,10 +1,8 @@
-# Test 1: for loops over strings
 def for_loop_over_string_to_find_e(str):
     for char in str:
         if char == "e":
             print("found e")
 
-# Test 2: for loops over strings
 def while_loop_and_strings():
     an_letters = "aefhilmnorsxAEFHILMNORSX"
     word = input("I will cheer for you! Enter a word: ")
@@ -25,8 +23,21 @@ def perfect_cube():
        if guess**3 == cube:
            print("Cube root of", cube, "is", guess)
 
+def guess_and_check_cube_root():
+    cube = int(input("Enter an integer for cube: "))
+    for guess in range(abs(cube)+1):
+        if guess**3 >= abs(cube):
+           break
+    if guess**3 != abs(cube):
+        print(cube, 'is not a perfect cube')
+    elif cube < 0:
+        guess = -guess
+    else:
+        print('Cube root of ' + str(cube) + ' is ' + str(guess))
+
 #################################################
 s = 'hello'
 for_loop_over_string_to_find_e(s)
 while_loop_and_strings()
 perfect_cube()
+guess_and_check_cube_root()
