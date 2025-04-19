7b6ed71678857df384e555ecd08e19084cb4726c
PO-FENG CHANG <107752584+JumboZhang1119@users.noreply.github.com>
2025-04-19 16:11:12 +0000

    lec3: feat(tests): 新增字串迴圈測試函數
    * 新增 `while_loop_and_strings` 函數以測試字串迴圈
    * 更新測試流程以包含新的函數調用

--- tests/group4/111651070/lec3_test.py
+++ tests/group4/111651070/lec3_test.py
@@ -4,6 +4,22 @@ def for_loop_over_string_to_find_e(str):
         if char == "e":
             print("found e")
 
+# Test 2: for loops over strings
+def while_loop_and_strings():
+    an_letters = "aefhilmnorsxAEFHILMNORSX"
+    word = input("I will cheer for you! Enter a word: ")
+    times = int(input("Enthusiasm level (1-10): "))
+    for i in range(len(word)):
+        char = word[i]
+        if char in an_letters:
+            print("Give me an " + char + "! " + char)
+        else:
+            print("Give me a  " + char + "! " + char)
+    print("What does that spell?")
+    for i in range(times):
+        print(word, "!!!")
+
 #################################################
 s = 'hello'
 for_loop_over_string_to_find_e(s)
+while_loop_and_strings()
