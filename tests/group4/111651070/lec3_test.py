e067411e5d1d3d00281633d9d046d9e678e84ec2
PO-FENG CHANG <107752584+JumboZhang1119@users.noreply.github.com>
2025-04-19 16:02:37 +0000

    feat(tests): 新增字串迴圈測試函數
    * 新增函數 `for_loop_over_string_to_find_e` 用於檢查字串中是否包含字母 "e"
    * 測試字串 'hello' 以驗證函數功能

---
+++ tests/group4/111651070/lec3_test.py
@@ -0,0 +1,9 @@
+# Test 1: for loops over strings
+def for_loop_over_string_to_find_e(str):
+    for char in str:
+        if char == "e":
+            print("found e")
+
+#################################################
+s = 'hello'
+for_loop_over_string_to_find_e(s)
