6924addcaeab4ef2f24337c848844f822f457457
PO-FENG CHANG <107752584+JumboZhang1119@users.noreply.github.com>
2025-04-20 09:33:16 +0000

    lec4: feat(tests): Add a new function to check even number without return.
    * 新增 `is_even_without_return` 函數以檢查偶數
    * 更新測試以包含無回傳函數的測試案例

--- tests/group4/111651070/lec4_test.py
+++ tests/group4/111651070/lec4_test.py
@@ -3,6 +3,10 @@ def is_even_with_return( i ):
     remainder = i % 2
     return remainder == 0
 
+def is_even_without_return( i ):
+    print('without return')
+    remainder = i % 2
+
 
 #####################################
 print("=====================================")
@@ -11,3 +15,9 @@ print("=====================================")
 print(f"input: 20, output: {is_even_with_return(20)}")
 print("-------------------------------------")
 print(f"input: 21, output: {is_even_with_return(21)}")
+print("=====================================")
+print("**              Test2              **")
+print("=====================================")
+print(f"input: 20, output: {is_even_without_return(20)}")
+print("-------------------------------------")
+print(f"input: 21, output: {is_even_without_return(21)}")
