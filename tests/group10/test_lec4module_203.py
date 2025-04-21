bdf1184b443ab215363a3a4c1f3426f836208132
HSIN-HUI YANG <yxh.ee13@nycu.edu.tw>
2025-04-21 14:54:42 +0800

    add test_lec4module_203.py

---
+++ tests/group10/test_lec4module_203.py
@@ -0,0 +1,9 @@
+import random
+import pytest
+import math
+import mit_ocw_data_science.lec4.lec4_module as lec4
+
+def test_roll_die():
+    # Test the roll_die function
+    die = lec4.roll_die()
+    assert die >= 1 and die <= 6, "Die roll should be between 1 and 6"
