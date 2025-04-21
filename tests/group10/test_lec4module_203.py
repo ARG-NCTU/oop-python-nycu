43a81dbb1ae7bed8bba02f8b2f0aa590c66a2199
HSIN-HUI YANG <yxh.ee13@nycu.edu.tw>
2025-04-21 14:59:34 +0800

    update test_lec4module_203.py

--- tests/group10/test_lec4module_203.py
+++ tests/group10/test_lec4module_203.py
@@ -7,3 +7,10 @@ def test_roll_die():
     # Test the roll_die function
     die = lec4.roll_die()
     assert die >= 1 and die <= 6, "Die roll should be between 1 and 6"
+
+def test_roll():
+    n = 15
+    result = ''
+    for i in range(n):
+        result += str(lec4.roll_die())
+    assert len(result) == n, "Result should be of length n"
