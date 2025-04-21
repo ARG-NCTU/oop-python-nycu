5ad818cbada7fc932cf2f31d558241a07e049537
weiouo-0817 <jack930817.ee11@nycu.edu.tw>
2025-04-21 19:01:07 +0800

    add test_menu.py

--- tests/group14/test_menu.py
+++ tests/group14/test_menu.py
@@ -15,6 +15,23 @@ def test_menu():
     assert len(m.get_foods()) == 2
     assert str(m) == "apple: <10, 100>; banana: <20, 200>; "
 
+def test_greedy():
+    items = [lec2.Food("apple",10,100), lec2.Food("banana",20,200)]
+    max_cost = 150
+    key_function = lambda x: -x.get_cost()
+    result , val = lec2.greedy(items, max_cost, key_function)
+    
+    assert len(result) == 1
+    assert result[0].get_value() == 10
+    assert result[0].get_cost() == 100
+
+def test_max_val():
+    items = [lec2.Food("apple",10,100), lec2.Food("banana",20,200),lec2.Food("orange",30,150)]
+    max_cost = 250
+    result = lec2.max_val(items, max_cost)
+    
+    assert result[0] == 40
+
 
    
         
