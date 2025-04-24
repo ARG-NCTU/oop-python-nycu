91b6a75d2491f9d179e9caba7b7848cb5f58d73b
PO-FENG CHANG <107752584+JumboZhang1119@users.noreply.github.com>
2025-04-24 06:10:28 +0000

    Lec5: feat(tests): Add get_data function and test function
    * 實現 `get_data` 函數以處理元組數據
    * 增加對 `get_data` 的測試用例，驗證最小值、最大值及唯一詞數

--- tests/group4/111651070/lec5_test.py
+++ tests/group4/111651070/lec5_test.py
@@ -13,34 +13,37 @@ def test_quotient_and_remainder():
     assert rem == 6
 
 ## EXAMPLE: iterating over tuples
-# def get_data(aTuple):
-#     nums = ()    # empty tuple
-#     words = ()
-#     for t in aTuple:
-#         # concatenating with a singleton tuple
-#         nums = nums + (t[0],)
-#         # only add words haven't added before
-#         if t[1] not in words:
-#             words = words + (t[1],)
-#     min_n = min(nums)
-#     max_n = max(nums)
-#     unique_words = len(words)
-#     return (min_n, max_n, unique_words)
+def get_data(aTuple):
+    nums = ()    # empty tuple
+    words = ()
+    for t in aTuple:
+        # concatenating with a singleton tuple
+        nums = nums + (t[0],)
+        # only add words haven't added before
+        if t[1] not in words:
+            words = words + (t[1],)
+    min_n = min(nums)
+    max_n = max(nums)
+    unique_words = len(words)
+    return (min_n, max_n, unique_words)
 
-# test = ((1,"a"),(2, "b"),
-#         (1,"a"),(7,"b"))
-# (a, b, c) = get_data(test)
-# print("a:",a,"b:",b,"c:",c)
-
-# # apply to any data you want!
-# tswift = ((2014,"Katy"),
-#           (2014, "Harry"),
-#           (2012,"Jake"),
-#           (2010,"Taylor"),
-#           (2008,"Joe"))
-# (min_year, max_year, num_people) = get_data(tswift)
-# print("From", min_year, "to", max_year, \
-#         "Taylor Swift wrote songs about", num_people, "people!")
+def test_get_data():
+    test = ((1,"a"),(2, "b"),
+            (1,"c"),(7,"b"))
+    (a, b, c) = get_data(test)
+    assert a == 1
+    assert b == 7
+    assert c == 3
+    ##########################
+    test = ((2014,"Katy"),
+            (2014, "Harry"),
+            (2012,"Jake"),
+            (2010,"Taylor"),
+            (2008,"Joe"))
+    (a, b, c) = get_data(test)
+    assert a == 2008
+    assert b == 2014
+    assert c == 5
 
 # #########################
 # ## EXAMPLE: sum of elements in a list
