0f91614b507c00ded2d8e93e672370fd4e3601f2
PO-FENG CHANG <107752584+JumboZhang1119@users.noreply.github.com>
2025-04-24 06:12:09 +0000

    Lec5: feat(tests): Add sum_elem_method1/2 Function and tests
    * Implemented `sum_elem_method1` and `sum_elem_method2` functions.
    * Added tests for both methods to ensure correct functionality.
    * Included edge cases in tests for better coverage.

--- tests/group4/111651070/lec5_test.py
+++ tests/group4/111651070/lec5_test.py
@@ -45,24 +45,28 @@ def test_get_data():
     assert b == 2014
     assert c == 5
 
-# #########################
-# ## EXAMPLE: sum of elements in a list
-# #########################
-# def sum_elem_method1(L):
-#   total = 0
-#   for i in range(len(L)):
-#       total += L[i]
-#   return total
+## EXAMPLE: sum of elements in a list
+def sum_elem_method1(L):
+  total = 0
+  for i in range(len(L)):
+      total += L[i]
+  return total
 
-# def sum_elem_method2(L):
-#     total = 0
-#     for i in L:
-#         total += i
-#     return total
+def sum_elem_method2(L):
+    total = 0
+    for i in L:
+        total += i
+    return total
 
-# print(sum_elem_method1([1,2,3,4]))
-# print(sum_elem_method2([1,2,3,4]))
+def test_sum_elem_method1():
+    assert sum_elem_method1([1,2,3,4]) == 10
+    assert sum_elem_method1([0,0,0]) == 0
+    assert sum_elem_method1([-1,-2,-3]) == -6
 
+def test_sum_elem_method2():
+    assert sum_elem_method2([1,2,3,4]) == 10
+    assert sum_elem_method2([0,0,0]) == 0
+    assert sum_elem_method2([-1,-2,-3]) == -6
 
 # #########################
 # ## EXAMPLE: various list operations
