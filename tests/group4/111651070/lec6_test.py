4be36c833de81ad98d05893d6c2a69844a557c17
PO-FENG CHANG <107752584+JumboZhang1119@users.noreply.github.com>
2025-04-24 07:54:16 +0000

    Lec6: feat(tests): Add lyrics frequency analysis tests
    * Implemented `lyrics_to_frequencies` function to count word
      occurrences in lyrics.
    * Added tests for `lyrics_to_frequencies`, `most_common_words`,
      and `words_often` functions to ensure correct functionality.

--- tests/group4/111651070/lec6_test.py
+++ tests/group4/111651070/lec6_test.py
@@ -63,98 +63,112 @@ def test_is_palindrome():
     assert is_palindrome('Able was I, ere I saw Elba') == True
     assert is_palindrome('Is this a palindrome') == False
 
-# #####################################
-# # EXAMPLE: using dictionaries
-# #          counting frequencies of words in song lyrics
-# #####################################
+#####################################
+# EXAMPLE: using dictionaries
+#          counting frequencies of words in song lyrics
+#####################################
 
-# def lyrics_to_frequencies(lyrics):
-#     myDict = {}
-#     for word in lyrics:
-#         if word in myDict:
-#             myDict[word] += 1
-#         else:
-#             myDict[word] = 1
-#     return myDict
-
-
-# she_loves_you = ['she', 'loves', 'you', 'yeah', 'yeah',
-# 'yeah','she', 'loves', 'you', 'yeah', 'yeah', 'yeah',
-# 'she', 'loves', 'you', 'yeah', 'yeah', 'yeah',
-
-# 'you', 'think', "you've", 'lost', 'your', 'love',
-# 'well', 'i', 'saw', 'her', 'yesterday-yi-yay',
-# "it's", 'you', "she's", 'thinking', 'of',
-# 'and', 'she', 'told', 'me', 'what', 'to', 'say-yi-yay',
-
-# 'she', 'says', 'she', 'loves', 'you',
-# 'and', 'you', 'know', 'that', "can't", 'be', 'bad',
-# 'yes', 'she', 'loves', 'you',
-# 'and', 'you', 'know', 'you', 'should', 'be', 'glad',
-
-# 'she', 'said', 'you', 'hurt', 'her', 'so',
-# 'she', 'almost', 'lost', 'her', 'mind',
-# 'and', 'now', 'she', 'says', 'she', 'knows',
-# "you're", 'not', 'the', 'hurting', 'kind',
-
-# 'she', 'says', 'she', 'loves', 'you',
-# 'and', 'you', 'know', 'that', "can't", 'be', 'bad',
-# 'yes', 'she', 'loves', 'you',
-# 'and', 'you', 'know', 'you', 'should', 'be', 'glad',
-
-# 'oo', 'she', 'loves', 'you', 'yeah', 'yeah', 'yeah',
-# 'she', 'loves', 'you', 'yeah', 'yeah', 'yeah',
-# 'with', 'a', 'love', 'like', 'that',
-# 'you', 'know', 'you', 'should', 'be', 'glad',
-
-# 'you', 'know', "it's", 'up', 'to', 'you',
-# 'i', 'think', "it's", 'only', 'fair',
-# 'pride', 'can', 'hurt', 'you', 'too',
-# 'pologize', 'to', 'her',
-
-# 'Because', 'she', 'loves', 'you',
-# 'and', 'you', 'know', 'that', "can't", 'be', 'bad',
-# 'Yes', 'she', 'loves', 'you',
-# 'and', 'you', 'know', 'you', 'should', 'be', 'glad',
-
-# 'oo', 'she', 'loves', 'you', 'yeah', 'yeah', 'yeah',
-# 'she', 'loves', 'you', 'yeah', 'yeah', 'yeah',
-# 'with', 'a', 'love', 'like', 'that',
-# 'you', 'know', 'you', 'should', 'be', 'glad',
-# 'with', 'a', 'love', 'like', 'that',
-# 'you', 'know', 'you', 'should', 'be', 'glad',
-# 'with', 'a', 'love', 'like', 'that',
-# 'you', 'know', 'you', 'should', 'be', 'glad',
-# 'yeah', 'yeah', 'yeah',
-# 'yeah', 'yeah', 'yeah', 'yeah'
-# ]
-
-# beatles = lyrics_to_frequencies(she_loves_you)
+def lyrics_to_frequencies(lyrics):
+    # Like: {'she': 20, 'loves': 13, 'you': 36, 'yeah': 28, ...}
+    myDict = {}
+    for word in lyrics:
+        if word in myDict:
+            myDict[word] += 1
+        else:
+            myDict[word] = 1
+    return myDict
 
 
-# def most_common_words(freqs):
-#     values = freqs.values()
-#     best = max(freqs.values())
-#     words = []
-#     for k in freqs:
-#         if freqs[k] == best:
-#             words.append(k)
-#     return (words, best)
+she_loves_you = ['she', 'loves', 'you', 'yeah', 'yeah',
+'yeah','she', 'loves', 'you', 'yeah', 'yeah', 'yeah',
+'she', 'loves', 'you', 'yeah', 'yeah', 'yeah',
 
-# def words_often(freqs, minTimes):
-#     result = []
-#     done = False
-#     while not done:
-#         temp = most_common_words(freqs)
-#         if temp[1] >= minTimes:
-#             result.append(temp)
-#             for w in temp[0]:
-#                 del(freqs[w])
-#         else:
-#             done = True
-#     return result
+'you', 'think', "you've", 'lost', 'your', 'love',
+'well', 'i', 'saw', 'her', 'yesterday-yi-yay',
+"it's", 'you', "she's", 'thinking', 'of',
+'and', 'she', 'told', 'me', 'what', 'to', 'say-yi-yay',
 
-# #print(words_often(beatles, 5))
+'she', 'says', 'she', 'loves', 'you',
+'and', 'you', 'know', 'that', "can't", 'be', 'bad',
+'yes', 'she', 'loves', 'you',
+'and', 'you', 'know', 'you', 'should', 'be', 'glad',
+
+'she', 'said', 'you', 'hurt', 'her', 'so',
+'she', 'almost', 'lost', 'her', 'mind',
+'and', 'now', 'she', 'says', 'she', 'knows',
+"you're", 'not', 'the', 'hurting', 'kind',
+
+'she', 'says', 'she', 'loves', 'you',
+'and', 'you', 'know', 'that', "can't", 'be', 'bad',
+'yes', 'she', 'loves', 'you',
+'and', 'you', 'know', 'you', 'should', 'be', 'glad',
+
+'oo', 'she', 'loves', 'you', 'yeah', 'yeah', 'yeah',
+'she', 'loves', 'you', 'yeah', 'yeah', 'yeah',
+'with', 'a', 'love', 'like', 'that',
+'you', 'know', 'you', 'should', 'be', 'glad',
+
+'you', 'know', "it's", 'up', 'to', 'you',
+'i', 'think', "it's", 'only', 'fair',
+'pride', 'can', 'hurt', 'you', 'too',
+'pologize', 'to', 'her',
+
+'Because', 'she', 'loves', 'you',
+'and', 'you', 'know', 'that', "can't", 'be', 'bad',
+'Yes', 'she', 'loves', 'you',
+'and', 'you', 'know', 'you', 'should', 'be', 'glad',
+
+'oo', 'she', 'loves', 'you', 'yeah', 'yeah', 'yeah',
+'she', 'loves', 'you', 'yeah', 'yeah', 'yeah',
+'with', 'a', 'love', 'like', 'that',
+'you', 'know', 'you', 'should', 'be', 'glad',
+'with', 'a', 'love', 'like', 'that',
+'you', 'know', 'you', 'should', 'be', 'glad',
+'with', 'a', 'love', 'like', 'that',
+'you', 'know', 'you', 'should', 'be', 'glad',
+'yeah', 'yeah', 'yeah',
+'yeah', 'yeah', 'yeah', 'yeah'
+]
+
+def test_lyrics_to_frequencies():
+    beatles = lyrics_to_frequencies(she_loves_you)
+    assert beatles['she'] == 20
+    assert beatles['loves'] == 13
+    assert beatles['you'] == 36
+
+def most_common_words(freqs):
+    best = max(freqs.values())
+    words = []
+    # 逐一檢查所有跟best數量一樣多的word
+    for k in freqs:
+        if freqs[k] == best:
+            words.append(k)
+    return (words, best)
+
+def test_most_common_words():
+    beatles = lyrics_to_frequencies(she_loves_you)
+    # e.g. beatles[0] == ['you', 'she'...]
+    #      beatles[1] == 36
+    assert most_common_words(beatles) == (['you'], 36)
+
+def words_often(freqs, minTimes):
+    result = []
+    done = False
+    while not done:
+        temp = most_common_words(freqs)
+        if temp[1] >= minTimes: # 若字典中的某個word數量超過minTimes
+            result.append(temp) # 把這個word加入result
+            for w in temp[0]:   # 把這個word從字典中刪除
+                del(freqs[w])
+        else:
+            done = True
+    return result
+
+def test_words_often():
+    beatles = lyrics_to_frequencies(she_loves_you)
+    result = words_often(beatles, 5)
+    assert result[0] == (['you'], 36)
+    assert result[1] == (['yeah'], 28)
 
 # #####################################
 # # EXAMPLE: comparing fibonacci using memoization
