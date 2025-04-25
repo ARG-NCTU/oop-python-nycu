96c8f82bf9d1677c45d9c53957edb312a10c87ba
howardhung14 <howard11023331@gmail.com>
2025-04-25 14:40:14 +0800

    add lec12 testcode

---
+++ tests/group2/test_code/test_lec12_sorting_113101014.py
@@ -0,0 +1,12 @@
+import pytest
+import lec12_sorting as lec12
+import math
+
+def test_bubble_sort():
+    List = [4,3,2,1]
+    assert lec12.bubble_sort_np(List) == [1,2,3,4]
+    
+
+def test_merge_sort():
+    List = [4,3,2,1]
+    assert lec12.merge_sort_np(List) == [1,2,3,4]
