dc9a52da3aca4e1f631f862da99ee95bc8a23fcf
PO-FENG CHANG <107752584+JumboZhang1119@users.noreply.github.com>
2025-04-25 13:03:42 +0000

    Lec9: feat(tests): Implement Cat class and its test function
    * Added `Cat` class inheriting from `Animal`.
    * Implemented `speak` and `__str__` methods for `Cat`.
    * Created `test_Cat` function to validate `Cat` behavior.

--- tests/group4/111651070/lec9_test.py
+++ tests/group4/111651070/lec9_test.py
@@ -29,22 +29,22 @@ def test_Animal():
     assert str(a) == "animal:fluffy:4"
     a.set_name()
     assert str(a) == "animal::4"
-    
+
 #################################
 ## Inheritance example
 #################################
-# class Cat(Animal):
-#     def speak(self):
-#         print("meow")
-#     def __str__(self):
-#         return "cat:"+str(self.name)+":"+str(self.age)
+class Cat(Animal):
+    def speak(self):
+        print("meow")
+    def __str__(self):
+        return "cat:"+str(self.name)+":"+str(self.age)
 
-# print("\n---- cat tests ----")
-# c = Cat(5)
-# c.set_name("fluffy")
-# print(c)
-# c.speak()
-# print(c.get_age())
+def test_Cat():
+    c = Cat(5)
+    c.set_name("fluffy")
+    assert str(c) == "cat:fluffy:5"
+    c.speak()
+    assert c.get_age() == 5
 
 # #################################
 # ## Inheritance example
