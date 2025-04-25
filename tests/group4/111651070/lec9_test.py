55b6bf70c6a2c289fb84cd21de4da4de45487203
PO-FENG CHANG <107752584+JumboZhang1119@users.noreply.github.com>
2025-04-25 13:10:35 +0000

    Lec9: feat(tests): Add Person class and its test function
    * Implemented the `Person` class inheriting from `Animal`.
    * Added `test_Person` function to validate `Person` class methods.
    * Ensured proper functionality of `get_name`, `get_age`, and `__str__`.

--- tests/group4/111651070/lec9_test.py
+++ tests/group4/111651070/lec9_test.py
@@ -46,37 +46,35 @@ def test_Cat():
     c.speak()
     assert c.get_age() == 5
 
-# #################################
-# ## Inheritance example
-# #################################
-# class Person(Animal):
-#     def __init__(self, name, age):
-#         Animal.__init__(self, age)
-#         self.set_name(name)
-#         self.friends = []
-#     def get_friends(self):
-#         return self.friends
-#     def speak(self):
-#         print("hello")
-#     def add_friend(self, fname):
-#         if fname not in self.friends:
-#             self.friends.append(fname)
-#     def age_diff(self, other):
-#         diff = self.age - other.age
-#         print(abs(diff), "year difference")
-#     def __str__(self):
-#         return "person:"+str(self.name)+":"+str(self.age)
+#################################
+## Inheritance example
+#################################
+class Person(Animal):
+    def __init__(self, name, age):
+        Animal.__init__(self, age)
+        self.set_name(name)
+        self.friends = []
+    def get_friends(self):
+        return self.friends
+    def speak(self):
+        print("hello")
+    def add_friend(self, fname):
+        if fname not in self.friends:
+            self.friends.append(fname)
+    def age_diff(self, other):
+        diff = self.age - other.age
+        print(abs(diff), "year difference")
+    def __str__(self): # print / 使用字串都會呼叫此函式
+        return "person:"+str(self.name)+":"+str(self.age)
 
-# print("\n---- person tests ----")
-# p1 = Person("jack", 30)
-# p2 = Person("jill", 25)
-# print(p1.get_name())
-# print(p1.get_age())
-# print(p2.get_name())
-# print(p2.get_age())
-# print(p1)
-# p1.speak()
-# p1.age_diff(p2)
+def test_Person():
+    p1 = Person("jack", 30)
+    p2 = Person("jill", 25)
+    assert p1.get_name() == "jack"
+    assert p1.get_age() == 30
+    assert p2.get_name() == "jill"
+    assert p2.get_age() == 25
+    assert str(p1) == "person:jack:30"
 
 # #################################
 # ## Inheritance example
