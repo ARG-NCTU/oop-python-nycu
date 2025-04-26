704c1c403042fc4f9498d266b585d1bdd8364fbf
PO-FENG CHANG <107752584+JumboZhang1119@users.noreply.github.com>
2025-04-26 14:56:26 +0000

    Lec12: feat(tests): Add tests for non-printing sorting functions
    * Implemented `test_bubble_sort_np` to validate the
      non-printing bubble sort function.
    * Added `test_selection_sort_np` to ensure the
      correctness of the non-printing selection sort.
    * Created `test_merge_sort_np` for testing the
      non-printing merge sort functionality.

--- tests/group4/111651070/lec12_test.py
+++ tests/group4/111651070/lec12_test.py
@@ -76,6 +76,7 @@ def test_merge_sort():
     testList = [3, 1, 2, 6, 5, 4, 10, 8]
     assert merge_sort(testList) == [1, 2, 3, 4, 5, 6, 8, 10]
 
+# np: No Print
 def bubble_sort_np(L):
     swap = False
     while not swap:
@@ -88,6 +89,10 @@ def bubble_sort_np(L):
                 L[j-1] = temp
     return L
 
+def test_bubble_sort_np():
+    testList = [3, 1, 2, 6, 5, 4, 10, 8]
+    assert bubble_sort_np(testList) == [1, 2, 3, 4, 5, 6, 8, 10]
+
 def selection_sort_np(L):
     suffixSt = 0
     while suffixSt != len(L):
@@ -95,6 +100,11 @@ def selection_sort_np(L):
             if L[i] < L[suffixSt]:
                 L[suffixSt], L[i] = L[i], L[suffixSt]
         suffixSt += 1
+
+def test_selection_sort_np():
+    testList = [3, 1, 2, 6, 5, 4, 10, 8]
+    selection_sort_np(testList)
+    assert testList == [1, 2, 3, 4, 5, 6, 8, 10]
 
 
 def merge_np(left, right):
@@ -124,3 +134,6 @@ def merge_sort_np(L):
         right = merge_sort_np(L[middle:])
         return merge_np(left, right)
 
+def test_merge_sort_np():
+    testList = [3, 1, 2, 6, 5, 4, 10, 8]
+    assert merge_sort_np(testList) == [1, 2, 3, 4, 5, 6, 8, 10]
