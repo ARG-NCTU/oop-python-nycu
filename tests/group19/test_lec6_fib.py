1c4f06c2228d7f2fef84501e449946bf1d1802c8
113511080 <qs.ee13@nycu.edu.tw>
2025-04-26 19:17:46 +0800

    feat(tests): ✨ Update Fibonacci tests to use local function
    * Replaced calls to `lec6.fib` with a local `fib` function.
    * Ensured all Fibonacci test cases remain intact and functional.

--- tests/group19/test_lec6_fib.py
+++ tests/group19/test_lec6_fib.py
@@ -1,22 +1,25 @@
-import add_path
-import mit_ocw_exercises.lec6_recursion_dictionaries as lec6
 import pytest
 
+def fib(x):
+    if x == 0 or x == 1:
+        return 1
+    else:
+        return fib(x-1) + fib(x-2)
 
 def test_fib():
-    assert lec6.fib(0) == 1
-    assert lec6.fib(1) == 1
-    assert lec6.fib(2) == 2
-    assert lec6.fib(3) == 3
-    assert lec6.fib(4) == 5
-    assert lec6.fib(5) == 8
-    assert lec6.fib(6) == 13
-    assert lec6.fib(7) == 21
-    assert lec6.fib(8) == 34
-    assert lec6.fib(9) == 55
-    assert lec6.fib(10) == 89
-    assert lec6.fib(11) == 144
-    assert lec6.fib(12) == 233
-    assert lec6.fib(13) == 377
-    assert lec6.fib(14) == 610
-    assert lec6.fib(15) == 987
+    assert fib(0) == 1
+    assert fib(1) == 1
+    assert fib(2) == 2
+    assert fib(3) == 3
+    assert fib(4) == 5
+    assert fib(5) == 8
+    assert fib(6) == 13
+    assert fib(7) == 21
+    assert fib(8) == 34
+    assert fib(9) == 55
+    assert fib(10) == 89
+    assert fib(11) == 144
+    assert fib(12) == 233
+    assert fib(13) == 377
+    assert fib(14) == 610
+    assert fib(15) == 987
