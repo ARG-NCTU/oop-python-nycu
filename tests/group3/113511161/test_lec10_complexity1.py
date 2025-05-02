112bdc519c9693919b529bc0f1a201b3a6482b5e
JonathanYangSW <jonathanyangjonathanyang0923@gmail.com>
2025-05-02 16:07:52 +0800

    add lec10 testing code

---
+++ tests/group3/113511161/test_lec10_complexity1.py
@@ -0,0 +1,17 @@
+import add_path
+import mit_ocw_exercises.lec10_complexity_part1 as lec10
+import pytest
+import time
+import random
+def generateTestList(n:int) ->list[int]:
+	tl=[]
+	for i in range(n):
+		tl.append(random.randint(0,100))
+	return tl
+
+def test_linear_search():
+	n=random.randint(1,20)
+	testlist=generateTestList(n)
+	assert lec10.linear_search(testlist,testlist[0])==True
+	assert lec10.linear_search([],5) ==False
+def test_search():
