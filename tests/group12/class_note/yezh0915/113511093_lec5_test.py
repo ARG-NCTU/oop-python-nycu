c389a5d54e58aeec5923e5d1d4ea4af34f5707b8
yezh0915 <y1590263748@gmail.com>
2025-05-05 13:49:29 +0800

    Update 113511093_lec5_test.py
    add pytest code

--- tests/group12/class_note/yezh0915/113511093_lec5_test.py
+++ tests/group12/class_note/yezh0915/113511093_lec5_test.py
@@ -1,1 +1,15 @@
+import pytest
+from src.mit_ocw_exercises.lec5_tuples_lists import (
+    quotient_and_remainder,
+    get_data,
+    sum_elem_method1,
+    sum_elem_method2,
+    remove_dups,
+    remove_dups_new,
+)
+
+def test_quotient_and_remainder():
+    assert quotient_and_remainder(5, 3) == (1, 2)
+    assert quotient_and_remainder(10, 2) == (5, 0)
+    assert quotient_and_remainder(0, 5) == (0, 0)
 
