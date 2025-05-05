813a01e0d0db776e8d041afc7a79d8f3d989afeb
yezh0915 <y1590263748@gmail.com>
2025-05-05 13:42:51 +0800

    Update 113511093_lec4_test.py
    add more pytest code

--- tests/group12/class_note/yezh0915/113511093_lec4_test.py
+++ tests/group12/class_note/yezh0915/113511093_lec4_test.py
@@ -10,6 +10,12 @@ def test_is_even_with_return():
     assert is_even_with_return(2) == True
     assert is_even_with_return(3) == False
     assert is_even_with_return(0) == True
+def test_bisection_cuberoot_approx():
+    epsilon = 0.01
+    assert abs(bisection_cuberoot_approx(27, epsilon) - 3) < epsilon
+    assert abs(bisection_cuberoot_approx(64, epsilon) - 4) < epsilon
+    assert abs(bisection_cuberoot_approx(1, epsilon) - 1) < epsilon
+
 
 if __name__ == "__main__":
     pytest.main()
