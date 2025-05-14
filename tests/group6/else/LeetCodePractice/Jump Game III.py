1cf51756cf8fcbfa6cc4c49fbb49477de5c3126e
charles691 <kiki.ee13@nycu.edu.tw>
2025-05-14 21:40:06 +0800

    chore: 📝 更新註解以說明集合創建方式
    * 增加註解以提醒使用者不能用 {} 創建空集合。
    * 確保代碼可讀性，並提供額外的上下文。

--- tests/group6/else/LeetCodePractice/Jump Game III.py
+++ tests/group6/else/LeetCodePractice/Jump Game III.py
@@ -14,3 +14,8 @@ class Solution:
                 return find(index + arr[index]) or find(index - arr[index])
         
         return find(start)
+    #注意：不能用 {} 創建空集合（這會創建空字典），必須用 set()。
+
+#from typing import List
+
+#class Solution: def canReach(self, arr: List[int], start: int) -> bool: # 創建集合記錄已訪問的索引 visited = set()
