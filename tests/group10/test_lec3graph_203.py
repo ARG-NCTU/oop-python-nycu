45bd88743bcdabc4ca82f5567a3460d9c2edda6e
HSIN-HUI YANG <yxh.ee13@nycu.edu.tw>
2025-05-19 14:23:04 +0800

    Update test_lec3graph_203.py

--- tests/group10/test_lec3graph_203.py
+++ tests/group10/test_lec3graph_203.py
@@ -44,10 +44,10 @@ class TestDigraph:
         self.graph.add_node(self.node3)
         assert self.graph.get_node('3') == self.node3
 
-    @pytest.mark.xfail(raises=ValueError)
     def test_add_edge(self):
         self.graph.add_node(self.node1)
         self.graph.add_node(self.node2)
         self.graph.add_edge(self.edge1)
         assert self.node2 in self.graph.children_of(self.node1)
         self.graph.add_node(self.node3)
+        self.graph.add_edge(self.edge2)
