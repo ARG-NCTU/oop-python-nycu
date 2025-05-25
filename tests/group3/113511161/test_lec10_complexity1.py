73b378f38d88d83fba8c29dabc3819d87434526d
JonathanYangSW <jonathanyangjonathanyang0923@gmail.com>
2025-05-26 00:15:03 +0800

    finish lec10 testing code

--- tests/group3/113511161/test_lec10_complexity1.py
+++ tests/group3/113511161/test_lec10_complexity1.py
@@ -15,4 +15,27 @@ def test_linear_search():
 	assert lec10.linear_search(testlist,testlist[0])==True
 	assert lec10.linear_search([],5) ==False
 def test_search():
-	assert 1==1
+	l1=generateTestList(4)
+	assert lec10.search(l1,101) == False
+	assert lec10.search(l1,0) == False
+def test_isSubset():
+	l1=generateTestList(20)
+	l2=[]
+	for i in range(16):
+		l2.append(l1[i])
+		i+=2
+	assert lec10.isSubset(l2,l1) ==True
+def test_intersect():
+	l1=generateTestList(20)
+	l2=generateTestList(20)
+	l3=[]
+	for k in l1:
+		for m in l2:
+			if k==m:
+				l3.append(k)
+	re=[]
+	for e in l3:
+		if not(e in re):
+			re.append(e)
+	assert lec10.intersect(l1,l2)==re
+	assert lec10.intersect(l1,l2)!=[]
