bb34113aaabbbb0e1ca03fa332ec9f6d40c6abe1
yezh0915 <y1590263748@gmail.com>
2025-05-26 13:50:22 +0800

    Update 113511093_lec6_test.py

--- tests/group12/class_note/yezh0915/113511093_lec6_test.py
+++ tests/group12/class_note/yezh0915/113511093_lec6_test.py
@@ -8,3 +8,17 @@ def test_towers_of_hanoi(capsys):
     assert "move from A to B" in captured
     assert "move from C to B" in captured
 
+def test_fib():
+    assert lec6.fib(0) == 1
+    assert lec6.fib(1) == 1
+    assert lec6.fib(5) == 8
+    assert lec6.fib(6) == 13
+
+
+
+def test_fib_efficient():
+    d = {1: 1, 2: 2}
+    assert lec6.fib_efficient(5, d) == lec6.fib_efficient(4, d) + lec6.fib_efficient(3, d)
+    # The memoization should work
+    assert 5 in d
+    assert 3 in d
