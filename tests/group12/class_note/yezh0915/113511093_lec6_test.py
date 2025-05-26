831b7af10a5cb23ceeed351e4a978189125b64b2
yezh0915 <y1590263748@gmail.com>
2025-05-26 13:50:03 +0800

    Update 113511093_lec6_test.py

--- tests/group12/class_note/yezh0915/113511093_lec6_test.py
+++ tests/group12/class_note/yezh0915/113511093_lec6_test.py
@@ -1,1 +1,10 @@
+import pytest
+from src.mit_ocw_exercises import lec6_recursion_dictionaries as lec6
+
+def test_towers_of_hanoi(capsys):
+    lec6.Towers(2, 'A', 'B', 'C')
+    captured = capsys.readouterr().out
+    assert "move from A to C" in captured
+    assert "move from A to B" in captured
+    assert "move from C to B" in captured
 
