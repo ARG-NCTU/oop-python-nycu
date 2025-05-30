f3360d13f88dbbbfb4773e2dd7c13825d85680a3
yezh0915 <y1590263748@gmail.com>
2025-05-31 01:11:08 +0800

    Update 113511093_lec11_test.py

--- tests/group12/class_note/yezh0915/113511093_lec11_test.py
+++ tests/group12/class_note/yezh0915/113511093_lec11_test.py
@@ -1,1 +1,9 @@
+import pytest
+from src.mit_ocw_exercises import lec11_complexity_part2 as lec
+
+def test_bisect_search1_found():
+    L = list(range(100))
+    assert lec.bisect_search1(L, 76) is True
+    assert lec.bisect_search1(L, 0) is True
+    assert lec.bisect_search1(L, 99) is True
 
