21c6a718f2d96ea3868868bba013bf5d77fd94b7
yezh0915 <y1590263748@gmail.com>
2025-05-31 00:42:27 +0800

    Update 113511093_lec7_test.py

--- tests/group12/class_note/yezh0915/113511093_lec7_test.py
+++ tests/group12/class_note/yezh0915/113511093_lec7_test.py
@@ -1,1 +1,18 @@
+import pytest
+import math
+
+from src.mit_ocw_exercises.lec7_debug_except import rev_list, primes_list, get_ratios, get_stats, avg
+
+def test_rev_list():
+    L = [1, 2, 3, 4]
+    rev_list(L)
+    assert L == [4, 3, 2, 1]
+
+    L = []
+    rev_list(L)
+    assert L == []
+
+    L = [7]
+    rev_list(L)
+    assert L == [7]
 
