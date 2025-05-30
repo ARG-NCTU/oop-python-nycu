714da53168fb1d6c26d53e536472b26bb747d665
CHENG-JE <lcjbao@gmail.com>
2025-05-30 11:47:01 +0800

    Update add_path.py

--- tests/group6/lec_test_codes/add_path.py
+++ tests/group6/lec_test_codes/add_path.py
@@ -1,8 +1,6 @@
-# tests/group9/add_path.py
 import sys
 import os
 
-# 找到 src 資料夾，然後加入 sys.path
 project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../src'))
 if project_root not in sys.path:
     sys.path.insert(0, project_root)
