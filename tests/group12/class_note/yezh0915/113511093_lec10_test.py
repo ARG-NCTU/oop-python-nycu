0466875784d0215fe48efe0f72206fed95ac4dd2
yezh0915 <y1590263748@gmail.com>
2025-05-31 01:08:29 +0800

    Update 113511093_lec10_test.py

--- tests/group12/class_note/yezh0915/113511093_lec10_test.py
+++ tests/group12/class_note/yezh0915/113511093_lec10_test.py
@@ -6,4 +6,12 @@ def test_linear_search():
     assert linear_search([1, 2, 3, 4], 5) is False
     assert linear_search([], 1) is False
 
+def test_search():
+    assert search([1, 2, 3, 4, 5], 3) is True
+    assert search([1, 2, 3, 4, 5], 6) is False
+    assert search([], 1) is False
+    # test for sorted input (should stop early)
+    assert search([1, 2, 4, 10], 5) is False
+
+
 
