fc88c5bab2d208b3d8f5ecf1ea39c3a7c8879415
yezh0915 <y1590263748@gmail.com>
2025-05-31 00:43:40 +0800

    Update 113511093_lec7_test.py

--- tests/group12/class_note/yezh0915/113511093_lec7_test.py
+++ tests/group12/class_note/yezh0915/113511093_lec7_test.py
@@ -36,3 +36,18 @@ def test_avg():
     with pytest.raises(AssertionError):
         avg([])
 
+def test_get_stats():
+    test_grades = [
+        [['peter', 'parker'], [80.0, 70.0, 85.0]],
+        [['bruce', 'wayne'], [100.0, 80.0, 74.0]],
+        [['captain', 'america'], [80.0, 70.0, 96.0]],
+    ]
+    result = get_stats(test_grades)
+    assert result[0][0] == ['peter', 'parker']
+    assert result[0][1] == [80.0, 70.0, 85.0]
+    assert result[0][2] == pytest.approx(78.333, rel=1e-2)
+
+    # 空成绩列表断言异常
+    test_grades = [[['deadpool'], []]]
+    with pytest.raises(AssertionError):
+        get_stats(test_grades)
