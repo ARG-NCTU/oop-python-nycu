b8208cf6f769f3d0d3703d62817aee62b47d90fa
CHENG-JE <lcjbao@gmail.com>
2025-05-30 12:22:49 +0800

    Update add_path.py

--- tests/group9/add_path.py
+++ tests/group9/add_path.py
@@ -1,6 +1,6 @@
 import sys
 import os
 
-project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../.."))
+project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../src'))
 if project_root not in sys.path:
     sys.path.insert(0, project_root)
