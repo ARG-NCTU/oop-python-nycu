b53fd1d886e055eb906a404b99e4cbea7e5a89db
yezh0915 <y1590263748@gmail.com>
2025-05-31 01:13:09 +0800

    Update 113511093_lec11_test.py

--- tests/group12/class_note/yezh0915/113511093_lec11_test.py
+++ tests/group12/class_note/yezh0915/113511093_lec11_test.py
@@ -40,3 +40,14 @@ def test_genSubsets_multiple():
     result = lec.genSubsets([1, 2, 3])
     assert len(result) == 8  # 2^3 subsets
 
+def test_genSubsets_contains_all_subsets():
+    s = [1, 2, 3]
+    subsets = lec.genSubsets(s)
+    assert [] in subsets
+    assert [1] in subsets
+    assert [2] in subsets
+    assert [3] in subsets
+    assert [1,2] in subsets
+    assert [1,3] in subsets
+    assert [2,3] in subsets
+    assert [1,2,3] in subsets
