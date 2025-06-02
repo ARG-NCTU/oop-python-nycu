95ea681220a177fd47692a3d8ba4283a3602e279
Hazel-1212 <achievegoal9@gmail.com>
2025-06-02 19:20:47 +0800

    lec5 correction

--- tests/group8/hazel_113511170/lec5/test_lec5.py
+++ tests/group8/hazel_113511170/lec5/test_lec5.py
@@ -3,47 +3,49 @@
 #########################
 import lec5
 import pytest
+import add_path
 
+
+# please write a test for quotient_and_remainder function
 def test_quotient_and_remainder():
-    """
-    Function that returns the quotient and remainder of two numbers
-    x: int, y: int returns: tuple (quotient, remainder)
+    assert lec5.quotient_and_remainder(20, 6) == (3, 2)
+    assert lec5.quotient_and_remainder(20, 7) == (2, 6)
+    assert lec5.quotient_and_remainder(20, 8) == (2, 4)
+    assert lec5.quotient_and_remainder(20, 9) == (2, 2)
+    assert lec5.quotient_and_remainder(20, 10) == (2, 0)
+    assert lec5.quotient_and_remainder(20, 11) == (1, 9)
 
-    """
-    assert lec5.quotient_and_remainder(168,8)==(21,0)
-    assert lec5.quotient_and_remainder(91,11)==(8,3)
+    try:
+        lec5.quotient_and_remainder(20, 0)
+    except ZeroDivisionError:
+         print("ZeroDivisionError")
+    assert lec5.quotient_and_remainder(20, 11) == (1, 9)
 
-#########################
-## EXAMPLE: iterating over tuples
-#########################
-def test_get_data():
-    """
-    aTuple, tuple of tuples (int, string)
-    Extracts all integers from aTuple and sets 
-    them as elements in a new tuple. 
-    Extracts all unique strings from from aTuple 
-    and sets them as elements in a new tuple.
-    Returns a tuple of the minimum integer, the
-    maximum integer, and the number of unique strings
-    """
-    test = ((1,"a"),(2, "b"),
-        (1,"a"),(7,"b"))
-    assert lec5.get_data(test)==(1,7,2)
-    tswift = ((2014,"Katy"),
-          (2014, "Harry"),
-          (2012,"Jake"), 
-          (2010,"Taylor"), 
-          (2008,"Joe"))   
-    assert lec5.get_data(tswift)==(2008,2014,5)
 
-#########################
-## EXAMPLE: sum of elements in a list
-#########################
 def test_sum_elem_method1():
-    L=[1,2,3,4]
-    assert lec5.sum_elem_method1(L)==10
-  
-def test_sum_elem_method2():
-    L=[1,2,3,4]
-    assert lec5.sum_elem_method2(L)==10
+    assert lec5.sum_elem_method1([1, 2, 3, 4]) == 10
+    assert lec5.sum_elem_method1([-1, -2, -3, -4]) == -10
+    assert lec5.sum_elem_method1([13, 34, 0, 88, 88, 0, 9]) == 232
+    assert lec5.sum_elem_method1([5]) == 5
+    assert lec5.sum_elem_method1([]) == 0
 
+
+def test_sum_elem_method2():
+    assert lec5.sum_elem_method2([1, 2, 3, 4]) == 10
+    assert lec5.sum_elem_method2([-1, -2, -3, -4]) == -10
+    assert lec5.sum_elem_method2([13, 34, 0, 88, 88, 0, 9]) == 232
+    assert lec5.sum_elem_method2([5]) == 5
+    assert lec5.sum_elem_method2([]) == 0
+
+def new_test_quotient_and_remainder():
+    assert lec5.quotient_and_remainder(24, 6) == (4, 0)
+    assert lec5.quotient_and_remainder(25, 7) == (3, 4)
+    assert lec5.quotient_and_remainder(12, 8) == (1, 4)
+    assert lec5.quotient_and_remainder(30, 9) == (3, 3)
+    assert lec5.quotient_and_remainder(19, 10) == (1, 9)
+
+    try:
+        lec5.quotient_and_remainder(11, 0)
+    except ZeroDivisionError:
+         print("ZeroDivisionError")
+    assert lec5.quotient_and_remainder(23, 13) == (1, 10)
