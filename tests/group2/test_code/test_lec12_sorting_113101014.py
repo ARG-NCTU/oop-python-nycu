44a073475b20db8bc59c0fb2b67772b35b506d89
howardhung14 <howard11023331@gmail.com>
2025-06-06 17:14:49 +0800

    lec12v3.0

--- tests/group2/test_code/test_lec12_sorting_113101014.py
+++ tests/group2/test_code/test_lec12_sorting_113101014.py
@@ -12,3 +12,5 @@ def test_bubble_sort():
 def test_merge_sort():
     List = [4,3,2,1]
     assert lec12.merge_sort_np(List) == [1,2,3,4]
+    List2 = []
+    assert lec12.merge_sort_np(List2) == []
