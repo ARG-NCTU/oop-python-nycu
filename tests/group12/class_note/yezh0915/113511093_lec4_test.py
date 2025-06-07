de0bc0b04bde70d7996248b7a0a86e87ef9ac242
yezh0915 <y1590263748@gmail.com>
2025-06-07 17:26:54 +0800

    Update 113511093_lec4_test.py

--- tests/group12/class_note/yezh0915/113511093_lec4_test.py
+++ tests/group12/class_note/yezh0915/113511093_lec4_test.py
@@ -1,10 +1,5 @@
 import pytest
-from src.mit_ocw_exercises.lec4_functions import (
-    is_even_with_return,
-    bisection_cuberoot_approx,
-    f,
-    g
-)
+# from src.mit_ocw_exercises.lec4_functions import (is_even_with_return,bisection_cuberoot_approx,f,g)
 
 def test_is_even_with_return():
     assert is_even_with_return(2) == True
