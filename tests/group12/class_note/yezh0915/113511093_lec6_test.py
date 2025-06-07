36008f37a90d2c608bb5a04993b8de86efa7a3ec
yezh0915 <y1590263748@gmail.com>
2025-06-07 17:27:23 +0800

    Update 113511093_lec6_test.py

--- tests/group12/class_note/yezh0915/113511093_lec6_test.py
+++ tests/group12/class_note/yezh0915/113511093_lec6_test.py
@@ -1,5 +1,5 @@
 import pytest
-from src.mit_ocw_exercises import lec6_recursion_dictionaries as lec6
+#from src.mit_ocw_exercises import lec6_recursion_dictionaries as lec6
 
 def test_towers_of_hanoi(capsys):
     lec6.Towers(2, 'A', 'B', 'C')
