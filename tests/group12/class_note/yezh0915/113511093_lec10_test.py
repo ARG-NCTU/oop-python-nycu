b542df8fb0655c1c2f3865fb774f917c7f1ee204
yezh0915 <y1590263748@gmail.com>
2025-06-07 17:36:57 +0800

    Update 113511093_lec10_test.py

--- tests/group12/class_note/yezh0915/113511093_lec10_test.py
+++ tests/group12/class_note/yezh0915/113511093_lec10_test.py
@@ -1,5 +1,5 @@
 import pytest
-
+#from src.mit_ocw_exercises.lec10_complexity_part1 import linear_search, search, isSubset, intersect
 
 def test_linear_search():
     assert linear_search([1, 2, 3, 4], 3) is True
