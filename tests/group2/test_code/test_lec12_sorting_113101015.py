b5a58e60965d35f4f518ff181ef167917b3d2980
neoAurora <Zanghongjun0611@gmail.com>
2025-06-07 21:37:29 +0800

    update: more test code for lec12

--- tests/group2/test_code/test_lec12_sorting_113101015.py
+++ tests/group2/test_code/test_lec12_sorting_113101015.py
@@ -5,8 +5,12 @@ import math
 def test_bubble_sort():
     List = [4,2,3,1]
     assert lec12.bubble_sort(List) == [1,2,3,4]
+    List = [4,2,3,1,5,-1]
+    assert lec12.bubble_sort(List) == [-1,1,2,3,4,5]
 
 def test_merge_sort():
     List = [4,3,2,1]
     assert lec12.merge_sort(List) == [1,2,3,4]
+    List = [4,2,3,1,5,-1]
+    assert lec12.merge_sort(List) == [-1,1,2,3,4,5]
     #
