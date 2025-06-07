e502e7f83998ef8d52735b200854701ee4461842
neoAurora <Zanghongjun0611@gmail.com>
2025-06-07 21:37:06 +0800

    update: more test code for lec10

--- tests/group2/test_code/test_lec10_complexity_part1_113101015.py
+++ tests/group2/test_code/test_lec10_complexity_part1_113101015.py
@@ -8,6 +8,11 @@ def test_linear_search():
     assert lec10.linear_search(testList,3) == True
     assert lec10.linear_search(testList,27) == True
     assert lec10.linear_search(testList,2) == False
+    testList = [2,2,2,2,2,6,7,8]
+    assert lec10.linear_search(testList,1) == False
+    assert lec10.linear_search(testList,3) == False
+    assert lec10.linear_search(testList,27) == False
+    assert lec10.linear_search(testList,2) == True
 testSet = [1, 2, 3, 4, 5]
 testSet1 = [1, 5, 3]
 testSet2 = [1, 6]
@@ -16,6 +21,8 @@ def test_subset():
     assert lec10.isSubset(testSet,testSet) == True
     assert lec10.isSubset(testSet1,testSet) == True
     assert lec10.isSubset(testSet,testSet2) == False
+    assert lec10.isSubset(testSet2,testSet) == False
+    assert lec10.isSubset(testSet2,testSet2) == True
 
 def test_intersect():
     assert lec10.intersect(testSet,testSet1) == [1,3,5]
