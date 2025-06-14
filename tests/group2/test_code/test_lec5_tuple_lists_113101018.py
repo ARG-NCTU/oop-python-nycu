1a4e8ab32fceed94e2ee88d946e91282af442ac0
Lawrence16428 <a0905201129@gmail.com>
2025-06-14 15:04:00 +0800

    modify lec5code

--- tests/group2/test_code/test_lec5_tuple_lists_113101018.py
+++ tests/group2/test_code/test_lec5_tuple_lists_113101018.py
@@ -8,6 +8,7 @@ def test_quoNrem():
     assert lec5.quotient_and_remainder(9,  3) == (3, 0)
     assert lec5.quotient_and_remainder(-10, 4) == (-3, 2)
     assert lec5.quotient_and_remainder(18, 7) == (2, 4)
+    assert lec5.quotient_and_remainder(12, 6) == (2, 0)
 
 def test_divide_by_zero():
     with pytest.raises(ZeroDivisionError):
