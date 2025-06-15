2665e46f4ef7f905b31898f7fc839688e413922a
yezh0915 <y1590263748@gmail.com>
2025-06-15 18:38:50 +0800

    Update 113511093_lec4_test.py

--- tests/group12/class_note/yezh0915/113511093_lec4_test.py
+++ tests/group12/class_note/yezh0915/113511093_lec4_test.py
@@ -1,22 +1,24 @@
+import add_path
+import mit_ocw_exercises.lec4_functions as lec4
 import pytest
-# from src.mit_ocw_exercises.lec4_functions import (is_even_with_return,bisection_cuberoot_approx,f,g)
+
 
 def test_is_even_with_return():
-    assert is_even_with_return(2) == True
-    assert is_even_with_return(3) == False
-    assert is_even_with_return(0) == True
+    assert lec4.is_even_with_return(2) == True
+    assert lec4.is_even_with_return(3) == False
+    assert lec4.is_even_with_return(0) == True
 def test_bisection_cuberoot_approx():
     epsilon = 0.01
-    assert abs(bisection_cuberoot_approx(27, epsilon) - 3) < epsilon
-    assert abs(bisection_cuberoot_approx(64, epsilon) - 4) < epsilon
-    assert abs(bisection_cuberoot_approx(1, epsilon) - 1) < epsilon
+    assert lec4.abs(bisection_cuberoot_approx(27, epsilon) - 3) < epsilon
+    assert lec4.abs(bisection_cuberoot_approx(64, epsilon) - 4) < epsilon
+    assert lec4.abs(bisection_cuberoot_approx(1, epsilon) - 1) < epsilon
 def test_function_object_f():
     func = f()
-    assert func(2, 3) == 5
-    assert func(10, 20) == 30
+    assert lec4.func(2, 3) == 5
+    assert lec4.func(10, 20) == 30
 def test_scope_g():
     result = g(3)
-    assert result == 4
+    assert lec4.result == 4
 
 
 
