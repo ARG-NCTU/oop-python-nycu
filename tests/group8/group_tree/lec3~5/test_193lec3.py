ea021b99b9c8313bf22d686bedf4e745348baddb
tree <941014tree@gmail.com>
2025-06-17 17:06:01 +0800

    remove

--- tests/group8/group_tree/lec3~5/test_193lec3.py
+++
@@ -1,7 +0,0 @@
-import pytest
-import lec3
-
-def test_is_palindrome():
-    assert lec3.is_palindrome("Tree Lee RT")==False
-    assert lec3.is_palindrome("TreeLee")==False
-    assert lec3.is_palindrome("YesseY") ==True
