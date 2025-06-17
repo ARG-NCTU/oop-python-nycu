ea8ec5161c32cc3e6f04d0c309142a111318a891
kufanghua <fanghuaku.ee13@nycu.edu.tw>
2025-06-17 15:22:26 +0800

    Update 113511250_lec5_test.py

--- tests/group12/class_note/kufanghua/113511250_lec5_test.py
+++ tests/group12/class_note/kufanghua/113511250_lec5_test.py
@@ -7,3 +7,11 @@ def test_quotient_and_remainder():
     assert lec5.quotient_and_remainder(17, 5) == (3, 2)
     assert lec5.quotient_and_remainder(8, 4) == (2, 0)
     assert lec5.quotient_and_remainder(22, 7) == (3, 1)
+
+def test_get_data():
+    data1 = ((5, "apple"), (3, "banana"), (9, "apple"), (7, "cherry"))
+    assert lec5.get_data(data1) == (3, 9, 3)
+    data2 = ((100, "x"), (200, "y"), (300, "x"), (50, "z"))
+    assert lec5.get_data(data2) == (50, 300, 3)
+    data3 = ((1, "a"), (2, "b"), (1, "a"), (7, "b"))
+    assert lec5.get_data(data3) == (1, 7, 2)
