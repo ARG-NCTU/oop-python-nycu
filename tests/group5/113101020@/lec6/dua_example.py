ae04a39112753e81cd5ec0aa9dfc145afcd21561
WuHsuan <sammi.md13@nycu.edu.tw>
2025-06-17 15:58:31 +0800

    Add dua_example.py, pytest test, and Makefile

---
+++ tests/group5/113101020@/lec6/dua_example.py
@@ -0,0 +1,90 @@
+# dua_example.py
+
+def printMove(fr, to):
+    print('move from ' + str(fr) + ' to ' + str(to))
+
+def Towers(n, fr, to, spare):
+    if n == 1:
+        printMove(fr, to)
+    else:
+        Towers(n-1, fr, spare, to)
+        Towers(1, fr, to, spare)
+        Towers(n-1, spare, to, fr)
+
+
+def fib(x):
+    """assumes x an int >= 0, returns Fibonacci of x"""
+    if x == 0 or x == 1:
+        return 1
+    else:
+        return fib(x-1) + fib(x-2)
+
+
+def is_palindrome(s):
+    def to_chars(s):
+        s = s.lower()
+        ans = ''
+        for c in s:
+            if c in 'abcdefghijklmnopqrstuvwxyz':
+                ans = ans + c
+        return ans
+
+    def is_pal(s):
+        if len(s) <= 1:
+            return True
+        else:
+            return s[0] == s[-1] and is_pal(s[1:-1])
+
+    return is_pal(to_chars(s))
+
+
+def lyrics_to_frequencies(lyrics):
+    myDict = {}
+    for word in lyrics:
+        if word in myDict:
+            myDict[word] += 1
+        else:
+            myDict[word] = 1
+    return myDict
+
+
+def most_common_words(freqs):
+    values = freqs.values()
+    best = max(values)
+    words = []
+    for k in freqs:
+        if freqs[k] == best:
+            words.append(k)
+    return (words, best)
+
+
+def words_often(freqs, minTimes):
+    result = []
+    done = False
+    while not done:
+        temp = most_common_words(freqs)
+        if temp[1] >= minTimes:
+            result.append(temp)
+            for w in temp[0]:
+                del(freqs[w])
+        else:
+            done = True
+    return result
+
+
+def fib_mem(n):
+    if n == 1:
+        return 1
+    elif n == 2:
+        return 2
+    else:
+        return fib_mem(n-1) + fib_mem(n-2)
+
+
+def fib_efficient(n, d):
+    if n in d:
+        return d[n]
+    else:
+        ans = fib_efficient(n-1, d) + fib_efficient(n-2, d)
+        d[n] = ans
+        return ans
