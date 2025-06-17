4fb50994523e7da4c58bd9faf7981484052dd9e6
kufanghua <fanghuaku.ee13@nycu.edu.tw>
2025-06-17 15:45:23 +0800

    Update 113511250_lec7_test.py

--- tests/group12/class_note/kufanghua/113511250_lec7_test.py
+++ tests/group12/class_note/kufanghua/113511250_lec7_test.py
@@ -1,1 +1,22 @@
+import math
+import pytest
+
+from src.mit_ocw_exercises.lec7_debug_except import rev_list, primes_list, get_ratios, avg
+import add_path
+
+def test_rev_list():
+    data = [9, 8, 7, 6]
+    expected = [6, 7, 8, 9]
+    rev_list(data)
+    assert data == expected
+
+    data = []
+    expected = []
+    rev_list(data)
+    assert data == expected
+
+    data = [11]
+    expected = [11]
+    rev_list(data)
+    assert data == expected
 
