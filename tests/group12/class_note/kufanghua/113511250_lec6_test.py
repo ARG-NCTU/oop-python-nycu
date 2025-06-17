84a01513fbe4bcaaf6782ab511153290320cfd5a
kufanghua <fanghuaku.ee13@nycu.edu.tw>
2025-06-17 15:31:45 +0800

    Update 113511250_lec6_test.py

--- tests/group12/class_note/kufanghua/113511250_lec6_test.py
+++ tests/group12/class_note/kufanghua/113511250_lec6_test.py
@@ -6,3 +6,9 @@ def test_fib():
     assert lec6.fib(1) == 1
     assert lec6.fib(5) == 8
     assert lec6.fib(6) == 13
+def test_is_palindrome():
+    assert lec6.is_palindrome('eve') is True
+    assert lec6.is_palindrome('Able was I, ere I saw Elba') is True
+    assert lec6.is_palindrome('Is this a palindrome') is False
+    assert lec6.is_palindrome('A man, a plan, a canal, Panama') is True
+
