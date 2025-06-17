416175f67762f859537f1aed404e969dc911c178
kufanghua <fanghuaku.ee13@nycu.edu.tw>
2025-06-17 14:49:08 +0800

    Update 113511250_lec5_test.py

--- tests/group12/class_note/kufanghua/113511250_lec5_test.py
+++ tests/group12/class_note/kufanghua/113511250_lec5_test.py
@@ -1,1 +1,9 @@
+import add_path  # if your repo requires this to set up sys.path
+import mit_ocw_exercises.lec5_tuples_lists as lec5
+import pytest
 
+def test_quotient_and_remainder():
+    assert lec5.quotient_and_remainder(10, 3) == (3, 1)
+    assert lec5.quotient_and_remainder(17, 5) == (3, 2)
+    assert lec5.quotient_and_remainder(8, 4) == (2, 0)
+    assert lec5.quotient_and_remainder(22, 7) == (3, 1)
