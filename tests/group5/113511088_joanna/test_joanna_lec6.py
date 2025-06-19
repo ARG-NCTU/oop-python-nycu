735d9c5e8b4bc6915a90ccc9952b4e486ca70f1d
Joanna <joanna.ee13@nycu.edu.tw>
2025-06-19 21:13:18 +0800

    lec6

--- tests/group5/113511088_joanna/test_joanna_lec6.py
+++ tests/group5/113511088_joanna/test_joanna_lec6.py
@@ -4,9 +4,11 @@ def test_towers_of_hanoi():
     moves = towers_of_hanoi(2, 'A', 'C', 'B')
     assert moves == ['move from A to B', 'move from A to C', 'move from B to C']
 
+
 def test_fib():
     assert fib(0) == 1
     assert fib(4) == 5
+
 
 def test_is_palindrome():
     assert is_palindrome("Able was I, ere I saw Elba")
