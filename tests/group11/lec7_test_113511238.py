3a6bf59c97cc7fe5c442343113557286c6587a0f
hfchiang <chianghungfan@gmail.com>
2025-06-19 21:58:18 +0800

    Update lec7_test_113511238.py

--- tests/group11/lec7_test_113511238.py
+++ tests/group11/lec7_test_113511238.py
@@ -23,3 +23,14 @@ def test_quotient_and_remainder_negative(a, b, expected):
 def test_quotient_and_remainder_zero_division():
     with pytest.raises(ZeroDivisionError):
         lec5.quotient_and_remainder(5, 0)
+
+# --- Test for get_data ---
+@pytest.mark.parametrize("data, expected", [
+    (((5, "apple"), (3, "banana"), (9, "apple"), (7, "cherry")), (3, 9, 3)),
+    (((100, "x"), (200, "y"), (300, "x"), (50, "z")), (50, 300, 3)),
+    (((1, "a"), (2, "b"), (1, "a"), (7, "b")), (1, 7, 2)),
+    (((10, "mango"),), (10, 10, 1)),
+    (((1, "x"), (2, "x"), (3, "x")), (1, 3, 1)),
+])
+def test_get_data(data, expected):
+    assert lec5.get_data(data) == expected
