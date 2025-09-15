a32b9d97ca2ad90b3d961217f8c76881c72fd6f4
jui-pixel <bxc529@gmail.com>
2025-09-15 13:28:48 +0800

    Update test_lec9_inheritance.py

--- tests/group_12/lec_test_codes/test_lec9_inheritance.py
+++ tests/group_12/lec_test_codes/test_lec9_inheritance.py
@@ -3,7 +3,7 @@ import mit_ocw_exercises.lec9_inheritance as l9
 import pytest
 
 def test_animal_basic():
-    a = Animal(4)
+    a = l9.Animal(4)
     assert a.get_age() == 4
     assert a.get_name() is None
     a.set_name("test_animal_a")
@@ -11,3 +11,4 @@ def test_animal_basic():
     a.set_age(10)
     assert a.get_age() == 10
     assert "animal:fluffy:10" == str(a)
+
