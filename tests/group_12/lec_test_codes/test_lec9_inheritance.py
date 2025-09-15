e4850ee93d6e4df0671abb9053e9ff2fd2d3bdae
jui-pixel <bxc529@gmail.com>
2025-09-15 13:32:45 +0800

    Update test_lec9_inheritance.py

--- tests/group_12/lec_test_codes/test_lec9_inheritance.py
+++ tests/group_12/lec_test_codes/test_lec9_inheritance.py
@@ -12,3 +12,25 @@ def test_animal_basic():
     assert a.get_age() == 10
     assert "animal:fluffy:10" == str(a)
 
+def test_cat_inheritance(capsys):
+    b = l9.Cat(5)
+    b.set_name("test_animal_b")
+    assert str(c) == "cat:test_animal_b:5"
+    b.speak()
+    captured = capsys.readouterr()
+    assert "meow" in captured.out
+
+def test_person_methods(capsys):
+    p1 = l9.Person("P1", 101)
+    p2 = l9.Person("P2", 102)
+    assert p1.get_name() == "P123"
+    assert p2.get_age() == 1
+    p1.add_friend("Nobody")
+    assert p1.get_friends() == ["Nobody"]
+    p1.speak()
+    captured = capsys.readouterr()
+    assert "hello" in captured.out
+    p1.age_diff(p2)
+    captured = capsys.readouterr()
+    assert "100 year difference" in captured.out
+    assert "person:P123:101" == str(p1)
