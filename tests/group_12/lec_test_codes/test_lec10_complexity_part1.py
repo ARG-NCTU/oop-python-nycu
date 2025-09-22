2fdc203fde0f563e24e84260418f6a0c3ffa5427
jui-pixel <bxc529@gmail.com>
2025-09-22 14:00:59 +0800

    Update test_lec10_complexity_part1.py

--- tests/group_12/lec_test_codes/test_lec10_complexity_part1.py
+++ tests/group_12/lec_test_codes/test_lec10_complexity_part1.py
@@ -17,3 +17,22 @@ def test_search():
     assert l10.search([1], 1) == True
     assert l10.search(testList, 0) == False
 
+def test_isSubset():
+    testSet = [1, 2, 3, 4, 5]
+    testSet1 = [1, 5, 3]
+    testSet2 = [1, 6]
+    assert l10.isSubset(testSet1, testSet) == True
+    assert l10.isSubset(testSet2, testSet) == False
+    assert l10.isSubset([], testSet) == True
+    assert l10.isSubset(testSet, []) == False
+    assert l10.isSubset([], []) == True
+
+def test_intersect():
+    testSet = [1, 2, 3, 4, 5]
+    testSet1 = [1, 5, 3]
+    testSet2 = [1, 6]
+    assert sorted(l10.intersect(testSet, testSet1)) == [1, 3, 5]
+    assert sorted(l10.intersect(testSet, testSet2)) == [1]
+    assert l10.intersect(testSet, []) == []
+    assert l10.intersect([], []) == []
+    assert sorted(l10.intersect([1, 1, 2], [1, 1, 3])) == [1]
