c1baa5a51cfa109aa097f5041f4bf436d0a73136
jui-pixel <bxc529@gmail.com>
2025-09-22 13:59:09 +0800

    Update test_lec10_complexity_part1.py

--- tests/group_12/lec_test_codes/test_lec10_complexity_part1.py
+++ tests/group_12/lec_test_codes/test_lec10_complexity_part1.py
@@ -8,6 +8,12 @@ def test_linear_search():
     assert l10.linear_search(testList, 10) == False
     assert l10.linear_search([], 5) == False
     assert l10.linear_search([5,], 5) == True
-    
+
 def test_search():
-    pass
+    testList = [1, 3, 4, 5, 9, 18, 27]
+    assert l10.search(testList, 5) == True
+    assert l10.search(testList, 10) == False
+    assert l10.search([], 1) == False
+    assert l10.search([1], 1) == True
+    assert l10.search(testList, 0) == False
+
