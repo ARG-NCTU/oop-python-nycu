8017fe6bfdd7e18aeec642e7b778957d1365180f
jui-pixel <bxc529@gmail.com>
2025-10-13 14:15:04 +0800

    add test_bubble_sort_np

--- tests/group12/lec_test_codes/test_lec12_sorting.py
+++ tests/group12/lec_test_codes/test_lec12_sorting.py
@@ -39,3 +39,11 @@ def test_merge_sort():
     assert l12.merge_sort([1, 2]) == [1, 2]
     assert l12.merge_sort([1, 1, 1]) == [1, 1, 1]
 
+def test_bubble_sort_np():
+    testList = [5, 3, 6, 2, 10, 1]
+    assert l12.bubble_sort_np(testList) == [1, 2, 3, 5, 6, 10]
+    assert l12.bubble_sort_np([]) == []
+    assert l12.bubble_sort_np([1]) == [1]
+    assert l12.bubble_sort_np([2, 1]) == [1, 2]
+    assert l12.bubble_sort_np([1, 2]) == [1, 2]
+    assert l12.bubble_sort_np([1, 1, 1]) == [1, 1, 1]
