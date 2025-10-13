1e6e990659c3b9a7c051eddeb65cf2c843e40c42
jui-pixel <bxc529@gmail.com>
2025-10-13 14:13:36 +0800

    add test mergesort

--- tests/group12/lec_test_codes/test_lec12_sorting.py
+++ tests/group12/lec_test_codes/test_lec12_sorting.py
@@ -29,3 +29,13 @@ def test_merge():
     assert l12.merge([4, 5, 6], [1, 2, 3]) == [1, 2, 3, 4, 5, 6]
     assert l12.merge([1, 3, 5], [2]) == [1, 2, 3, 5]
     assert l12.merge([2], [1, 3, 5]) == [1, 2, 3, 5]
+
+def test_merge_sort():
+    testList = [5, 3, 6, 2, 10, 1]
+    assert l12.merge_sort(testList) == [1, 2, 3, 5, 6, 10]
+    assert l12.merge_sort([]) == []
+    assert l12.merge_sort([1]) == [1]
+    assert l12.merge_sort([2, 1]) == [1, 2]
+    assert l12.merge_sort([1, 2]) == [1, 2]
+    assert l12.merge_sort([1, 1, 1]) == [1, 1, 1]
+
