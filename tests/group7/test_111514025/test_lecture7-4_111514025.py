ec4771e568c22bdc0ba929b99991d6011172dd27
Waynelee0124 <waynelee.ee11@nycu.edu.tw>
2025-10-25 16:30:00 +0800

    Exercise7-4

--- tests/group7/test_111514025/test_lecture7-4_111514025.py
+++ tests/group7/test_111514025/test_lecture7-4_111514025.py
@@ -1,41 +1,3 @@
-########################################
-### EXAMPLE: Buggy code to reverse a list
-### Try to debug it! (fixes needed are explained below)
-########################################
-##def rev_list_buggy(L):
-##    """
-##    input: L, a list
-##    Modifies L such that its elements are in reverse order
-##    returns: nothing
-##    """
-##    for i in range(len(L)):
-##        j = len(L) - i
-##        L[i] = temp
-##        L[i] = L[j]
-##        L[j] = L[i]
-#
-## FIXES: --------------------------
-## temp unknown
-## list index out of range -> sub 1 to j
-## get same list back -> iterate only over half
-## --------------------------
-def rev_list(L):
-    """
-    input: L, a list
-    Modifies L such that its elements are in reverse order
-    returns: nothing
-    """
-    for i in range(len(L)//2):
-        j = len(L) - i - 1
-        temp = L[i]
-        L[i] = L[j]
-        L[j] = temp
-        
-L = [1,2,3,4]
-rev_list(L)
-print(L)
-
-
 
 
 
@@ -62,6 +24,17 @@ def get_ratios(L1, L2):
     
 print(get_ratios([1, 4], [2, 4]))
 
+######################################
+print(get_ratios([1, 4], [2, 0]))
+######################################
+print(get_ratios([1, 4], [2]))
 
 
 #print(get_stats(test_grades))
+def test_get_ratios():
+    assert get_ratios([1, 2, 3], [1, 2, 3]) == [1.0, 1.0, 1.0]
+    assert get_ratios([1, 2, 3], [1, 0, 3]) == [1.0, float('nan'), 1.0]
+    try:
+        get_ratios([1, 2], [1])
+    except ValueError as e:
+        assert str(e) == 'get_ratios called with bad arg'
