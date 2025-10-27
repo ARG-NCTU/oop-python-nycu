92b842e31860f9c74a2f7a7e4a24a8d0949db01f
jui-pixel <bxc529@gmail.com>
2025-10-27 14:03:17 +0800

    add lec11 testing code

---
+++ tests/group12/lec_test_codes/test_lec11_complexity_part2.py
@@ -0,0 +1,21 @@
+import lec_test_codes.add_path
+import mit_ocw_exercises.lec11_complexity_part2 as l11
+import pytest
+
+
+#############################
+# Test bisect_search1
+#############################
+def test_bisect_search1_found():
+    L = list(range(0, 100))
+    assert l11.bisect_search1(L, 0) is True
+    assert l11.bisect_search1(L, 50) is True
+    assert l11.bisect_search1(L, 99) is True
+
+
+def test_bisect_search1_not_found():
+    L = list(range(0, 100))
+    assert l11.bisect_search1(L, -1) is False
+    
+    assert l11.bisect_search1(L, 100) is False
+    assert l11.bisect_search1([], 5) is False
