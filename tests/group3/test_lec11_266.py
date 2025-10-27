87a6ab1d7289116428feded7eeb9b7e67d4c9f44
xiaotin22 <terry941020@gmail.com>
2025-10-27 14:42:19 +0800

    feat(tests): add tests for bisect_search2 function 新增 bisect_search2 函數的測試，涵蓋多種情況以確保 功能正確性。

--- tests/group3/test_lec11_266.py
+++ tests/group3/test_lec11_266.py
@@ -10,3 +10,11 @@ def test_bisect_search1():
     assert lec11.bisect_search1([None], 5) == False
     assert lec11.bisect_search1([90], 90) == True
     assert lec11.bisect_search1([64], 8) == False
+
+def test_bisect_search2():
+    testList = list(range(600))
+    assert lec11.bisect_search2(testList, 512) == True
+    assert lec11.bisect_search2(testList, 1000) == False
+    assert lec11.bisect_search2([], 10) == False
+    assert lec11.bisect_search2([80], 80) == True
+    assert lec11.bisect_search2([55], 7) == False
