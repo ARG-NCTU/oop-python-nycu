fa3ff3e583bcbe13a080718fa0d950a7f210ad1b
Bryson¨git config --global user.name Bryson¨ <brysonhung.ee13@nycu.edu.tw>
2025-11-10 01:30:56 +0800

    feat: add tests for binary search, union, and difference
    - Implement tests for binary search, union, and difference functions.
    - Add parameterized tests for random sizes in union and difference.
    - Ensure all tests validate expected outcomes for various inputs.

--- tests/group1/test_le11_complexity_part2.py
+++ tests/group1/test_le11_complexity_part2.py
@@ -1,26 +1,70 @@
-from lec11_complexity_part2 import binary_search, merge_sorted, union, difference
+from lec11_complexity_part2 import bisect_search1, bisect_search2, genSubsets, genPerms
 import pytest
 import add_path
 import random
 def test_binary_search_found():
     L = list(range(100))
     e = 42
-    assert binary_search(L, e) == True
+    assert bisect_search1(L, e) == True
 
 def test_binary_search_not_found():
     L = list(range(100))
     e = 150
-    assert binary_search(L, e) == False
+    assert bisect_search1(L, e) == False
 
-def test_merge_sorted():
-    L1 = [1, 3, 5]
-    L2 = [2, 4, 6]
-    assert merge_sorted(L1, L2) == [1, 2, 3, 4, 5, 6]
+def test_binary_search2_found():
+    L = list(range(100))
+    e = 42
+    assert bisect_search2(L, e) == True
 
-def test_union():
-    L1 = [1, 2, 3]
-    L2 = [3, 4, 5]
-    assert union(L1, L2) == [1, 2, 3, 4, 5]
+def test_binary_search2_not_found():
+    L = list(range(100))
+    e = 150
+    assert bisect_search2(L, e) == False
+
+def union(L1, L2):
+    res = []
+    i, j = 0, 0
+    while i < len(L1) and j < len(L2):
+        if L1[i] < L2[j]:
+            if L1[i] not in res:
+                res.append(L1[i])
+            i += 1
+        elif L1[i] > L2[j]:
+            if L2[j] not in res:
+                res.append(L2[j])
+            j += 1
+        else:
+            if L1[i] not in res:
+                res.append(L1[i])
+            i += 1
+            j += 1
+    while i < len(L1):
+        if L1[i] not in res:
+            res.append(L1[i])
+        i += 1
+    while j < len(L2):
+        if L2[j] not in res:
+            res.append(L2[j])
+        j += 1
+    return res
+
+def difference(L1, L2):
+    res = []
+    i, j = 0, 0
+    while i < len(L1) and j < len(L2):
+        if L1[i] < L2[j]:
+            res.append(L1[i])
+            i += 1
+        elif L1[i] > L2[j]:
+            j += 1
+        else:
+            i += 1
+            j += 1
+    while i < len(L1):
+        res.append(L1[i])
+        i += 1
+    return res
 
 def test_difference():
     L1 = [1, 2, 3, 4]
