6e613021cd1e1578c7cf7fe60a5f1f064c9408c5
Joanna <joanna.ee13@nycu.edu.tw>
2025-11-17 16:00:06 +0800

    create lec5-2

--- tests/group2/113511088/lec5_tuples_lists_113511088.py
+++ tests/group2/113511088/lec5_tuples_lists_113511088.py
@@ -1,23 +1,40 @@
 #########################
-# EXAMPLE: returning a tuple
+## EXAMPLE: iterating over tuples
 #########################
-
-
-def quotient_and_remainder(x, y):
+def get_data(aTuple):
     """
-    Function that returns the quotient and remainder of two numbers.
-
-    x: int
-    y: int
-    returns: tuple (quotient, remainder)
+    aTuple, tuple of tuples (int, string)
+    Extracts all integers from aTuple and sets 
+    them as elements in a new tuple. 
+    Extracts all unique strings from from aTuple 
+    and sets them as elements in a new tuple.
+    Returns a tuple of the minimum integer, the
+    maximum integer, and the number of unique strings
     """
-    q = x // y
-    r = x % y
-    return (q, r)
+    nums = ()    # empty tuple
+    words = ()
+    for t in aTuple:
+        # concatenating with a singleton tuple
+        nums = nums + (t[0],)   
+        # only add words haven't added before
+        if t[1] not in words:   
+            words = words + (t[1],)
+    min_n = min(nums)
+    max_n = max(nums)
+    unique_words = len(words)
+    return (min_n, max_n, unique_words)
 
+test = ((1,"a"),(2, "b"),
+        (1,"a"),(7,"b"))
+(a, b, c) = get_data(test)
+print("a:",a,"b:",b,"c:",c)
 
-if __name__ == "__main__":
-    # 小 demo：直接跑這個檔案時才會執行
-    (quot, rem) = quotient_and_remainder(5, 3)
-    print(quot)
-    print(rem)
+# apply to any data you want!
+tswift = ((2014,"Katy"),
+          (2014, "Harry"),
+          (2012,"Jake"), 
+          (2010,"Taylor"), 
+          (2008,"Joe"))    
+(min_year, max_year, num_people) = get_data(tswift)
+print("From", min_year, "to", max_year, \
+        "Taylor Swift wrote songs about", num_people, "people!")
