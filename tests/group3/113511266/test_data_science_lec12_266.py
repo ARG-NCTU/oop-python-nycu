b1106ca213915fb199d5daecfe9de9d632e104ae
xiaotin22 <terry941020@gmail.com>
2025-11-24 14:27:20 +0800

    feat: add tests for cluster and dissimilarity functions
    - Implement tests for the Cluster class to verify its behavior.
    - Add tests for dissimilarity function to ensure correct output.

--- tests/group3/113511266/test_data_science_lec12_266.py
+++ tests/group3/113511266/test_data_science_lec12_266.py
@@ -23,5 +23,23 @@ def test_example():
     assert str(ex) == "Tim:[1, 5, 9, 48]:eating"
 
 
+def test_cluster():
+    ex0 = Example("Tim", [1, 5, 9, 48], "eating")
+    ex1 = Example("Bob", [21, 75, 49, 8], "sleeping")
+    cluster = Cluster([ex0, ex1])
+    assert cluster.examples == [ex0, ex1]
+    assert cluster.centroid.getName() == "centroid"
+    assert cluster.centroid.dimensionality() == 4
+    assert cluster.centroid.getLabel() is None
+    assert cluster.getCentroid() == cluster.centroid
+    assert cluster.variability() == 4250.0
+    assert list(cluster.members()) == [ex0, ex1]
+    assert str(cluster) == "Cluster with centroid [11. 40. 29. 28.] contains:\n  Bob, Tim"
+    assert cluster.update([ex0, ex1]) == 0.0
 
+def test_dissimilarity():
+    ex0 = Example("Tim", [1, 5, 9, 48], "eating")
+    ex1 = Example("Bob", [21, 75, 49, 8], "sleeping")
+    cluster = Cluster([ex0, ex1])
+    assert dissimilarity([cluster]) == 4250.0
 
