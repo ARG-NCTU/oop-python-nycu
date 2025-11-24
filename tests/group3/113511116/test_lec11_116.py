c71d0906fef14aaabab816e560568bd4bfbeb066
tpvupu <a0966903085@gmail.com>
2025-11-24 14:07:52 +0800

    feat: add tests for bisect_search functions
    Added unit tests for bisect_search1 and bisect_search2 to ensure
    correct functionality and output verification.

---
+++ tests/group3/113511116/test_lec11_116.py
@@ -0,0 +1,17 @@
+import add_path
+import mit_ocw_exercises.lec11_complexity_part2 as lec11
+import pytest
+
+def test_bisect_search1 (capsys):
+    testList = list(range(100))
+    lec11.bisect_search1(testList, 76)
+    captured = capsys.readouterr()  
+    assert 'low: 0; high: 99' in captured.out
+
+
+def test_bisect_search2 (capsys):
+    testList = list(range(100))
+    lec11.bisect_search2(testList, 76)
+    captured = capsys.readouterr()  
+    assert 'low: 0; high: 99' in captured.out
+    
