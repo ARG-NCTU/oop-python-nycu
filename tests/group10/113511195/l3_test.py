1992432c582e785299e14ee46b39dff124f5b0db
billy <billyyc44.ee13@nycu.edu.tw>
2025-11-29 00:29:12 +0800

    add test for lec3

---
+++ tests/group10/113511195/l3_test.py
@@ -0,0 +1,11 @@
+import add_path
+import lec3_string_algos as lec3 # type: ignore
+import pytest
+import random
+
+def test_is_palindrome():
+    assert lec3.is_palindrome("racecar") == True
+    assert lec3.is_palindrome("Racecar") == True
+    assert lec3.is_palindrome("hello") == False
+    assert lec3.is_palindrome("A man a plan a canal Panama") == True
+    assert lec3.is_palindrome("") == True
