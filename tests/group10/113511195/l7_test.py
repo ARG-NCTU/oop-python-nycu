83ddac356b90c20bba9c5bdfdf2da79bd7995dc1
billy <billyyc44.ee13@nycu.edu.tw>
2025-11-29 00:35:52 +0800

    start testing lec6

---
+++ tests/group10/113511195/l7_test.py
@@ -0,0 +1,18 @@
+import add_path
+import lec7_debug_except as lec7 # type: ignore
+import pytest
+import random
+
+def test_rev_list():
+    L = [1, 2, 3, 4, 5, 6]
+    lec7.rev_list(L)
+    assert L == [6, 5, 4, 3, 2, 1]
+    L = ['q', 'w', 'e', 'r', 't', 'y']
+    lec7.rev_list(L)
+    assert L == ['y', 't', 'r', 'e', 'w', 'q']
+    L = []
+    lec7.rev_list(L)
+    assert L == []
+    L = [1]
+    lec7.rev_list(L)
+    assert L == [1]
