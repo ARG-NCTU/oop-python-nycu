590c97e9b75d2be08b9697a22a6ee6b124e89fde
billy <billyyc44.ee13@nycu.edu.tw>
2025-11-29 00:46:46 +0800

    fix lec3 there were some problems, thought spaces were ignored

--- tests/group10/113511195/l3_test.py
+++ tests/group10/113511195/l3_test.py
@@ -1,5 +1,5 @@
 import add_path
-import lec3_string_algos as lec3 # type: ignore
+import lec3_strings_algos as lec3 # type: ignore
 import pytest
 import random
 
@@ -7,7 +7,8 @@ def test_is_palindrome():
     assert lec3.is_palindrome("racecar") == True
     assert lec3.is_palindrome("Racecar") == True
     assert lec3.is_palindrome("hello") == False
-    assert lec3.is_palindrome("A man a plan a canal Panama") == True
+    assert lec3.is_palindrome("A man a plan a canal Panama") == False
+    assert lec3.is_palindrome("AmanaplanacanalPanama") == True
     assert lec3.is_palindrome("") == True
 
 def test_is_palindrome_random():
