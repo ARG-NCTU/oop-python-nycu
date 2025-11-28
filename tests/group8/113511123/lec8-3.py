e0a7a0a544d09edd2ce71fd709d603e372744d6f
hatthebutterfly <johnsonchen0801@gmail.com>
2025-11-28 17:03:43 +0800

    add several lecture code

---
+++ tests/group8/113511123/lec8-3.py
@@ -0,0 +1,59 @@
+##############
+## EXAMPLE: a set of integers as class
+##############
+class intSet(object):
+    """
+    An intSet is a set of integers
+    The value is represented by a list of ints, self.vals
+    Each int in the set occurs in self.vals exactly once
+    """
+    def __init__(self):
+        """ Create an empty set of integers """
+        self.vals = []
+
+    def insert(self, e):
+        """ Assumes e is an integer and inserts e into self """
+        if not e in self.vals:
+            self.vals.append(e)
+
+    def member(self, e):
+        """ Assumes e is an integer
+        Returns True if e is in self, and False otherwise """
+        return e in self.vals
+
+    def remove(self, e):
+        """ Assumes e is an integer and removes e from self
+        Raises ValueError if e is not in self """
+        try:
+            self.vals.remove(e)
+        except:
+            raise ValueError(str(e) + ' not found')
+
+    def __str__(self):
+        """ Returns a string representation of self """
+        self.vals.sort()
+        return '{' + ','.join([str(e) for e in self.vals]) + '}'
+    
+    
+def test_intSet():
+    s = intSet()
+    assert str(s) == "{}"  # 初始集合應該是空的
+
+    s.insert(3)
+    s.insert(4)
+    s.insert(4)  # 重複插入應該無效
+    assert str(s) == "{3,4}"
+
+    assert s.member(4) is True
+    assert s.member(54) is False
+
+    s.insert(6)
+    assert str(s) == "{3,4,6}"
+
+    s.remove(3)
+    assert str(s) == "{4,6}"
+
+    try:
+        s.remove(3)  # 應該拋出 ValueError
+    except ValueError as e:
+        assert str(e) == "3 not found"
