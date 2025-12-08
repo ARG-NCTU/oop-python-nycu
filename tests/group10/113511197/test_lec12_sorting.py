413087d2287bda2fc268bc68bda744338b6f474e
Kyle Hsu <167048613+iamkyleh@users.noreply.github.com>
2025-12-08 14:28:22 +0800

    Update test_lec12_sorting.py

--- tests/group10/113511197/test_lec12_sorting.py
+++ tests/group10/113511197/test_lec12_sorting.py
@@ -13,38 +13,24 @@ def test_bubble_sort_np():
     assert bubble_sort_np(L3) == [1, 2, 3]
     L4 = [2, 1, 3, 2, 1]
     assert bubble_sort_np(L4) == [1, 1, 2, 2, 3]
-    
-    # 測試含負數
     L5 = [-10, 5, 0, -2]
     assert bubble_sort_np(L5) == [-10, -2, 0, 5]
-    
-    # 測試邊界：空列表
     assert bubble_sort_np([]) == []
-    
-    # 測試邊界：單一元素
     assert bubble_sort_np([42]) == [42]
-
-# -----------------
-# 測試 Selection Sort (就地修改, 無回傳值)
-# -----------------
 def test_selection_sort_np():
-    # 測試基本案例
+
     L1 = [5, 1, 4, 2, 8]
-    return_val = selection_sort_np(L1) # 呼叫函式
-    assert return_val is None           # 1. 斷言它沒有回傳值
-    assert L1 == [1, 2, 4, 5, 8]      # 2. 斷言「原始列表」被修改了
-    
-    # 測試反向
+    return_val = selection_sort_np(L1) 
+    assert return_val is None          
+    assert L1 == [1, 2, 4, 5, 8]      
     L3 = [3, 2, 1]
     selection_sort_np(L3)
     assert L3 == [1, 2, 3]
-    
-    # 測試含重複
     L4 = [2, 1, 3, 2, 1]
     selection_sort_np(L4)
     assert L4 == [1, 1, 2, 2, 3]
     
-    # 測試邊界：空列表
+ 
     L_empty = []
     selection_sort_np(L_empty)
     assert L_empty == []
