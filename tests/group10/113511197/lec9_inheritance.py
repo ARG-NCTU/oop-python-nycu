39008ebc1c76529fbc8c07a05be154b44c62d019
Kyle Hsu <167048613+iamkyleh@users.noreply.github.com>
2025-12-08 15:14:52 +0800

    Refactor imports from l9 to lecture9

--- tests/group10/113511197/lec9_inheritance.py
+++ tests/group10/113511197/lec9_inheritance.py
@@ -1,9 +1,9 @@
 import lec_test_codes.add_path
-import mit_ocw_exercises.lec9_inheritance as l9
+import mit_ocw_exercises.lec9_inheritance as lecture9
 import pytest
 
 def test_animal_basic():
-    a = l9.Animal(4)
+    a = lecture9.Animal(4)
     assert a.get_age() == 4
     assert a.get_name() is None
     a.set_name("test_animal_a")
@@ -13,7 +13,7 @@ def test_animal_basic():
     assert "animal:test_animal_a:10" == str(a)
 
 def test_cat_inheritance(capsys):
-    b = l9.Cat(5)
+    b = lecture9.Cat(5)
     b.set_name("test_animal_b")
     assert str(b) == "cat:test_animal_b:5"
     b.speak()
@@ -21,8 +21,8 @@ def test_cat_inheritance(capsys):
     assert "meow" in captured.out
 
 def test_person_methods(capsys):
-    p1 = l9.Person("P1", 101)
-    p2 = l9.Person("P2", 102)
+    p1 = lecture9.Person("P1", 101)
+    p2 = lecture9.Person("P2", 102)
     assert p1.get_name() == "P1"
     assert p2.get_age() == 102
     p1.add_friend("Nobody")
@@ -36,10 +36,10 @@ def test_person_methods(capsys):
     assert "person:P1:101" == str(p1)
 
 def test_rabbit_creation_and_addition():
-    l9.Rabbit.tag = 1  # reset counter for test reproducibility
-    r1 = l9.Rabbit(3)
-    r2 = l9.Rabbit(4)
-    r3 = l9.Rabbit(5)
+    lecture9.Rabbit.tag = 1  # reset counter for test reproducibility
+    r1 = lecture9.Rabbit(3)
+    r2 = lecture9.Rabbit(4)
+    r3 = lecture9.Rabbit(5)
     assert r1.get_rid() == "001"
     assert r2.get_rid() == "002"
     assert r3.get_rid() == "003"
@@ -49,13 +49,13 @@ def test_rabbit_creation_and_addition():
     r4 = r1 + r2
     assert r4.get_parent1() is r1
     assert r4.get_parent2() is r2
-    assert isinstance(r4, l9.Rabbit)
+    assert isinstance(r4, lecture9.Rabbit)
     
 def test_rabbit_equality():
-    l9.Rabbit.tag = 10  # reset again
-    r1 = l9.Rabbit(1)
-    r2 = l9.Rabbit(2)
-    r3 = l9.Rabbit(3)
+    lecture9.Rabbit.tag = 10  # reset again
+    r1 = lecture9.Rabbit(1)
+    r2 = lecture9.Rabbit(2)
+    r3 = lecture9.Rabbit(3)
     r4 = r1 + r2
     r5 = r3 + r4
     r6 = r4 + r3
