a717275003f4a1de76a7d70d9b6d50e78f0b72f5
BenjaminHuang <huangkunwei.ee12@nycu.edu.tw>
2025-12-09 23:41:48 +0800

    refactor: remove unused imports in test file
    Cleaned up the test file by removing unnecessary imports and
    path adjustments to streamline the code.

--- tests/group4/data_science_lec12_lect12_test.py
+++ tests/group4/data_science_lec12_lect12_test.py
@@ -1,14 +1,2 @@
-import sys, os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../..")))
-lec12_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../src/mit_ocw_data_science/lec12"))
-sys.path.insert(0, lec12_dir)
-
-import add_path
-import numpy as np
-from src.mit_ocw_data_science.lec12.lect12 import scaleAttrs, getData, kmeans, trykmeans, printClustering, testClustering ,Patient
-from src.mit_ocw_data_science.lec12.cluster import minkowski_dist, Example, Cluster, dissimilarity
-
-
-import random
 
 
