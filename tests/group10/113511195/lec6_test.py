219007192c263e9bbf9c729411cc15abf81d2dc4
billy <billyyc44.ee13@nycu.edu.tw>
2025-12-12 00:41:36 +0800

    add basic test for lec6

---
+++ tests/group10/113511195/lec6_test.py
@@ -0,0 +1,17 @@
+import pytest
+import add_path
+import lec6_recursion_dictionaries as lec6 # type: ignore
+import time
+
+def test_fib():
+    assert lec6.fib(0) == 1
+    assert lec6.fib(1) == 1
+    assert lec6.fib(2) == 2
+    assert lec6.fib(3) == 3
+    assert lec6.fib(4) == 5
+    assert lec6.fib(5) == 8
+    assert lec6.fib(6) == 13
+    assert lec6.fib(7) == 21
+    assert lec6.fib(8) == 34
+    assert lec6.fib(9) == 55
+    assert lec6.fib(10) == 89
