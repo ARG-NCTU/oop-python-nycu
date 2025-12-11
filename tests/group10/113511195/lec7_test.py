9d5e641aee8f2488d49cc1c3e57daeb5d8e3954e
billy <billyyc44.ee13@nycu.edu.tw>
2025-12-12 00:46:09 +0800

    add new test in lec7

--- tests/group10/113511195/lec7_test.py
+++ tests/group10/113511195/lec7_test.py
@@ -30,9 +30,15 @@ def test_primes_list():
     assert lec7.primes_list(10) == [2, 3, 5, 7]
     assert lec7.primes_list(20) == [2, 3, 5, 7, 11, 13, 17, 19]
     assert lec7.primes_list(40) == [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37]
-    
+
 def test_primes_list_generalize():
     known_primes_up_to_100 = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
     for n in range(2, 101):
         expected = [p for p in known_primes_up_to_100 if p <= n]
         assert lec7.primes_list(n) == expected
+    
+def test_get_ratios():
+    assert lec7.get_ratios([0, 0], [1, 2]) == [0, 0]
+    assert lec7.get_ratios([1, 3, 5, 7], [1, 3, 5, 7]) == [1, 1, 1, 1]
+    assert lec7.get_ratios([8, 8, 8, 8, 8], [2, 4, 8, 16, 32]) == [4, 2, 1, 0.5, 0.25]
+    assert lec7.get_ratios([], []) == []
