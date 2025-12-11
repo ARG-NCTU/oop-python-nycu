d7f18917f3c3561fde6de6fc82a20fda9371ab7f
billy <billyyc44.ee13@nycu.edu.tw>
2025-12-12 00:40:59 +0800

    making the test more general

--- tests/group10/113511195/lec3_test.py
+++ tests/group10/113511195/lec3_test.py
@@ -10,3 +10,12 @@ def test_is_palindrome():
     assert lec3.is_palindrome("A man a plan a canal Panama") == False
     assert lec3.is_palindrome("AmanaplanacanalPanama") == True
     assert lec3.is_palindrome("") == True
+
+def test_is_palindrome_random():
+    for _ in range(100):
+        length = random.randint(0, 100)
+        tar = ""
+        for _ in range(length):
+            char = chr(random.randint(97, 122))
+            tar = char + tar + char
+        assert lec3.is_palindrome(tar) == True
