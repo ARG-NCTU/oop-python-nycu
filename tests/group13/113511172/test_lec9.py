c8098517a15b22bcb43149b490695ef86a526d98
jeffjun113 <jeffjun113@gmail.com>
2025-12-18 15:55:00 +0800

    lec9 test_person_methods

--- tests/group13/113511172/test_lec9.py
+++ tests/group13/113511172/test_lec9.py
@@ -19,3 +19,18 @@ def test_cat_inheritance(capsys):
     b.speak()
     captured = capsys.readouterr()
     assert "meow" in captured.out
+
+def test_person_methods(capsys):
+    p1 = l9.Person("P1", 101)
+    p2 = l9.Person("P2", 102)
+    assert p1.get_name() == "P1"
+    assert p2.get_age() == 102
+    p1.add_friend("Nobody")
+    assert p1.get_friends() == ["Nobody"]
+    p1.speak()
+    captured = capsys.readouterr()
+    assert "hello" in captured.out
+    p1.age_diff(p2)
+    captured = capsys.readouterr()
+    assert "1 year difference" in captured.out
+    assert "person:P1:101" == str(p1)
