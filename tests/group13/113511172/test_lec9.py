3945217e583428fa2fa3c9fccf505192f29a89cc
jeffjun113 <jeffjun113@gmail.com>
2025-12-18 15:54:46 +0800

    lec9 test_cat_inheritance

--- tests/group13/113511172/test_lec9.py
+++ tests/group13/113511172/test_lec9.py
@@ -11,3 +11,11 @@ def test_animal_basic():
     a.set_age(10)
     assert a.get_age() == 10
     assert "animal:test_animal_a:10" == str(a)
+
+def test_cat_inheritance(capsys):
+    b = l9.Cat(5)
+    b.set_name("test_animal_b")
+    assert str(b) == "cat:test_animal_b:5"
+    b.speak()
+    captured = capsys.readouterr()
+    assert "meow" in captured.out
