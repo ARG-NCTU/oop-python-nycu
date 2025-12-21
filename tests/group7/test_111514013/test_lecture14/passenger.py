a62f829433854bb4c419eddb157f8dc0ccd806c7
unknown899 <kaiwy58.ee11@nycu.edu.tw>
2025-12-22 00:14:12 +0800

    massenger.py

---
+++ tests/group7/test_111514013/test_lecture14/passenger.py
@@ -0,0 +1,60 @@
+#-nycu/src/mit_ocw_data_science/lec14/passenger.py
+import os
+from distance import minkowskiDist
+
+class Passenger(object):
+    featureNames = ('C1', 'C2', 'C3', 'age', 'male gender')
+    def __init__(self, pClass, age, gender, survived, name):
+        self.name = name
+        self.featureVec = [0, 0, 0, age, gender]
+        self.featureVec[pClass - 1] = 1
+        self.label = survived
+        self.cabinClass = pClass
+    def distance(self, other):
+        return minkowskiDist(self.featureVec, other.featureVec, 2)
+    def getClass(self):
+        return self.cabinClass
+    def getAge(self):
+        return self.featureVec[3]
+    def getGender(self):
+        return self.featureVec[4]
+    def getName(self):
+        return self.name
+    def getFeatures(self):
+        return self.featureVec[:]
+    def getLabel(self):
+        return self.label
+
+def getTitanicData(fname):
+    data = {}
+    data['class'], data['survived'], data['age'] = [], [], []
+    data['gender'], data['name'] = [], []
+    fpath = os.path.join(os.path.dirname(__file__), fname)
+    f = open(fpath)
+    line = f.readline()
+    while line != '':
+        split = line.split(',')
+        data['class'].append(int(split[0]))
+        data['age'].append(float(split[1]))
+        if split[2] == 'M':
+            data['gender'].append(1)
+        else:
+            data['gender'].append(0)
+        if split[3] == '1':
+            data['survived'].append('Survived')
+        else:
+            data['survived'].append('Died')
+        data['name'].append(split[4:])
+        line = f.readline()
+    return data
+                
+def buildTitanicExamples(fileName):
+    data = getTitanicData(fileName)
+    examples = []
+    for i in range(len(data['class'])):
+        p = Passenger(data['class'][i], data['age'][i],
+                      data['gender'][i], data['survived'][i],
+                      data['name'][i])
+        examples.append(p)
+    print('Finished processing', len(examples), 'passengers\n')    
+    return examples
