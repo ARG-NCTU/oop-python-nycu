68f5b753bd3415969a43a23ad7d377a28e5b76eb
Joanna <joanna.ee13@nycu.edu.tw>
2025-12-22 22:34:42 +0800

    lec12 code

---
+++ tests/group2/113511088/lec12_sorting.py
@@ -0,0 +1,112 @@
+# -*- coding: utf-8 -*-
+"""
+lec12_sorting_113511088.py
+Sorting: bubble sort, selection sort, merge sort
+"""
+
+def bubble_sort(L):
+    swap = False
+    while not swap:
+        print('bubble sort: ' + str(L))
+        swap = True
+        for j in range(1, len(L)):
+            if L[j-1] > L[j]:
+                swap = False
+                temp = L[j]
+                L[j] = L[j-1]
+                L[j-1] = temp
+    return L
+
+
+def selection_sort(L):
+    suffixSt = 0
+    while suffixSt != len(L):
+        print('selection sort: ' + str(L))
+        for i in range(suffixSt, len(L)):
+            if L[i] < L[suffixSt]:
+                L[suffixSt], L[i] = L[i], L[suffixSt]
+        suffixSt += 1
+    return L
+
+
+def merge(left, right):
+    result = []
+    i, j = 0, 0
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+    while i < len(left):
+        result.append(left[i])
+        i += 1
+    while j < len(right):
+        result.append(right[j])
+        j += 1
+    print('merge: ' + str(left) + '&' + str(right) + ' to ' + str(result))
+    return result
+
+
+def merge_sort(L):
+    print('merge sort: ' + str(L))
+    if len(L) < 2:
+        return L[:]
+    else:
+        middle = len(L)//2
+        left = merge_sort(L[:middle])
+        right = merge_sort(L[middle:])
+        return merge(left, right)
+
+
+def bubble_sort_np(L):
+    swap = False
+    while not swap:
+        swap = True
+        for j in range(1, len(L)):
+            if L[j-1] > L[j]:
+                swap = False
+                temp = L[j]
+                L[j] = L[j-1]
+                L[j-1] = temp
+    return L
+
+
+def selection_sort_np(L):
+    suffixSt = 0
+    while suffixSt != len(L):
+        for i in range(suffixSt, len(L)):
+            if L[i] < L[suffixSt]:
+                L[suffixSt], L[i] = L[i], L[suffixSt]
+        suffixSt += 1
+    return L
+
+
+def merge_np(left, right):
+    result = []
+    i, j = 0, 0
+    while i < len(left) and j < len(right):
+        if left[i] <= right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+    while i < len(left):
+        result.append(left[i])
+        i += 1
+    while j < len(right):
+        result.append(right[j])
+        j += 1
+    return result
+
+
+def merge_sort_np(L):
+    if len(L) < 2:
+        return L[:]
+    else:
+        middle = len(L)//2
+        left = merge_sort_np(L[:middle])
+        right = merge_sort_np(L[middle:])
+        return merge_np(left, right)
