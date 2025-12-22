1debb8d50fd335553d56d44d44f5200ed74e05b1
jerry <jerrywustarwars@gmail.com>
2025-12-22 17:44:44 +0800

    add

---
+++ tests/group6/test_codes/113511086/test_lec12_bubble_buggy.py
@@ -0,0 +1,14 @@
+import pytest
+
+@pytest.mark.skip(reason="Known bug in bubble sort")
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        for j in range(n):
+            if arr[j] > arr[j+1]:
+                arr[j] = arr[j+1]
+                arr[j+1] = arr[j]
+    return arr
+
+#print(bubble_sort([4, 2, 1, 8, 7, 6]))
+
