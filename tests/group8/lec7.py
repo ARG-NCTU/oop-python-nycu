4bb2fc616e4a5bcaa40fe2596cd18ff7b3a76101
hungchi0222 <charlie950222@gmail.com>
2025-12-23 21:59:00 +0800

    changing lec7

--- tests/group8/lec7.py
+++ tests/group8/lec7.py
@@ -52,11 +52,7 @@ def avg(grades):
     return sum(grades) / len(grades)
   except ZeroDivisionError:
     return 0.0
-
-
-########################################
 # 5. get_stats — 回傳學生平均成績
-########################################
 def get_stats(class_list):
   new_stats = []
   for person in class_list:
