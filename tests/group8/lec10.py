7993c33913b2bd07962dfe54d63aa68427ff3afd
hungchi0222 <charlie950222@gmail.com>
2025-12-23 21:56:29 +0800

    trying lec10

--- tests/group8/lec10.py
+++ tests/group8/lec10.py
@@ -21,7 +21,7 @@ def isSubset(L1, L2):
     for e1 in L1:
         matched = False
         for e2 in L2:
-            complexity += 1
+            complexity ++
             if e1 == e2:
                 matched = True
                 break
