f8983089040f4d1c871ad7d0ea2f9e4f569767ec
iamkyleh <kylehsu0803@gmail.com>
2025-12-29 05:22:03 +0800

    delete some code

--- tests/group10/113511197/lec_11.py
+++ tests/group10/113511197/lec_11.py
@@ -2,10 +2,7 @@
 import pytest
 from lec11_complexity_part2 import bisect_search1, bisect_search2, genSubsets
 
-# -----------------
-# 測試資料 (全域)
-# -----------------
-# 一個大列表
+
 L_100 = list(range(100)) 
 # 一個小列表 (奇數長度)
 L_small = [1, 3, 5, 7, 9] 
@@ -37,9 +34,7 @@ def test_bisect_search1():
 # 測試 bisect_search2 (索引版本)
 # -----------------
 def test_bisect_search2():
-    # 測試案例應該和 search1 完全一樣，因為它們的「功能」相同
-    
-    # 測試找到
+
     assert bisect_search2(L_100, 76) is True
     assert bisect_search2(L_small, 5) is True  # 找中間
     assert bisect_search2(L_small, 1) is True  # 找開頭
@@ -56,30 +51,24 @@ def test_bisect_search2():
     assert bisect_search2([5], 5) is True       # 單一元素 (找到)
     assert bisect_search2([5], 4) is False      # 單一元素 (沒找到)
 
-# -----------------
-# 測試 genSubsets (產生子集)
-# -----------------
+
 
 def test_genSubsets():
 
-    # 測試空列表
     result_empty = genSubsets([])
-    assert result_empty == [[]] # 規範定義
-
-    # 測試單一元素
+    assert result_empty == [[]] 
     result_one = genSubsets([1])
-    # [[]] + [[1]]
+
     expected_one = sorted([[], [1]])
-    # 把結果也排序，確保順序一致
+
     sorted_result_one = sorted([sorted(sub) for sub in result_one])
     assert sorted_result_one == expected_one
 
-    # 測試兩個元素
     result_two = genSubsets([1, 2])
-    # [[]] + [[1]] + [[2], [1, 2]]
+
     expected_two = sorted([[], [1], [2], [1, 2]])
     sorted_result_two = sorted([sorted(sub) for sub in result_two])
-    assert len(result_two) == 4 # 2^2
+    assert len(result_two) == 4 
     assert sorted_result_two == expected_two
     result_three = genSubsets([1, 2, 3])
     expected_three = sorted([
@@ -88,5 +77,5 @@ def test_genSubsets():
         [1, 2, 3]
     ])
     sorted_result_three = sorted([sorted(sub) for sub in result_three])
-    assert len(result_three) == 8 # 2^3
+    assert len(result_three) == 8 
     assert sorted_result_three == expected_three
