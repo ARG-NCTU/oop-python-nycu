e90912e9bb7dce7a70bb91392fda1f98de80f0f1
billy <billyyc44.ee13@nycu.edu.tw>
2025-12-28 23:50:41 +0800

    add test

--- tests/group10/test_113511195/lec9_test.py
+++ tests/group10/test_113511195/lec9_test.py
@@ -1,3 +1,7 @@
 import add_path
 import lec9_inheritance.py as lec9
 
+def test_animal_init_defaults():
+    a = lec9.Animal(4)
+    assert lec9.Animal.get_age(a) == 4
+    assert lec9.Animal.get_name(a) is None
