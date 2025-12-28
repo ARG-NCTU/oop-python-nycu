4a7494f775c260fc6398e65a77c9a08c5072d0b5
iamkyleh <kylehsu0803@gmail.com>
2025-12-29 05:01:23 +0800

    debug

--- tests/group10/113511197/test_lec12_sorting.py
+++ tests/group10/113511197/test_lec12_sorting.py
@@ -42,21 +42,4 @@ def test_merge_sort_np():
     
     result = merge_sort_np(L1)
     assert result == [1, 2, 4, 5, 8]   
-    assert L1 == L1_original         
-    L3 = [3, 2, 1]
-    assert merge_sort_np(L3) == [1, 2, 3]
-    L4 = [2, 1, 3, 2, 1]
-    assert merge_sort_np(L4) == [1, 1, 2, 2, 3]
-    assert merge_sort_np([]) == []
-    assert merge_sort_np([42]) == [42]
-def test_merge_np():
-
-    left = [1, 3, 5]
-    right = [2, 4, 6]
-    assert merge_np(left, right) == [1, 2, 3, 4, 5, 6]
-
-    left_short = [1, 5]
-    right_long = [2, 4, 6, 8]
-    assert merge_np(left_short, right_long) == [1, 2, 4, 5, 6, 8]
- 
-   
+    asser
