b3671939a3be7a1044d5184e26fe0e9370b35889
billy <billyyc44.ee13@nycu.edu.tw>
2025-12-28 23:51:15 +0800

    add test

--- tests/group10/test_113511195/lec9_test.py
+++ tests/group10/test_113511195/lec9_test.py
@@ -10,3 +10,9 @@ def test_animal_set_age():
     a = lec9.Animal(4)
     lec9.Animal.set_age(a, 10)
     assert lec9.Animal.get_age(a) == 10
+    
+def test_animal_set_name_explicit():
+    a = lec9.Animal(4)
+    lec9.Animal.set_name(a, "fluffy")
+    assert lec9.Animal.get_name(a) == "fluffy"
+    assert str(a) == "animal:fluffy:4"
