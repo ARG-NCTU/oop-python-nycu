ee6ee728fd07d47ac292030a15671a4794f0a62c
billy <billyyc44.ee13@nycu.edu.tw>
2025-12-28 23:50:57 +0800

    add test

--- tests/group10/test_113511195/lec9_test.py
+++ tests/group10/test_113511195/lec9_test.py
@@ -5,3 +5,8 @@ def test_animal_init_defaults():
     a = lec9.Animal(4)
     assert lec9.Animal.get_age(a) == 4
     assert lec9.Animal.get_name(a) is None
+
+def test_animal_set_age():
+    a = lec9.Animal(4)
+    lec9.Animal.set_age(a, 10)
+    assert lec9.Animal.get_age(a) == 10
