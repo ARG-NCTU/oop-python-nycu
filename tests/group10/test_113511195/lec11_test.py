cdca7f8519ed4beca5206c11b303f4b3dc1a1076
billy <billyyc44.ee13@nycu.edu.tw>
2025-12-29 15:24:27 +0800

    add test

--- tests/group10/test_113511195/lec11_test.py
+++ tests/group10/test_113511195/lec11_test.py
@@ -31,3 +31,23 @@ def test_bisect_search1_prints_trace_for_nonempty(capsys):
     printed = capsys.readouterr().out
     assert "low:" in printed
     assert "high:" in printed
+
+@pytest.mark.parametrize(
+    "L,e,expected",
+    [
+        ([], 0, False),
+        ([0], 0, True),
+        ([0], 1, False),
+        ([0, 1, 2, 3, 4], 0, True),
+        ([0, 1, 2, 3, 4], 4, True),
+        ([0, 1, 2, 3, 4], 2, True),
+        ([0, 1, 2, 3, 4], 5, False),
+        ([0, 1, 2, 3, 4], -1, False),
+        (list(range(100)), 76, True),
+        (list(range(100)), 100, False),
+    ],
+)
+def test_bisect_search2_results(L, e, expected, capsys):
+    result = lec11.bisect_search2(L, e)
+    capsys.readouterr()
+    assert result is expected
