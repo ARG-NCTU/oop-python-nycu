2156d1dfcd1596afc2e52ee47f03264ff226f0c2
billy <billyyc44.ee13@nycu.edu.tw>
2025-12-29 15:24:02 +0800

    add test

--- tests/group10/test_113511195/lec11_test.py
+++ tests/group10/test_113511195/lec11_test.py
@@ -5,3 +5,22 @@ def test_bisect_search1_empty_list_raises_indexerror():
     # Lecture code prints L[0] and L[-1] before checking empty -> IndexError
     with pytest.raises(IndexError):
         lec11.bisect_search1([], 0)
+
+@pytest.mark.parametrize(
+    "L,e,expected",
+    [
+        ([0], 0, True),
+        ([0], 1, False),
+        ([0, 1, 2, 3, 4], 0, True),
+        ([0, 1, 2, 3, 4], 4, True),
+        ([0, 1, 2, 3, 4], 2, True),
+        ([0, 1, 2, 3, 4], 5, False),
+        ([0, 1, 2, 3, 4], -1, False),
+        (list(range(100)), 76, True),
+        (list(range(100)), 100, False),
+    ],
+)
+def test_bisect_search1_results_nonempty(L, e, expected, capsys):
+    result = lec11.bisect_search1(L, e)
+    capsys.readouterr()
+    assert result is expected
