4f75f9cbf91fc4db135d6a17943506553a891f93
billy <billyyc44.ee13@nycu.edu.tw>
2025-12-29 15:23:44 +0800

    add test

--- tests/group10/test_113511195/lec11_test.py
+++ tests/group10/test_113511195/lec11_test.py
@@ -1,1 +1,7 @@
 import lec_code.lec11 as lec11
+import pytest
+
+def test_bisect_search1_empty_list_raises_indexerror():
+    # Lecture code prints L[0] and L[-1] before checking empty -> IndexError
+    with pytest.raises(IndexError):
+        lec11.bisect_search1([], 0)
