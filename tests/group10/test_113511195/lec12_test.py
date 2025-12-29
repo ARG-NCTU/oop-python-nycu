ec95ce7e74ea74e92d882cb3c98c2076cdcef7e0
billy <billyyc44.ee13@nycu.edu.tw>
2025-12-29 15:29:48 +0800

    add test

--- tests/group10/test_113511195/lec12_test.py
+++ tests/group10/test_113511195/lec12_test.py
@@ -84,3 +84,22 @@ def test_bubble_sort_np_sorts_correctly(L):
     out = lec12.bubble_sort_np(L)
     assert out == sorted(original)
     assert out is L  # still mutates and returns same object
+
+@pytest.mark.parametrize(
+    "L",
+    [
+        [],
+        [1],
+        [2, 1],
+        [3, 1, 2],
+        [5, 4, 3, 2, 1],
+        [1, 2, 3, 4, 5],
+        [2, 3, 2, 1, 3, 1],
+        [-1, 0, -3, 2, 1],
+    ],
+)
+def test_selection_sort_np_sorts_correctly(L):
+    original = L[:]
+    out = lec12.selection_sort_np(L)
+    assert out == sorted(original)
+    assert out is L
