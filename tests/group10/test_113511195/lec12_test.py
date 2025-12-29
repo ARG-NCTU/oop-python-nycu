0d53653e5360a0537e1d6ce58e29955311049e1a
billy <billyyc44.ee13@nycu.edu.tw>
2025-12-29 15:29:04 +0800

    add test

--- tests/group10/test_113511195/lec12_test.py
+++ tests/group10/test_113511195/lec12_test.py
@@ -22,3 +22,24 @@ def test_bubble_sort_sorts_correctly(L, capsys):
     assert out == sorted(original)
     # bubble_sort returns the same list object it mutated
     assert out is L
+
+
+@pytest.mark.parametrize(
+    "L",
+    [
+        [],
+        [1],
+        [2, 1],
+        [3, 1, 2],
+        [5, 4, 3, 2, 1],
+        [1, 2, 3, 4, 5],
+        [2, 3, 2, 1, 3, 1],
+        [-1, 0, -3, 2, 1],
+    ],
+)
+def test_selection_sort_sorts_correctly(L, capsys):
+    original = L[:]
+    out = lec12.selection_sort(L)
+    capsys.readouterr()
+    assert out == sorted(original)
+    assert out is L
