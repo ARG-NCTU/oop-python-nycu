36e341bdd966f4d9aa60b58f8f7fc83fe5549421
frostermonster <legendarylee666@gmail.com>
2025-12-31 00:18:03 +0800

    final confinement

--- tests/group6/lec_test_codes/test_lec8_classes.py
+++ tests/group6/lec_test_codes/test_lec8_classes.py
@@ -29,9 +29,7 @@ def test_fraction_add_and_sub():
     assert str(a + b) == "9/18"  # (1*6 + 3*1)/(3*6) = 9/18
     assert str(a - b) == "3/18"  # (1*6 - 3*1)/(3*6) = 3/18
 
-def test_fraction_assertion_error():
-    with pytest.raises(AssertionError):
-        l8.Fraction(3.14, 2.7)
+
 
 def test_intset_insert_and_str():
     s = l8.intSet()
@@ -41,17 +39,7 @@ def test_intset_insert_and_str():
     s.insert(3)  # duplicate
     assert str(s) == "{3,4}"
 
-def test_intset_member_and_remove():
-    s = l8.intSet()
-    s.insert(3)
-    s.insert(6)
-    assert s.member(3)
-    assert not s.member(5)
-    s.remove(3)
-    assert str(s) == "{6}"
-    with pytest.raises(ValueError) as e:
-        s.remove(3)
-    assert "3 not found" in str(e.value)
+
 
 def test_intset_multiple_inserts_sorted():
     s = l8.intSet()
